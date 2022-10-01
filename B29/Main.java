f6c283e8bed9d2d5a48c65f0311575f26ce8db69
kouta1612 <kouta1612world69@gmail.com>
2022-10-02 07:57:15 +0900

    B29

---
+++ B29/Main.java
@@ -0,0 +1,26 @@
+package B29;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        sc.close();
+
+        System.out.println(power(a, b, 1000000007));
+    }
+
+    static long power(long a, long b, long m) {
+        long result = 1;
+        for (int i = 0; i < 60; i++) {
+            if ((b >> i) % 2 == 1) {
+                result = (result * a) % m;
+            }
+            a = (a * a) % m;
+        }
+
+        return result;
+    }
+}
