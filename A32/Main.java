ec92f6e6478a2c2c3cf4b337e06ea51f1577e6a8
kouta1612 <kouta1612world69@gmail.com>
2022-10-02 10:40:46 +0900

    A32

---
+++ A32/Main.java
@@ -0,0 +1,30 @@
+package A32;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (i >= a && dp[i - a] == false) {
+                dp[i] = true;
+            } else if (i >= b && dp[i - b] == false) {
+                dp[i] = true;
+            } else {
+                dp[i] = false;
+            }
+        }
+
+        if (dp[n]) {
+            System.out.println("First");
+        } else {
+            System.out.println("Second");
+        }
+    }
+}
