7f9dc3d38f8061b8fa912f4850e346f05d3b7e01
kouta1612 <kouta1612world69@gmail.com>
2022-10-04 01:18:50 +0900

    B34

---
+++ B34/Main.java
@@ -0,0 +1,35 @@
+package B34;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        long[] a = new long[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextLong();
+        }
+        sc.close();
+
+        // grundy[i]: x = 2, y = 3のもとで00112の周期がある
+        int xorSum = 0;
+        for (int i = 0; i < n; i++) {
+            if ((a[i] + 1) % 5 == 0) {
+                xorSum ^= 2;
+            } else if ((a[i] + 2) % 5 == 0 || (a[i] + 3) % 5 == 0) {
+                xorSum ^= 1;
+            } else if (a[i] % 5 == 0 || (a[i] + 1) % 5 == 0) {
+                xorSum ^= 0;
+            }
+        }
+
+        if (xorSum == 0) {
+            System.out.println("Second");
+        } else {
+            System.out.println("First");
+        }
+    }
+}
