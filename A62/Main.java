20a68842d2f9a0d2a8a3238718932e3352ed2f4e
kouta1612 <kouta1612world69@gmail.com>
2022-10-13 09:09:28 +0900

    A62

---
+++ A62/Main.java
@@ -0,0 +1,59 @@
+package A62;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static boolean[] found;
+    static ArrayList<Integer>[] g;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] a = new int[m];
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+        sc.close();
+
+        found = new boolean[n + 1];
+
+        g = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            g[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            g[a[i]].add(b[i]);
+            g[b[i]].add(a[i]);
+        }
+
+        dfs(1);
+
+        boolean result = true;
+        for (int i = 1; i <= n; i++) {
+            if (found[i] == false) {
+                result = false;
+            }
+        }
+
+        if (result) {
+            System.out.println("The graph is connected.");
+        } else {
+            System.out.println("The graph is not connected.");
+        }
+    }
+
+    static void dfs(int st) {
+        found[st] = true;
+        for (int i = 0; i < g[st].size(); i++) {
+            int next = g[st].get(i);
+            if (found[next] == false) {
+                dfs(next);
+            }
+        }
+        return;
+    }
+}
