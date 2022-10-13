c679115824810bae37cc299a4de6f05f0d8e1766
kouta1612 <kouta1612world69@gmail.com>
2022-10-13 21:32:58 +0900

    B62

---
+++ B62/Main.java
@@ -0,0 +1,58 @@
+package B62;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static ArrayDeque<Integer> path;
+    static boolean[] visited;
+    static ArrayList<Integer>[] g;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] a = new int[m];
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+        sc.close();
+
+        path = new ArrayDeque<>();
+        visited = new boolean[n + 1];
+        g = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            g[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            g[a[i]].add(b[i]);
+            g[b[i]].add(a[i]);
+        }
+        dfs(1, n);
+
+        StringBuilder builder = new StringBuilder();
+        for (int p : path) {
+            builder.append(p).append(" ");
+        }
+
+        System.out.println(builder.toString().trim());
+    }
+
+    static void dfs(int st, int n) {
+        visited[st] = true;
+        path.addLast(st);
+        for (int i = 0; i < g[st].size(); i++) {
+            int next = g[st].get(i);
+            if (!visited[next]) {
+                dfs(next, n);
+            }
+        }
+        if (path.getLast() != n) {
+            path.removeLast();
+        }
+        return;
+    }
+}
