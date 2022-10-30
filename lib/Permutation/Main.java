57870c732ebbf2c809f62a0c62b38379729588ed
kouta1612 <kouta1612world69@gmail.com>
2022-10-30 22:39:58 +0900

    順列ライブラリ改良

--- lib/Permutation/Main.java
+++ lib/Permutation/Main.java
@@ -1,32 +1,43 @@
 package lib.Permutation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        permutation(new int[] { 1, 2, 3 });
+        Permutation p = new Permutation();
+        List<Integer[]> result = p.make(new Integer[] { 1, 2, 3 });
+        for (Integer[] list : result) {
+            System.out.println(Arrays.toString(list));
+        }
     }
+}
 
-    static void permutation(int[] seed) {
-        int[] perm = new int[seed.length];
+class Permutation {
+    List<Integer[]> make(Integer[] seed) {
+        List<Integer[]> result = new ArrayList<>();
+        Integer[] perm = new Integer[seed.length];
         boolean[] used = new boolean[seed.length];
-        buildPerm(0, seed, perm, used);
+        return buildPerm(0, result, seed, perm, used);
     }
 
-    static void buildPerm(int pos, int[] seed, int[] perm, boolean[] used) {
-        if (pos == perm.length) {
-            System.out.println(Arrays.toString(perm));
-            return;
+    List<Integer[]> buildPerm(Integer pos, List<Integer[]> result, Integer[] seed, Integer[] perm, boolean[] used) {
+        Integer[] newPerm = Arrays.copyOf(perm, perm.length);
+        if (pos == newPerm.length) {
+            result.add(newPerm);
         }
 
-        for (int i = 0; i < perm.length; i++) {
+        for (int i = 0; i < newPerm.length; i++) {
             if (used[i]) {
                 continue;
             }
-            perm[pos] = seed[i];
+            newPerm[pos] = seed[i];
             used[i] = true;
-            buildPerm(pos + 1, seed, perm, used);
+            buildPerm(pos + 1, result, seed, newPerm, used);
             used[i] = false;
         }
+
+        return result;
     }
 }
