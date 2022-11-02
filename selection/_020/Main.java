3cfce69ac36d5cc647094982bf0b2e049f540391
kouta1612 <kouta1612world69@gmail.com>
2022-11-02 09:21:17 +0900

    マージ修正

--- selection/_020/Main.java
+++ selection/_020/Main.java
@@ -7,49 +7,59 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[] h = new long[n];
-        long[] s = new long[n];
+        int[] a = new int[n];
+        int[] b = new int[n];
+        int[] c = new int[n];
         for (int i = 0; i < n; i++) {
-            h[i] = sc.nextLong();
-            s[i] = sc.nextLong();
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            c[i] = sc.nextInt();
         }
         sc.close();
 
-        long l = -1, r = 1L << 60;
-        long mid = (l + r) / 2;
+        Arrays.sort(a);
+        Arrays.sort(c);
+
+        long result = 0;
+        for (int i = 0; i < n; i++) {
+            long ub = upper_bound(a, b[i]);
+            long lb = lower_bound(c, b[i]);
+            result += (ub + 1) * (n - lb);
+        }
+
+        System.out.println(result);
+    }
+
+    static int lower_bound(int[] a, int key) {
+        int l = -1, r = a.length;
         while (r - l > 1) {
-            mid = (l + r) / 2;
-            if (check(mid, h, s)) {
+            int mid = (l + r) / 2;
+            if (a[mid] > key) {
                 r = mid;
             } else {
                 l = mid;
             }
         }
 
-        System.out.println(r);
+        return r;
     }
 
-    // 高度の上限xでシミュレーションして全ての風船を割ることができるか
-    static boolean check(long x, long[] h, long[] s) {
-        // 各風船を何秒以内に割る必要があるかを前計算する
-        long[] limit = new long[h.length];
-        boolean ok = true;
-        for (int i = 0; i < limit.length; i++) {
-            limit[i] = (x - h[i]) / s[i];
-            // そもそも初期の高度が上限よりも高かったらダメ（ここの条件忘れてた）
-            if (h[i] > x) {
-                ok = false;
+    static int upper_bound(int[] a, int key) {
+        int l = -1, r = a.length;
+        while (r - l > 1) {
+            int mid = (l + r) / 2;
+            if (a[mid] < key) {
+                l = mid;
+            } else {
+                r = mid;
             }
         }
 
-        Arrays.sort(limit);
-
-        for (int i = 0; i < limit.length; i++) {
-            if (limit[i] < i) {
-                ok = false;
-            }
-        }
-
-        return ok;
+        return l;
     }
+
 }
