bef3a8885af1a846cf102621ca37d64d9de9a533
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2022-11-04 19:12:50 +0900

    static変数にまとめた

--- selection/_027/Main.java
+++ selection/_027/Main.java
@@ -3,32 +3,34 @@ package selection._027;
 import java.util.Scanner;
 
 public class Main {
+    static int h, w;
+    static int[][] a;
     static int result = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
-        int x = sc.nextInt();
-        int[][] a = new int[x][y];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
+        w = sc.nextInt();
+        h = sc.nextInt();
+        a = new int[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
                 a[i][j] = sc.nextInt();
             }
         }
         sc.close();
 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
                 if (a[i][j] == 0) {
                     continue;
                 }
-                dfs(i, j, 1, a, x, y);
+                dfs(i, j, 1);
             }
         }
 
         System.out.println(result);
     }
 
-    static void dfs(int x, int y, int count, int[][] a, int maxX, int maxY) {
+    static void dfs(int x, int y, int count) {
         a[x][y] = 0;
         int[] dx = new int[]{-1,0,1,0};
         int[] dy = new int[]{0,1,0,-1};
@@ -37,13 +39,13 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (!(0 <= nx && nx < maxX && 0 <= ny && ny < maxY)) {
+            if (!(0 <= nx && nx < h && 0 <= ny && ny < w)) {
                 continue;
             }
             if (a[nx][ny] == 0) {
                 continue;
             }
-            dfs(nx, ny, count + 1, a, maxX, maxY);
+            dfs(nx, ny, count + 1);
         }
         a[x][y] = 1;
     }
