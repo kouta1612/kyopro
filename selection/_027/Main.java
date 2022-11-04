184de739dbb5bb0eb9e04bdf20783d5c63969fcc
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2022-11-04 18:22:05 +0900

    visitedフラグ削除してリファクタリング

--- selection/_027/Main.java
+++ selection/_027/Main.java
@@ -15,14 +15,13 @@ public class Main {
         }
         sc.close();
 
-        boolean[][] visited = new boolean[x][y];
         int result = 0;
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
                 if (a[i][j] == 0) {
                     continue;
                 }
-                int cur = dfs(i, j, 1, visited, a, x, y);
+                int cur = dfs(i, j, 1, a, x, y);
                 result = Math.max(result, cur);
             }
         }
@@ -30,8 +29,8 @@ public class Main {
         System.out.println(result);
     }
 
-    static int dfs(int x, int y, int count, boolean[][] visited, int[][] a, int maxX, int maxY) {
-        visited[x][y] = true;
+    static int dfs(int x, int y, int count, int[][] a, int maxX, int maxY) {
+        a[x][y] = 0;
         int[] dx = new int[]{-1,0,1,0};
         int[] dy = new int[]{0,1,0,-1};
 
@@ -42,13 +41,13 @@ public class Main {
             if (!(0 <= nx && nx < maxX && 0 <= ny && ny < maxY)) {
                 continue;
             }
-            if (a[nx][ny] == 0 || visited[nx][ny]) {
+            if (a[nx][ny] == 0) {
                 continue;
             }
-            int cur = dfs(nx, ny, count + 1, visited, a, maxX, maxY);
+            int cur = dfs(nx, ny, count + 1, a, maxX, maxY);
             result = Math.max(result, cur);
         }
-        visited[x][y] = false;
+        a[x][y] = 1;
 
         return Math.max(count, result);
     }
