d4aa338b074186a2c46f348c864d81713ee1e667
kouta1612 <kouta1612world69@gmail.com>
2022-11-06 14:28:54 +0900

    033

---
+++ selection/_033/Main.java
@@ -0,0 +1,74 @@
+package selection._033;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int w = sc.nextInt();
+        String[][] s = new String[h][w];
+        for (int i = 0; i < h; i++) {
+            s[i] = sc.next().split("");
+        }
+        sc.close();
+
+        int[][] dist = new int[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                dist[i][j] = 1 << 30;
+            }
+        }
+        dist[0][0] = 0;
+
+        Deque<Point> queue = new ArrayDeque<>();
+        queue.add(new Point(0, 0));
+        while (!queue.isEmpty()) {
+            Point p = queue.poll();
+            int[] dx = new int[] { -1, 0, 1, 0 };
+            int[] dy = new int[] { 0, 1, 0, -1 };
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if (!(0 <= nx && nx < h && 0 <= ny && ny < w)) {
+                    continue;
+                }
+                if (s[nx][ny].equals("#")) {
+                    continue;
+                }
+                if (dist[nx][ny] != 1 << 30) {
+                    continue;
+                }
+                dist[nx][ny] = dist[p.x][p.y] + 1;
+                queue.add(new Point(nx, ny));
+            }
+        }
+
+        if (dist[h - 1][w - 1] == 1 << 30) {
+            System.out.println(-1);
+            return;
+        }
+
+        int result = 0;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (s[i][j].equals(".")) {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result - (dist[h - 1][w - 1] + 1));
+    }
+}
+
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
