9888455af42eeb9156cd3235c812b24c9e77d65f
kouta1612 <kouta1612world69@gmail.com>
2022-11-07 09:38:01 +0900

    035

---
+++ selection/_036/Main.java
@@ -0,0 +1,30 @@
+package selection._036;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int W = sc.nextInt();
+        int[] v = new int[N];
+        int[] w = new int[N];
+        for (int i = 0; i < N; i++) {
+            v[i] = sc.nextInt();
+            w[i] = sc.nextInt();
+        }
+        sc.close();
+
+        int[][] dp = new int[N + 1][W + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (j - w[i - 1] >= 0) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i][j - w[i - 1]] + v[i - 1]);
+                }
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+            }
+        }
+
+        System.out.println(dp[N][W]);
+    }
+}
