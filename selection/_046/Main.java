fa71f662688f2050804612e9d44de9176adc03dc
kouta1612 <kouta1612world69@gmail.com>
2022-11-14 23:18:51 +0900

    入力は実装した

---
+++ selection/_046/Main.java
@@ -0,0 +1,17 @@
+package selection._046;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+}
