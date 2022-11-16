fb18f9fb1be4507f5694945c4b135563a9a92be2
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2022-11-16 12:42:58 +0900

    046

--- selection/_046/Main.java
+++ selection/_046/Main.java
@@ -13,5 +13,25 @@ public class Main {
             b[i] = sc.nextInt();
         }
         sc.close();
+
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                if (i == j) {
+                    continue;
+                }
+                dp[i][j] = 1<<30;
+            }
+        }
+        for (int l = 2; l <= n; l++) {
+            for (int i = 1; i <= n - l + 1; i++) {
+                int j = i + l - 1;
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] + a[i - 1] * b[k - 1] * b[j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[1][n]);
     }
 }
