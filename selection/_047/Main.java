a776b75ec305664de772926d171461e68fe81504
kouta1612 <kouta1612world69@gmail.com>
2022-11-17 21:05:13 +0900

    60AC, 9WA

--- selection/_047/Main.java
+++ selection/_047/Main.java
@@ -1,23 +1,22 @@
 package selection._047;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[2 * n + 1];
+        long[] a = new long[2 * n + 1];
         for (int i = 1; i <= n; i++) {
-            a[i] = a[i + n] = sc.nextInt();
+            a[i] = a[i + n] = sc.nextLong();
         }
         sc.close();
 
         // dp[i][j]: 区間[i,j]におけるJOIくんの取りうる最大値
-        int[][] dp = new int[n + 1][2 * (n + 1)];
+        long[][] dp = new long[2 * n + 1][2 * n + 1];
 
         // 区間の長さが1の場合は別で処理
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= 2 * n; i++) {
             if (n % 2 == 1) {
                 dp[i][i] = a[i];
             }
@@ -26,25 +25,28 @@ public class Main {
         // l: 区間の取りうる長さ
         for (int l = 2; l <= n; l++) {
             // 区間の左端
-            for (int i = 1; i <= n - l + 1; i++) {
+            for (int i = 1; i <= n; i++) {
                 // 区間の右端
                 int j = i + l - 1;
 
-                // IOIの番
+                // JOIの番
                 if ((n % 2 == 0 && l % 2 == 0) || (n % 2 == 1 && l % 2 == 1)) {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-                } else {
                     dp[i][j] = Math.max(dp[i + 1][j] + a[i], dp[i][j - 1] + a[j]);
+                } else {
+                    if (a[i] > a[j]) {
+                        dp[i][j] = dp[i + 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 1];
+                    }
                 }
             }
         }
 
-        int result = 0;
+        long result = 0;
         for (int i = 1; i <= n; i++) {
-            result = Math.max(result, dp[i][n + i]);
+            result = Math.max(result, dp[i][n + i - 1]);
         }
 
         System.out.println(result);
-        System.out.println(Arrays.deepToString(dp));
     }
 }
