cdfce5e06aeb8559806399eba52bb7fe80ec5a22
kouta1612 <kouta1612world69@gmail.com>
2022-11-26 18:32:07 +0900

    ライブラリ修正

--- lib/BinarySearch/Main.java
+++ lib/BinarySearch/Main.java
@@ -4,53 +4,98 @@ public class Main {
     public static void main(String[] args) {
         int[] a = { 1, 2, 4, 5, 5 };
         System.out.println(lower_bound(a, 1));
-        System.out.println(upper_bound(a, 3));
+        System.out.println(upper_bound(a, 1));
+
+        // keyの個数を求めることができる
+        System.out.println(upper_bound(a, 5) - lower_bound(a, 5));
+
+        // 汎用的な二分探索法を使用
+        System.out.println(binary_search(a, 4));
     }
 
     /**
-     * ある条件(a[i] >= x)を満たす最小のiを求める
-     * (lまでは条件を満たさず、r以降は条件を満たすように更新し、rを返すように実装する)
-     * 参考：[lower_bound](https://qiita.com/drken/items/97e37dd6143e33a64c8c#2-%E4%B8%80%E8%88%AC%E5%8C%96%E3%81%97%E3%81%9F%E4%BA%8C%E5%88%86%E6%8E%A2%E7%B4%A2%E6%B3%95)
+     * a[index] >= key という条件を満たす最小の index を求める
      *
      * @param a
      * @param key
      * @return
      */
     static int lower_bound(int[] a, int key) {
-        int l = -1, r = a.length;
-        // 条件を満たすかどうかの堺目が分かる状態になるのはl + 1 = rの状態
-        while (r - l > 1) {
-            int mid = (l + r) / 2;
+        int left = -1; // 「index = 0」が条件を満たすこともあるので、初期値は -1
+        int right = a.length; // 「index = a.length-1」が条件を満たさないこともあるので、初期値は a.length
+
+        // left は「常に」条件を満たさず、right は「常に」条件を満たすよう更新
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+
             if (a[mid] >= key) {
-                r = mid;
+                right = mid;
             } else {
-                l = mid;
+                left = mid;
             }
         }
 
-        return r;
+        // left は条件を満たさない最大の値、right は条件を満たす最小の値になっている
+        return right;
     }
 
     /**
-     * ある条件(a[i] <= x)を満たす最大のiを求める
-     * (lまでは条件を満たし、r以降は条件をみたさないように更新し、lを返すように実装する)
-     * 参考：[upper_bound](https://qiita.com/drken/items/97e37dd6143e33a64c8c#2-%E4%B8%80%E8%88%AC%E5%8C%96%E3%81%97%E3%81%9F%E4%BA%8C%E5%88%86%E6%8E%A2%E7%B4%A2%E6%B3%95:~:text=%E4%BB%A3%E3%82%8F%E3%82%8A%E3%81%AB%E4%BB%A5%E4%B8%8B%E3%81%AE%E3%82%88%E3%81%86%E3%81%AB%E3%81%97%E3%81%A6%E3%80%8C%E6%9D%A1%E4%BB%B6%E3%82%92%E6%BA%80%E3%81%9F%E3%81%99%E6%9C%80%E5%A4%A7%E3%81%AE%20index%20%E3%82%92%E6%B1%82%E3%82%81%E3%82%8B%E3%80%8D%E3%81%A8%E3%81%84%E3%81%86%E9%A2%A8%E3%81%AB%E3%81%97%E3%81%9F%E3%81%8F%E3%81%AA%E3%82%8B%E5%A0%B4%E9%9D%A2%E3%82%82%E3%81%82%E3%82%8A%E3%81%BE%E3%81%99%E3%80%82)
+     * a[index] > key という条件を満たす最小の index を求める
      *
      * @param a
      * @param key
      * @return
      */
     static int upper_bound(int[] a, int key) {
-        int l = -1, r = a.length;
-        while (r - l > 1) {
-            int mid = (l + r) / 2;
-            if (a[mid] <= key) {
-                l = mid;
+        int left = -1; // 「index = 0」が条件を満たすこともあるので、初期値は -1
+        int right = a.length; // 「index = a.length-1」が条件を満たさないこともあるので、初期値は a.length
+
+        // left は「常に」条件を満たさず、right は「常に」条件を満たすよう更新
+        while (right - left > 1) {
+            int mid = (left + right) / 2;
+
+            if (a[mid] > key) {
+                right = mid;
             } else {
-                r = mid;
+                left = mid;
             }
         }
 
-        return l;
+        // left は条件を満たさない最大の値、right は条件を満たす最小の値になっている
+        return right;
     }
+
+    /**
+     * 汎用的な二分探索法テンプレ
+     * 
+     * @param a
+     * @param key
+     * @return
+     */
+    static int binary_search(int[] a, int key) {
+        int left = -1; // 「index = 0」が条件を満たすこともあるので、初期値は -1
+        int right = (int) a.length; // 「index = a.length-1」が条件を満たさないこともあるので、初期値は a.length
+
+        // left は「常に」条件を満たさず、right は「常に」条件を満たすよう更新
+        while (right - left > 1) {
+            int mid = left + (right - left) / 2;
+
+            if (isOK(a, mid, key))
+                right = mid;
+            else
+                left = mid;
+        }
+
+        // left は条件を満たさない最大の値、right は条件を満たす最小の値になっている
+        return right;
+    }
+
+    // index が条件を満たすかどうか
+    static boolean isOK(int[] a, int index, int key) {
+        if (a[index] >= key)
+            return true;
+        else
+            return false;
+    }
+
 }
