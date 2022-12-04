d685801826f3e304766269d277962fd581c54e45
kouta1612 <kouta1612world69@gmail.com>
2022-12-04 13:03:30 +0900

    UnionFindライブラリ作成

---
+++ lib/UnionFind/Main.java
@@ -0,0 +1,58 @@
+package lib.UnionFind;
+
+public class Main {
+    public static void main(String[] args) {
+        UnionFind uf = new UnionFind(5);
+        uf.unite(0, 1);
+        uf.unite(1, 2);
+        System.out.println(uf.same(0, 1));
+        System.out.println(uf.same(0, 2));
+        System.out.println(uf.same(0, 3));
+    }
+}
+
+class UnionFind {
+    // par[i] : 頂点iの親に該当する頂点番号
+    // size[i]: 頂点iを根とする木の頂点数
+    int[] par, size;
+
+    UnionFind(int n) {
+        par = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = -1;
+            size[i] = 1;
+        }
+    }
+
+    public void unite(int u, int v) {
+        int rootU = root(u);
+        int rootV = root(v);
+
+        // uとvが同じグループの場合は結合処理を行わない
+        if (rootU == rootV) {
+            return;
+        }
+
+        // UnionBySize
+        if (size[rootU] < size[rootV]) {
+            par[rootU] = rootV;
+            size[rootV] += size[rootU];
+        } else {
+            par[rootV] = rootU;
+            size[rootU] += size[rootV];
+        }
+    }
+
+    public boolean same(int u, int v) {
+        return root(u) == root(v);
+    }
+
+    private int root(int x) {
+        if (par[x] == -1) {
+            return x;
+        }
+
+        return root(par[x]);
+    }
+}
