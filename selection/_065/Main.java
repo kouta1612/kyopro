67fc6d86690db7eeafa5d0f9696b5dcc9a613b12
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2022-12-05 19:26:43 +0900

    途中まで実装したがサンプルが通らない

---
+++ selection/_065/Main.java
@@ -0,0 +1,111 @@
+package selection._065;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            int a = sc.nextInt() - 1;
+            int b = sc.nextInt() - 1;
+            int c = sc.nextInt();
+            edges.add(new Edge(a, b, c));
+        }
+        sc.close();
+
+        Collections.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return o1.weight < o2.weight ? -1 : 1;
+            }
+        });
+
+        int result = 0;
+        int remain = m;
+        UnionFind uf = new UnionFind(n);
+        for (Edge edge : edges) {
+            if (remain == k) {
+                break;
+            }
+            if (!uf.same(edge.source, edge.dest)) {
+                uf.unite(edge.source, edge.dest);
+                result += edge.weight;
+            }
+            remain--;
+        }
+
+        System.out.println(result);
+    }
+}
+
+class Edge {
+    int source, dest;
+    long weight;
+
+    Edge(int source, int dest, long weight) {
+        this.source = source;
+        this.dest = dest;
+        this.weight = weight;
+    }
+}
+
+class UnionFind {
+    // par[i] : 頂点iの親に該当する頂点番号
+    // size[i]: 頂点iを根とする木の頂点数
+    int[] par, size;
+
+    UnionFind(int n) {
+        par = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = -1;
+            size[i] = 1;
+        }
+    }
+
+    public boolean unite(int u, int v) {
+        int rootU = root(u);
+        int rootV = root(v);
+
+        // uとvが同じグループの場合は結合処理を行わない
+        if (rootU == rootV) {
+            return false;
+        }
+
+        // UnionBySize
+        if (size[rootU] < size[rootV]) {
+            par[rootU] = rootV;
+            size[rootV] += size[rootU];
+        } else {
+            par[rootV] = rootU;
+            size[rootU] += size[rootV];
+        }
+
+        return true;
+    }
+
+    public boolean same(int u, int v) {
+        return root(u) == root(v);
+    }
+
+    public long size(int x) {
+        int rootX = root(x);
+        return size[rootX];
+    }
+
+    private int root(int x) {
+        if (par[x] == -1) {
+            return x;
+        }
+
+        return root(par[x]);
+    }
+}
