7623cd29099226a0c04235881dbd64e28edf47e2
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2022-12-05 17:25:41 +0900

    064

--- selection/_064/Main.java
+++ selection/_064/Main.java
@@ -20,8 +20,24 @@ public class Main {
         }
         sc.close();
 
-        edges.sort(new EdgeComparator());
+        Collections.sort(edges, new Comparator<Edge>() {
+            @Override
+            public int compare(Edge o1, Edge o2) {
+                return o1.weight < o2.weight ? -1 : 1;
+            }
+        });
 
+        UnionFind uf = new UnionFind(n);
+        int result = 0;
+        for (Edge edge : edges) {
+            if (uf.same(edge.source, edge.dest)) {
+                continue;
+            }
+            result += edge.weight;
+            uf.unite(edge.source, edge.dest);
+        }
+
+        System.out.println(result);
     }
 }
 
@@ -36,9 +52,55 @@ class Edge {
     }
 }
 
-class EdgeComparator implements Comparator<Edge> {
-    @Override
-    public int compare(Edge o1, Edge o2) {
-        return o1.weight < o2.weight ? -1 : 1;
+class UnionFind {
+    // par[i] : 頂点iの親に該当する頂点番号
+    // size[i]: 頂点iを根とする木の頂点数
+    int[] par, size;
+
+    UnionFind(int n) {
+        par = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = -1;
+            size[i] = 1;
+        }
+    }
+
+    public boolean unite(int u, int v) {
+        int rootU = root(u);
+        int rootV = root(v);
+
+        // uとvが同じグループの場合は結合処理を行わない
+        if (rootU == rootV) {
+            return false;
+        }
+
+        // UnionBySize
+        if (size[rootU] < size[rootV]) {
+            par[rootU] = rootV;
+            size[rootV] += size[rootU];
+        } else {
+            par[rootV] = rootU;
+            size[rootU] += size[rootV];
+        }
+
+        return true;
+    }
+
+    public boolean same(int u, int v) {
+        return root(u) == root(v);
+    }
+
+    public long size(int x) {
+        int rootX = root(x);
+        return size[rootX];
+    }
+
+    private int root(int x) {
+        if (par[x] == -1) {
+            return x;
+        }
+
+        return root(par[x]);
     }
 }
