87446c2f2fff78973a3bce72f4c28caf3cdaee76
kouta1612 <kouta1612world69@gmail.com>
2022-12-08 09:40:32 +0900

    入力だけ実装

---
+++ selection/_072/Main.java
@@ -0,0 +1,12 @@
+package selection._072;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long w = sc.nextLong();
+        long h = sc.nextLong();
+        sc.close();
+    }
+}
