1a14765dff866753389a7ab4f13b910ad1b3b79f
kouta1612 <kouta1612world69@gmail.com>
2022-12-11 12:11:11 +0900

    途中まで実装

---
+++ selection/_079/Main.java
@@ -0,0 +1,20 @@
+package selection._079;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int q = sc.nextInt();
+
+        int[] sum = new int[m];
+        for (int i = 0; i < m; i++) {
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+
+        }
+
+    }
+}
