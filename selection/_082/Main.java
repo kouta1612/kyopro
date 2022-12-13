f518dc09e9e872afadff311ac78337f6aa7ffef3
kouta1612 <kouta1612world69@gmail.com>
2022-12-13 09:37:58 +0900

    入力まで実装

---
+++ selection/_082/Main.java
@@ -0,0 +1,22 @@
+package selection._082;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            for (int i = 0; i < n; i++) {
+                String s = sc.next();
+                String t = sc.next();
+            }
+        }
+        sc.close();
+    }
+}
