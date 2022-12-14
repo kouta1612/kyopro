d0555bdbdeb129cfc14b190dd72a64980eb9b315
hagiwara-lvgs <kota.hagiwara@leverages.jp>
2022-12-14 13:05:16 +0900

    途中まで実装した

--- selection/_083/Main.java
+++ selection/_083/Main.java
@@ -1,5 +1,6 @@
 package selection._083;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,17 +9,43 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] p = new int[m];
-        long[] a = new long[n];
-        long[] b = new long[n];
-        long[] c = new long[n];
+        long[] a = new long[n - 1];
+        long[] b = new long[n - 1];
+        long[] c = new long[n - 1];
         for (int i = 0; i < m; i++) {
-            p[i] = sc.nextInt();
+            p[i] = sc.nextInt() - 1;
         }
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             a[i] = sc.nextLong();
             b[i] = sc.nextLong();
             c[i] = sc.nextLong();
         }
         sc.close();
+
+        // 累積和
+        // sum[i]: 鉄道iに乗車する合計回数
+        int[] sum = new int[n + 1];
+        for (int i = 0; i < m - 1; i++) {
+            int start = p[i];
+            int end = p[i + 1];
+            if (start > end) {
+                sum[start + 1]--;
+                sum[end + 1]++;
+            } else {
+                sum[start]++;
+                sum[end]--;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            sum[i] += sum[i - 1];
+        }
+
+        long result = 0;
+        for (int i = 0; i < n - 1; i++) {
+            result += Math.min(a[i] * sum[i], b[i] * sum[i] + c[i]);
+        }
+
+        System.out.println(result);
+        // System.out.println(Arrays.toString(sum));
     }
 }
