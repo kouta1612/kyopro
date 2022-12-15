a83e509d4315df652628de65abe45f89e6e8a8b5
kouta1612 <kouta1612world69@gmail.com>
2022-12-15 09:48:55 +0900

    入力まで実装

---
+++ selection/_088/Main.java
@@ -0,0 +1,15 @@
+package selection._088;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+}
