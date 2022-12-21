3f6c4c3f2ba4840d1b4a32f7db5dcfa324d7e5c4
kouta1612 <kouta1612world69@gmail.com>
2022-12-21 09:14:10 +0900

    098

---
+++ selection/_098/Main.java
@@ -0,0 +1,28 @@
+package selection._098;
+
+import java.util.Scanner;
+
+public class Main {
+    static final int MOD = 1000000007;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        long[] count = new long[n + 1];
+        long result = 1;
+        count[0] = 3;
+        for (int i = 0; i < n; i++) {
+            result = result * count[a[i]] % MOD;
+            count[a[i]]--;
+            count[a[i] + 1]++;
+        }
+
+        System.out.println(result);
+    }
+}
