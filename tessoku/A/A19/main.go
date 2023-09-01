ae958a80b715ffde12ed7ee85c9f7f83ee92e6e4
kouta1612 <kouta1612world69@gmail.com>
2023-09-01 22:03:42 +0900

    より模範解答に近い方法で実装するようリファクタリング

--- tessoku/A/A19/main.go
+++ tessoku/A/A19/main.go
@@ -20,14 +20,27 @@ func main() {
 		dp[i] = make([]int64, W+1)
 	}
 
+	for i := 0; i <= N; i++ {
+		for j := 0; j <= W; j++ {
+			dp[i][j] = -int64(math.Pow10(11))
+		}
+	}
+	dp[0][0] = 0
+
 	for i := 1; i <= N; i++ {
 		for j := 0; j <= W; j++ {
 			if j-w[i-1] >= 0 {
-				dp[i][j] = int64(math.Max(float64(dp[i-1][j-w[i-1]]+v[i-1]), float64(dp[i][j])))
+				dp[i][j] = int64(math.Max(float64(dp[i-1][j-w[i-1]]+v[i-1]), float64(dp[i-1][j])))
+			} else {
+				dp[i][j] = dp[i-1][j]
 			}
-			dp[i][j] = int64(math.Max(float64(dp[i-1][j]), float64(dp[i][j])))
 		}
 	}
 
-	fmt.Println(dp[N][W])
+	var result int64
+	for i := 0; i <= W; i++ {
+		result = int64(math.Max(float64(result), float64(dp[N][i])))
+	}
+
+	fmt.Println(result)
 }
