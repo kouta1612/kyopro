3486b16a0b489943493f08de2d168cadc8ecfc3b
kouta1612 <kouta1612world69@gmail.com>
2023-10-07 10:48:13 +0900

    iの状態からi+1の状態へまずコピーするするようリファクタリング

--- atcoder/abc322/e/main.go
+++ atcoder/abc322/e/main.go
@@ -24,35 +24,20 @@ func main() {
 	// ここはより良い方法ないか探る
 	dp[0] = map[[5]int]int{{0, 0, 0, 0, 0}: 0}
 	for i := 0; i < n; i++ {
-		if dp[i+1] == nil {
-			dp[i+1] = make(map[[5]int]int)
+		dp[i+1] = make(map[[5]int]int)
+		for d, v := range dp[i] {
+			dp[i+1][d] = v
 		}
-		for status, cost := range dp[i] {
-			// 選ばない場合（TODO: ここで場合分けする理由を理解する）
-			if dp[i+1][status] == 0 {
-				dp[i+1][status] = cost
-			} else {
-				dp[i+1][status] = int(math.Min(float64(dp[i+1][status]), float64(cost)))
-			}
-
+		for status := range dp[i] {
 			// 選ぶ場合
 			nextStatus := status
 			for j := 0; j < k; j++ {
 				nextStatus[j] = int(math.Min(float64(p), float64(nextStatus[j]+a[i][j])))
 			}
-			if savedCost, exist := dp[i][nextStatus]; exist {
-				if dp[i+1][nextStatus] == 0 {
-					dp[i+1][nextStatus] = int(math.Min(float64(savedCost), float64(cost+c[i])))
-				} else {
-					dp[i+1][nextStatus] = int(math.Min(math.Min(float64(dp[i+1][nextStatus]), float64(savedCost)), float64(cost+c[i])))
-				}
+			if cost, exist := dp[i+1][nextStatus]; exist {
+				dp[i+1][nextStatus] = int(math.Min(float64(cost), float64(dp[i][status]+c[i])))
 			} else {
-				// （TODO: ここで場合分けする理由を理解する）
-				if dp[i+1][nextStatus] == 0 {
-					dp[i+1][nextStatus] = cost + c[i]
-				} else {
-					dp[i+1][nextStatus] = int(math.Min(float64(dp[i+1][nextStatus]), float64(cost+c[i])))
-				}
+				dp[i+1][nextStatus] = dp[i][status] + c[i]
 			}
 		}
 	}
