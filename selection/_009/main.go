9723b1a7faf003f18999339de270e9b9155d350b
kouta1612 <kouta1612world69@gmail.com>
2023-10-30 22:05:14 +0900

    解説AC

---
+++ selection/_009/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+type point struct {
+	x, y int
+}
+
+func main() {
+	var m int
+	fmt.Scan(&m)
+
+	a := make([]point, m)
+	for i := 0; i < m; i++ {
+		var x, y int
+		fmt.Scan(&x, &y)
+
+		a[i] = point{x: x, y: y}
+	}
+
+	var n int
+	fmt.Scan(&n)
+
+	b := make([]point, n)
+	for i := 0; i < n; i++ {
+		var x, y int
+		fmt.Scan(&x, &y)
+
+		b[i] = point{x: x, y: y}
+	}
+
+	for i := 0; i < n; i++ {
+		// 平行移動する量
+		dx := b[i].x - a[0].x
+		dy := b[i].y - a[0].y
+
+		c := make([]point, m)
+		copy(c, a)
+		for j := 0; j < m; j++ {
+			c[j].x = a[j].x + dx
+			c[j].y = a[j].y + dy
+		}
+
+		ok := true
+		for j := 0; j < m; j++ {
+			found := false
+			for k := 0; k < n; k++ {
+				if c[j].x == b[k].x && c[j].y == b[k].y {
+					found = true
+				}
+			}
+			if !found {
+				ok = false
+			}
+		}
+
+		if ok {
+			fmt.Println(dx, dy)
+		}
+	}
+}
