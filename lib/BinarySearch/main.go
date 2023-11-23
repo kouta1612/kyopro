02ba39a6e27a2f21179ffc4193708342bcaf9f43
kouta1612 <kouta1612world69@gmail.com>
2023-11-23 19:36:46 +0900

    lowerBoundとupperBoundのライブラリ作成

---
+++ lib/BinarySearch/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 4, 7}
+
+	fmt.Println(lowerBound(a, 7))
+	fmt.Println(upperBound(a, 7))
+}
+
+// a[i] >= v を満たす最小のインデックスを取得する
+func lowerBound(a []int, v int) int {
+	ng, ok := -1, len(a)
+	for ok-ng > 1 {
+		mid := (ok + ng) / 2
+		if a[mid] >= v {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// a[i] > v を満たす最小のインデックスを取得する
+func upperBound(a []int, v int) int {
+	ng, ok := -1, len(a)
+	for ok-ng > 1 {
+		mid := (ok + ng) / 2
+		if a[mid] > v {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
