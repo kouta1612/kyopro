4ef7ce6528aa52aa44dbd777a6c11711941fc6e7
kouta1612 <kouta1612world69@gmail.com>
2023-11-25 14:15:55 +0900

    PriorityQuue使いやすいように改造した

--- selection/_056/main.go
+++ selection/_056/main.go
@@ -34,15 +34,11 @@ func main() {
 
 	kakutei := make([]bool, v)
 
-	pq := make(priorityQueue, 0)
-	heap.Init(&pq)
-	heap.Push(&pq, &item{
-		v: r,
-		w: 0,
-	})
+	pq := NewPriorityQueue()
+	pq.push(&item{v: r, w: 0})
 
-	for pq.Len() > 0 {
-		element := heap.Pop(&pq).(*item)
+	for pq.len() > 0 {
+		element := pq.pop()
 
 		for _, edge := range g[element.v] {
 			if kakutei[edge.to] {
@@ -50,10 +46,7 @@ func main() {
 			}
 
 			vs[edge.to] = min(vs[edge.to], vs[element.v]+edge.d)
-			heap.Push(&pq, &item{
-				v: edge.to,
-				w: vs[edge.to],
-			})
+			pq.push(&item{v: edge.to, w: vs[edge.to]})
 		}
 
 		kakutei[element.v] = true
@@ -68,6 +61,29 @@ func main() {
 	}
 }
 
+func NewPriorityQueue() *priorityQueue {
+	pq := make(pq, 0)
+	heap.Init(&pq)
+
+	return &priorityQueue{pq: pq}
+}
+
+func (p *priorityQueue) len() int {
+	return p.pq.Len()
+}
+
+func (p *priorityQueue) push(item *item) {
+	heap.Push(&p.pq, item)
+}
+
+func (p *priorityQueue) pop() *item {
+	return heap.Pop(&p.pq).(*item)
+}
+
+type priorityQueue struct {
+	pq pq
+}
+
 type edge struct {
 	to, d int
 }
@@ -76,29 +92,29 @@ type item struct {
 	v, w, index int
 }
 
-type priorityQueue []*item
+type pq []*item
 
-func (pq priorityQueue) Len() int { return len(pq) }
+func (pq pq) Len() int { return len(pq) }
 
-func (pq priorityQueue) Less(i, j int) bool {
+func (pq pq) Less(i, j int) bool {
 	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
 	return pq[i].w < pq[j].w
 }
 
-func (pq priorityQueue) Swap(i, j int) {
+func (pq pq) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 	pq[i].index = i
 	pq[j].index = j
 }
 
-func (pq *priorityQueue) Push(x interface{}) {
+func (pq *pq) Push(x interface{}) {
 	n := len(*pq)
 	item := x.(*item)
 	item.index = n
 	*pq = append(*pq, item)
 }
 
-func (pq *priorityQueue) Pop() interface{} {
+func (pq *pq) Pop() interface{} {
 	old := *pq
 	n := len(old)
 	item := old[n-1]
