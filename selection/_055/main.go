b2a03ff495ab047ff1f5fec0346bb54827c4d8d4
kouta1612 <kouta1612world69@gmail.com>
2023-11-25 11:56:10 +0900

    解説AC

---
+++ selection/_055/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+const INF = int(1e16)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	n := scanInt()
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = scanInt()
+	}
+
+	dp := make([]int, 0)
+	for i := n - 1; i >= 0; i-- {
+		if i == n-1 {
+			dp = append(dp, a[i])
+			continue
+		}
+
+		it := upperBound(dp, a[i])
+		if it == len(dp) {
+			dp = append(dp, a[i])
+		} else {
+			dp[it] = a[i]
+		}
+	}
+
+	fmt.Println(len(dp))
+}
+
+func scanInt() int {
+	sc.Scan()
+
+	res, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+
+	return res
+}
+
+func scanStr() string {
+	sc.Scan()
+
+	return sc.Text()
+}
+
+// a[i] >= v を満たす最小のインデックスを取得する
+func lowerBound(a []int, v int) int {
+	ng, ok := -1, len(a)
+	for ok-ng > 1 {
+		mid := (ok + ng) / 2
+		if a[mid] >= v {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// a[i] > v を満たす最小のインデックスを取得する
+func upperBound(a []int, v int) int {
+	ng, ok := -1, len(a)
+	for ok-ng > 1 {
+		mid := (ok + ng) / 2
+		if a[mid] > v {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+func min(n ...int) int {
+	res := n[0]
+
+	for _, v := range n {
+		res = int(math.Min(float64(res), float64(v)))
+	}
+
+	return res
+}
+
+func max(n ...int) int {
+	res := n[0]
+
+	for _, v := range n {
+		res = int(math.Max(float64(res), float64(v)))
+	}
+
+	return res
+}
+
+func abs(a int) int {
+	return int(math.Abs(float64(a)))
+}
