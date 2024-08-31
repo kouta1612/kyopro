253bfebe6f67112086eb6d331b17993a6f16bca8
kouta1612 <kouta1612world69@gmail.com>
2024-08-31 10:24:36 +0900

    704

---
+++ neetcode/go/_704/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(search([]int{-1, 0, 3, 5, 9, 12}, 9))
+	fmt.Println(search([]int{1, 0, 3, 5, 9, 12}, 2))
+}
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return -1
+}
