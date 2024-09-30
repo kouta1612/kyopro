0116c6b6e1450a89e7d01ca41a2c49195ec2c2df
kouta1612 <kouta1612world69@gmail.com>
2024-09-30 12:01:23 +0900

    リファクタリング

--- atcoder/abc137/d/main.go
+++ atcoder/abc137/d/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"math"
 	"os"
 	"strconv"
+
+	"github.com/liyue201/gostl/ds/priorityqueue"
+	"github.com/liyue201/gostl/utils/comparator"
 )
 
 const (
@@ -35,82 +37,18 @@ func main() {
 		tasks[m-a] = append(tasks[m-a], b)
 	}
 
-	pq := NewPriorityQueue()
+	q := priorityqueue.New[int](comparator.Reverse(comparator.IntComparator))
 	result := 0
 	for i := m - 1; i >= 0; i-- {
-		for i, v := range tasks[i] {
-			pq.push(&item{index: i, v: v, w: v})
+		for _, v := range tasks[i] {
+			q.Push(v)
 		}
-		if pq.len() > 0 {
-			item := pq.pop()
-			result += item.v
+		if q.Size() > 0 {
+			result += q.Pop()
 		}
 	}
 
 	fmt.Println(result)
-}
-
-// 以降はプライオリティーキュー自前ライブラリ
-type priorityQueue struct {
-	pq pq
-}
-
-func NewPriorityQueue() *priorityQueue {
-	pq := make(pq, 0)
-	heap.Init(&pq)
-
-	return &priorityQueue{pq: pq}
-}
-
-func (p *priorityQueue) len() int {
-	return p.pq.Len()
-}
-
-func (p *priorityQueue) push(item *item) {
-	heap.Push(&p.pq, item)
-}
-
-func (p *priorityQueue) pop() *item {
-	return heap.Pop(&p.pq).(*item)
-}
-
-// プライオリティーキューに突っ込むオブジェクト
-type item struct {
-	v, w, index int
-}
-
-// 以降はcontainer/heapの実装クラス
-type pq []*item
-
-func (pq pq) Len() int { return len(pq) }
-
-// デフォルトでは優先度の低い順
-// 高い順にしたい場合: return pq[i].w > pq[j].w に修正する
-func (pq pq) Less(i, j int) bool {
-	return pq[i].w > pq[j].w
-}
-
-func (pq pq) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *pq) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*item)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *pq) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
 }
 
 func init() {
