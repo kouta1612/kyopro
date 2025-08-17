9bef09d3f170f82e41a4a82410924702134fbbfa
kouta1612 <kouta1612world69@gmail.com>
2025-08-18 07:06:24 +0900

    104

---
+++ neetcode/go/_104/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(maxDepth(nil))
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
