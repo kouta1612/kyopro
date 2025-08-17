784200b80792d27b72005037ed57c8e8e4c60763
kouta1612 <kouta1612world69@gmail.com>
2025-08-17 18:38:17 +0900

    226

---
+++ neetcode/go/_226/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(invertTree(nil))
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invertTree(root.Left)
+	invertTree(root.Right)
+	return root
+}
