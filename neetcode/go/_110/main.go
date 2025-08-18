d03e72c7ea60db8f26343073861ce15ca7b688e6
kouta1612 <kouta1612world69@gmail.com>
2025-08-19 07:33:08 +0900

    110

---
+++ neetcode/go/_110/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isBalanced(nil))
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if isBalanced(root.Left) && isBalanced(root.Right) && abs(dfs(root.Left)-dfs(root.Right)) <= 1 {
+		return true
+	}
+	return false
+}
+
+func dfs(root *TreeNode) int {
+	if root == nil {
+		return -1
+	}
+	return 1 + max(dfs(root.Left), dfs(root.Right))
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
