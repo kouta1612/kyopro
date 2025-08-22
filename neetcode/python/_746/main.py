ab1a997e75b66d9dc9c726aca3975f0916a87c1f
kouta1612 <kouta1612world69@gmail.com>
2025-08-23 08:18:16 +0900

    リファクタリング

--- neetcode/python/_746/main.py
+++ neetcode/python/_746/main.py
@@ -2,22 +2,12 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        dp1 = [1000000] * (len(cost)+1)
-        dp1[0] = 0
-        for i in range(len(dp1)):
-            if i + 1 < len(dp1):
-                dp1[i + 1] = min([dp1[i + 1], dp1[i] + cost[i]])
-            if i + 2 < len(dp1):
-                dp1[i + 2] = min([dp1[i + 2], dp1[i] + cost[i]])
-        
-        dp2 = [1000000] * (len(cost)+1)
-        dp2[0] = dp2[1] = 0
-        for i in range(1,len(dp2)):
-            if i + 1 < len(dp2):
-                dp2[i + 1] = min([dp2[i + 1], dp2[i] + cost[i]])
-            if i + 2 < len(dp2):
-                dp2[i + 2] = min([dp2[i + 2], dp2[i] + cost[i]])
+        n = len(cost)
+        dp = [0] * (n + 1)
 
-        return min([dp1[len(dp1)-1],dp2[len(dp2)-1]])
+        for i in range(2,n + 1):
+            dp[i] = min([dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]])
+
+        return dp[n]
 
 print(Solution().minCostClimbingStairs([10,15,20]))
