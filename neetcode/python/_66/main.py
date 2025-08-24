060caf3aa0ea00d9180258b06dca99779e08b936
kouta1612 <kouta1612world69@gmail.com>
2025-08-24 09:24:21 +0900

    リファクタリング

--- neetcode/python/_66/main.py
+++ neetcode/python/_66/main.py
@@ -2,22 +2,13 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        if digits[-1] >= 0 and digits[-1] < 9:
-            digits[-1] += 1
-        else:
-            idx = -1
-            for i in range(len(digits)-1, -1, -1):
-                if digits[i] != 9:
-                    idx = i
-                    break
-                else:
-                    digits[i] = 0
-            
-            if idx == -1:
-                digits = [1] + digits
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
             else:
-                digits[idx] += 1
+                digits[i] = 0
 
-        return digits
+        return [1] + digits
 
 print(Solution().plusOne([1,2,3]))
