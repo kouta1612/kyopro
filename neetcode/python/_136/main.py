34a2490570813eb54193467fffa559ee908cfcd9
kouta1612 <kouta1612world69@gmail.com>
2025-08-25 13:28:11 +0900

    リファクタリング

--- neetcode/python/_136/main.py
+++ neetcode/python/_136/main.py
@@ -2,12 +2,9 @@ from typing import List
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        keys = set()
+        res = 0
         for num in nums:
-            if num in keys:
-                keys.remove(num)
-            else:
-                keys.add(num)
-        return list(keys)[0]
+            res ^= num
+        return res
 
 print(Solution().singleNumber([4,1,2,1,2]))
