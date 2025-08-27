e968163f5e2b4a677140a028273a5d665c41b62e
kouta1612 <kouta1612world69@gmail.com>
2025-08-27 13:19:51 +0900

    パフォーマンス改善

--- neetcode/python/_338/main.py
+++ neetcode/python/_338/main.py
@@ -2,14 +2,16 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        res = []
-        for i in range(n + 1):
-            cnt = 0
-            for j in range(32):
-                if i & 1<<j:
-                    cnt += 1
-            res.append(cnt)
-        return res
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i-offset]
+        
+        return dp
+
 
 
 print(Solution().countBits(5))
