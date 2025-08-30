9cfa389e76216e2d697d2d52da076a4e22b35d60
kouta1612 <kouta1612world69@gmail.com>
2025-08-30 09:28:19 +0900

    パフォーマンス改善

--- neetcode/python/_125/main.py
+++ neetcode/python/_125/main.py
@@ -2,16 +2,17 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
-        s = re.sub("[^a-z0-9]", "", s)
         i,j = 0, len(s)-1
-
-        while i <= j:
-            if s[i] != s[j]:
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
                 return False
-            i += 1
-            j -= 1
+            i, j = i + 1, j - 1
         return True
+            
             
 
 print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
