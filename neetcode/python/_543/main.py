ee57680a9c68ab0dfd9d9f88674931339121e207
kouta1612 <kouta1612world69@gmail.com>
2025-09-04 13:59:26 +0900

    543

---
+++ neetcode/python/_543/main.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        
+        def dfs(root):
+            if root is None:
+                return 0
+            left, right = dfs(root.left), dfs(root.right)
+            self.res = max(self.res, left + right)
+            return max(left, right) + 1
+
+        dfs(root)
+
+        return self.res
+
+print(Solution().diameterOfBinaryTree(TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3))))
