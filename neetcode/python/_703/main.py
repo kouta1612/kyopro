42af1e43327eeb999ed7fe71f4ef3ce81e0c1a45
kouta1612 <kouta1612world69@gmail.com>
2025-09-05 10:27:53 +0900

    リファクタリング

--- neetcode/python/_703/main.py
+++ neetcode/python/_703/main.py
@@ -4,17 +4,16 @@ from typing import List
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.k = k
-        self.heapnums = nums
-        heapq.heapify(self.heapnums)
+        self.k, self.minHeap = k, nums
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
 
     def add(self, val: int) -> int:
-        heapq.heappush(self.heapnums, val)
-        while len(self.heapnums) > self.k:
-            heapq.heappop(self.heapnums)
-        res = heapq.heappop(self.heapnums)
-        heapq.heappush(self.heapnums, res)
-        return res
+        heapq.heappush(self.minHeap, val)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
