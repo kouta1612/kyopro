25a7a872178846602e4660c01e6ed8e4d5479059
kouta1612 <kouta1612world69@gmail.com>
2025-09-11 10:52:57 +0900

    リファクタリング

--- neetcode/python/_002/main.py
+++ neetcode/python/_002/main.py
@@ -7,27 +7,23 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode()
-        dummy = ListNode(0, head)
+        dummy = ListNode()
+        cur = dummy
 
-        add = 0
-        while l1 or l2 or add == 1:
-            v1, v2 = 0, 0
-            if l1:
-                v1 = l1.val
-            if l2:
-                v2 = l2.val
-            sum = v1 + v2 + add
-            head.val = sum % 10
-            add = sum // 10
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
-            if l1 or l2 or add == 1:
-                head.next = ListNode()
-            head = head.next
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            sum = v1 + v2 + carry
+            carry = sum // 10
+            cur.next = ListNode(sum % 10)
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            cur = cur.next
 
         return dummy.next
+
 
 print(Solution().addTwoNumbers(ListNode(2, ListNode(4, ListNode(3))), ListNode(5, ListNode(6, ListNode(4)))))
