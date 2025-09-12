ae92ab4d3a17cd12caa50dbe98cc62c0ff9c8cbd
kouta1612 <kouta1612world69@gmail.com>
2025-09-12 10:56:07 +0900

    リファクタリング

--- neetcode/python/_083/main.py
+++ neetcode/python/_083/main.py
@@ -8,13 +8,11 @@ class ListNode:
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur = head
-        while cur:
-            nxt = cur.next
-            while nxt and cur.val == nxt.val:
-                nxt = nxt.next
-            cur.next = nxt
-            cur = cur.next
-
+        while cur and cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
         return head
 
 print(Solution().deleteDuplicates(ListNode(1, ListNode(1, ListNode(2, ListNode(3, ListNode(3)))))))
