3e6ced231d1a7da6a25612e6f2349a2d1c56d7b8
kouta1612 <kouta1612world69@gmail.com>
2025-09-17 10:04:54 +0900

    リファクタリング

--- neetcode/python/_111/main.py
+++ neetcode/python/_111/main.py
@@ -10,12 +10,8 @@ class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        if not root.left and not root.right:
-            return 1
-        if not root.left:
-            return 1 + self.minDepth(root.right)
-        if not root.right:
-            return 1 + self.minDepth(root.left)
+        if not root.left or not root.right:
+            return 1 + max(self.minDepth(root.left), self.minDepth(root.right))
         return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
 
 print(Solution().minDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))))
