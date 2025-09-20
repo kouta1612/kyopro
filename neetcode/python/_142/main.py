0be75463e8bb02155407c180b5c373991d55fb08
kouta1612 <kouta1612world69@gmail.com>
2025-09-20 09:08:34 +0900

    142

---
+++ neetcode/python/_142/main.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        s = set()
+        while head:
+            if head in s:
+                return head
+            s.add(head)
+            head = head.next
+        return None
+
+print(Solution().detectCycle(ListNode(3, ListNode(2, ListNode(0, ListNode(-4))))))
