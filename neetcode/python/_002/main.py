a53276f5c17843073116402e89249e03b9f1e0cb
kouta1612 <kouta1612world69@gmail.com>
2025-09-22 09:34:41 +0900

    002

--- neetcode/python/_002/main.py
+++ neetcode/python/_002/main.py
@@ -7,22 +7,20 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        cur = dummy
+        dummy = cur = ListNode()
 
-        carry = 0
-        while l1 or l2 or carry:
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
-
-            sum = v1 + v2 + carry
-            carry = sum // 10
-            cur.next = ListNode(sum % 10)
-
+        one = 0
+        while l1 or l2 or one:
+            d1 = l1.val if l1 else 0
+            d2 = l2.val if l2 else 0
+            val = d1 + d2 + one
+            one = val // 10
+            val %= 10
+            cur.next = ListNode(val)
+            
+            cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            cur = cur.next
-
         return dummy.next
 
 
