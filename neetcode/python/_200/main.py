c884d3f41dc8f3fcfaa1b26f7a95575105367ed1
kouta1612 <kouta1612world69@gmail.com>
2025-09-24 09:40:39 +0900

    リファクタリング

--- neetcode/python/_200/main.py
+++ neetcode/python/_200/main.py
@@ -2,31 +2,25 @@ from typing import List
 
 class Solution:
     di, dj = [-1,0,1,0], [0,1,0,-1]
-    island = []
 
     def numIslands(self, grid: List[List[str]]) -> int:
-        for i in range(len(grid)):
-            vs = []
-            for j in range(len(grid[i])):
-                vs.append(grid[i][j] == "1")
-            self.island.append(vs)
         res = 0
-        for i in range(len(self.island)):
-            for j in range(len(self.island[i])):
-                if self.island[i][j]:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == "1":
                     res += 1
-                    self.dfs(i, j)
+                    self.dfs(i, j, grid)
         return res
 
-    def dfs(self, i, j: int):
-        self.island[i][j] = False
+    def dfs(self, i, j: int, grid: List[List[str]]):
+        grid[i][j] = "#"
         for k in range(4):
             ni, nj = i + self.di[k], j + self.dj[k]
-            if ni < 0 or nj < 0 or ni >= len(self.island) or nj >= len(self.island[ni]):
+            if ni < 0 or nj < 0 or ni >= len(grid) or nj >= len(grid[ni]):
                 continue
-            if not self.island[ni][nj]:
+            if not grid[ni][nj] == "1":
                 continue
-            self.dfs(ni, nj)
+            self.dfs(ni, nj, grid)
 
 print(Solution().numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
 print(Solution().numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]))
