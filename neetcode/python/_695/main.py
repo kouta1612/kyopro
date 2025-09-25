fd3f4e9677fc813c6076519c057429e57c6211c3
kouta1612 <kouta1612world69@gmail.com>
2025-09-25 14:32:47 +0900

    リファクタリング

--- neetcode/python/_695/main.py
+++ neetcode/python/_695/main.py
@@ -3,18 +3,19 @@ from typing import List
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         res = 0
+
+        def dfs(i, j: int) -> int:
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]):
+                return 0
+            if grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1 + dfs(i + 1, j) + dfs(i, j + 1) + dfs(i - 1, j) + dfs(i, j - 1)
+
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
-                    res = max(res, self.dfs(i, j, grid))
+                    res = max(res, dfs(i, j))
         return res
-
-    def dfs(self, i, j: int, grid: List[List[int]]) -> int:
-        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]):
-            return 0
-        if grid[i][j] == 0:
-            return 0
-        grid[i][j] = 0
-        return 1 + self.dfs(i + 1, j, grid) + self.dfs(i, j + 1, grid) + self.dfs(i - 1, j, grid) + self.dfs(i, j - 1, grid)
 
 print(Solution().maxAreaOfIsland([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]))
