fe05bb35c35ccf567688dfc003940b7520d01a13
kouta1612 <kouta1612world69@gmail.com>
2025-09-25 14:47:57 +0900

    102

---
+++ neetcode/python/_102/main.py
@@ -0,0 +1,30 @@
+from typing import Optional, List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        mapLevelToValue = dict()
+
+        def dfs(root: Optional[TreeNode], level: int):
+            if not root:
+                return
+            if level in mapLevelToValue:
+                mapLevelToValue[level].append(root.val)
+            else:
+                mapLevelToValue[level] = [root.val]
+            dfs(root.left, level + 1)
+            dfs(root.right, level + 1)
+
+        dfs(root, 0)
+        res = [[]] * len(mapLevelToValue)
+        print(mapLevelToValue)
+        for k, v in mapLevelToValue.items():
+            res[k] = v
+        return res
+        
+print(Solution().levelOrder(None))
