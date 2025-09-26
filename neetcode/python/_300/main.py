fad9283d7097f840a031db85c921ae742a584652
kouta1612 <kouta1612world69@gmail.com>
2025-09-26 22:25:21 +0900

    リファクタリング

--- neetcode/python/_300/main.py
+++ neetcode/python/_300/main.py
@@ -1,24 +1,15 @@
 from typing import List
+from bisect import bisect_left
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         res = []
 
-        def lower_bound(arr: List[int], v: int) -> int:
-            ng, ok = -1, len(arr)
-            while ng + 1 != ok:
-                mid = (ok + ng) // 2
-                if arr[mid] >= v:
-                    ok = mid
-                else:
-                    ng = mid
-            return ok
-
         for num in nums:
             if not res or (res and res[-1] < num):
                 res.append(num)
             else:
-                li = lower_bound(res, num)
+                li = bisect_left(res, num)
                 res[li] = num
         return len(res)
 
