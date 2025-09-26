beb549258f2ebc70212c103ac6ad8400d674f83d
kouta1612 <kouta1612world69@gmail.com>
2025-09-26 22:23:01 +0900

    リファクタリング

--- neetcode/python/_300/main.py
+++ neetcode/python/_300/main.py
@@ -2,11 +2,24 @@ from typing import List
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
+        res = []
+
+        def lower_bound(arr: List[int], v: int) -> int:
+            ng, ok = -1, len(arr)
+            while ng + 1 != ok:
+                mid = (ok + ng) // 2
+                if arr[mid] >= v:
+                    ok = mid
+                else:
+                    ng = mid
+            return ok
+
+        for num in nums:
+            if not res or (res and res[-1] < num):
+                res.append(num)
+            else:
+                li = lower_bound(res, num)
+                res[li] = num
+        return len(res)
 
 print(Solution().lengthOfLIS([10,9,2,5,3,7,101,18]))
