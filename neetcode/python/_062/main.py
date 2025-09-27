f2408cb9c3d4edc2388f94b3719dd78ce249761e
kouta1612 <kouta1612world69@gmail.com>
2025-09-27 11:46:10 +0900

    リファクタリング

--- neetcode/python/_062/main.py
+++ neetcode/python/_062/main.py
@@ -1,16 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n for i in range(m)]
-        dp[0][0] = 1
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    continue
-                if i == 0:
-                    dp[i][j] = dp[i][j-1]
-                elif j == 0:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        return dp[m-1][n-1]
+        dp = [[1] * n for i in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
 print(Solution().uniquePaths(3, 7))
