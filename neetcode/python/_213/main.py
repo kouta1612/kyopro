cf7b0cb99d1487bd33ddb6d71f399f0e7688ff00
kouta1612 <kouta1612world69@gmail.com>
2025-09-27 22:09:21 +0900

    リファクタリング

--- neetcode/python/_213/main.py
+++ neetcode/python/_213/main.py
@@ -2,19 +2,11 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 2:
-            return nums[0]
-        return max(self.helper(nums[:len(nums)-1]), self.helper(nums[1:]))
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:len(nums)-1]))
 
     def helper(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [[0] * 2 for i in range(n)]
-        dp[0][1] = nums[0]
-        for i in range(1, n):
-            for j in range(2):
-                if j == 0:
-                    dp[i][j] = max(dp[i-1][0], dp[i-1][1])
-                else:
-                    dp[i][j] = dp[i-1][0] + nums[i]
-        return max(dp[n-1][0], dp[n-1][1])
+        rob1, rob2 = 0, 0
+        for num in nums:
+            rob1, rob2 = rob2, max(num + rob1, rob2)
+        return rob2
 print(Solution().rob([1,2,3,1]))
