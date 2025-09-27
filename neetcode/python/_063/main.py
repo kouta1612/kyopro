cca965d026c40a540d2298c5df1b674c2d674680
kouta1612 <kouta1612world69@gmail.com>
2025-09-27 15:15:39 +0900

    リファクタリング

--- neetcode/python/_063/main.py
+++ neetcode/python/_063/main.py
@@ -3,19 +3,13 @@ from typing import List
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * n for i in range(m)]
+        dp = [0] * n
+        dp[0] = 1
         for i in range(m):
-            if obstacleGrid[i][0]:
-                break
-            dp[i][0] = 1
-        for i in range(n):
-            if obstacleGrid[0][i]:
-                break
-            dp[0][i] = 1
-        for i in range(1, m):
-            for j in range(1, n):
+            for j in range(n):
                 if obstacleGrid[i][j]:
-                    continue
-                dp[i][j] = dp[i-1][j] + dp[i][j-1]
-        return dp[-1][-1]
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j-1]
+        return dp[-1]
 print(Solution().uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]))
