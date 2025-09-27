3ee605f0d6bf479dcfc56948fd8f7242e323d694
kouta1612 <kouta1612world69@gmail.com>
2025-09-27 15:55:19 +0900

    リファクタリング

--- neetcode/python/_198/main.py
+++ neetcode/python/_198/main.py
@@ -2,12 +2,9 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) < 2:
-            return nums[0]
-        dp = [0] * len(nums)
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-        return dp[-1]
+        pre, cur = 0, 0
+        for num in nums:
+            pre, cur = cur, max(cur, pre + num)
+        return cur
 
 print(Solution().rob([1,2,3,1]))
