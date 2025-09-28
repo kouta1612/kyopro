36dc25f67d051ac4950633aa0ac7f2fdb29f98d6
kouta1612 <kouta1612world69@gmail.com>
2025-09-28 21:36:23 +0900

    153

---
+++ neetcode/python/_153/main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] <= nums[r]:
+                r = mid
+            else:
+                l = mid + 1
+        return nums[l]
+print(Solution().findMin([3,4,5,1,2]))
+print(Solution().findMin([4,5,6,7,0,1,2]))
+print(Solution().findMin([1]))
+print(Solution().findMin([2,1]))
+print(Solution().findMin([1,2]))
+print(Solution().findMin([2,1]))
