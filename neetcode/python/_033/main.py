23a90741dd45bee289a6c7b5dd0eb9dd99ae9b8f
kouta1612 <kouta1612world69@gmail.com>
2025-09-28 14:53:30 +0900

    033

---
+++ neetcode/python/_033/main.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+print(Solution().search([4,5,6,7,0,1,2], 0))
+print(Solution().search([4,5,6,7,0,1,2], 3))
+print(Solution().search([1], 0))
+print(Solution().search([1], 1))
+print(Solution().search([1], 2))
+print(Solution().search([1], 3))
+print(Solution().search([1], 4))
+print(Solution().search([1], 5))
