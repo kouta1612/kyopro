a70405fe0221792bce53e440942d0c8255fc3438
kouta1612 <kouta1612world69@gmail.com>
2025-09-29 22:17:51 +0900

    003

---
+++ neetcode/python/_003/main.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        j, res, uniqs = 0, 0, set()
+        for i in range(len(s)):
+            while j < len(s) and s[j] not in uniqs:
+                uniqs.add(s[j])
+                j += 1
+                res = max(res, j - i)
+            uniqs.remove(s[i])
+        return res
+
+print(Solution().lengthOfLongestSubstring("abcabcbb"))
