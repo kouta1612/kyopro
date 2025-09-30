b78c42ec77db4e1fe2c9b3437ae9ffc09fb67d9f
kouta1612 <kouta1612world69@gmail.com>
2025-09-30 14:24:15 +0900

    リファクタリング

--- neetcode/python/_078/main.py
+++ neetcode/python/_078/main.py
@@ -2,14 +2,16 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
         res = []
-        for bit in range(1<<n):
-            sub = []
-            for i in range(n):
-                if bit & 1<<i:
-                    sub.append(nums[i])
-            res.append(sub)
+        def helper(i: int, subs: List[int]):
+            if i == len(nums):
+                res.append(subs)
+                return
+            
+            helper(i + 1, subs)
+            helper(i + 1, subs + [nums[i]])
+        
+        helper(0, [])
         return res
 
 print(Solution().subsets([1,2,3]))
