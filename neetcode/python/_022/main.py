87f2b9678d055227ed49a03791a38130a65282bc
kouta1612 <kouta1612world69@gmail.com>
2025-09-30 20:44:03 +0900

    リファクタリング

--- neetcode/python/_022/main.py
+++ neetcode/python/_022/main.py
@@ -3,23 +3,18 @@ from typing import List
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        length = 2 * n
-        for bit in range(1<<length):
-            parenthesis = ""
-            for i in range(length):
-                parenthesis += "(" if bit & 1<<i else ")"
-            if self.helper(parenthesis):
-                res.append(parenthesis)
-        return res
 
-    def helper(self, parenthesis: str) -> bool:
-        stack = []
-        for p in parenthesis:
-            if len(stack) and p == ")" and stack[-1] == "(":
-                stack.pop()
-            else:
-                stack.append(p)
-        return len(stack) == 0
+        def helper(open, close: int, s: str):
+            if len(s) == n * 2:
+                res.append(s)
+                return
+            if open < n:
+                helper(open + 1, close, s + "(")
+            if open > close:
+                helper(open, close + 1, s + ")")
+        
+        helper(0, 0, "")
+        return res
 
 print(Solution().generateParenthesis(3))
 print(Solution().generateParenthesis(1))
