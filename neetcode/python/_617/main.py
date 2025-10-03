cebe96166cba670585da2bd566bba4c06429e758
kouta1612 <kouta1612world69@gmail.com>
2025-10-03 21:59:27 +0900

    リファクタリング

--- neetcode/python/_617/main.py
+++ neetcode/python/_617/main.py
@@ -8,7 +8,6 @@ class TreeNode:
 
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root1 and not root2: return None
         if not root1: return root2
         if not root2: return root1
 
