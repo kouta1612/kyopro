ea315f89d63e7ab7118bdb3c9accadae43a03d49
kouta1612 <kouta1612world69@gmail.com>
2025-10-03 21:58:51 +0900

    617

--- neetcode/python/_617/main.py
+++ neetcode/python/_617/main.py
@@ -8,13 +8,15 @@ class TreeNode:
 
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root1:
-            return root2
-        if not root2:
-            return root1
-        res = TreeNode(root1.val+root2.val)
-        res.left = self.mergeTrees(root1.left, root2.left)
-        res.right = self.mergeTrees(root1.right, root2.right)
+        if not root1 and not root2: return None
+        if not root1: return root2
+        if not root2: return root1
+
+        res = TreeNode(root1.val + root2.val)
+        left = self.mergeTrees(root1.left, root2.left)
+        right = self.mergeTrees(root1.right, root2.right)
+        res.left, res.right = left, right
+
         return res
 
 print(Solution().mergeTrees(TreeNode(1, TreeNode(3, TreeNode(5)), TreeNode(2)), TreeNode(2, TreeNode(1, None, TreeNode(4)), TreeNode(3, None, TreeNode(7)))))
