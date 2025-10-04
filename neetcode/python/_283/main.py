e65e0ac1b43020b8de93e81e4ad78a6b2140dc3a
kouta1612 <kouta1612world69@gmail.com>
2025-10-04 09:18:48 +0900

    283

--- neetcode/python/_283/main.py
+++ neetcode/python/_283/main.py
@@ -2,11 +2,19 @@ from typing import List
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        l = 0
-        for r in range(len(nums)):
-            if nums[r] != 0:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
+        if len(nums) == 1: return
+
+        j = 1
+        for i in range(len(nums)):
+            if nums[i] != 0: 
+                j = i + 1
+                continue
+            
+            while j < len(nums) and nums[j] == 0:
+                j += 1
+            if j == len(nums):
+                break
+            nums[i], nums[j] = nums[j], nums[i]
 
 print(Solution().moveZeroes([0,1,0,3,12]))
 print(Solution().moveZeroes([0]))
