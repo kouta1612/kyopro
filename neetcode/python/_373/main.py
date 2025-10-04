81b77e9a558a8762216867963f0bea3cac6dc080
kouta1612 <kouta1612world69@gmail.com>
2025-10-04 17:16:19 +0900

    373

--- neetcode/python/_373/main.py
+++ neetcode/python/_373/main.py
@@ -3,16 +3,16 @@ from typing import List
 
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        heap, res = [], []
+        h = []
         for i in range(min(len(nums1), k)):
-            heappush(heap, [nums1[i] + nums2[0], i, 0])
-
-        while len(heap) > 0 and k > 0:
-            s, i, j = heappop(heap)
+            heappush(h, [nums1[i] + nums2[0], i, 0])
+        
+        res = []
+        while len(h) > 0 and len(res) < k:
+            _, i, j = heappop(h)
             res.append([nums1[i], nums2[j]])
-            if j + 1 < len(nums2):
-                heappush(heap, [nums1[i] + nums2[j + 1], i, j + 1]) 
-            k -= 1
+            if j + 1 < len(nums2): heappush(h, [nums1[i] + nums2[j + 1], i, j + 1])
+        
         return res
 
 print(Solution().kSmallestPairs([1, 7, 11], [2, 4, 6], 3))
