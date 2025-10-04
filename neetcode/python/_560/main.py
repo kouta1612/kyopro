d247de6cbe56cf4606e7dc14411f5fb51c1f5549
kouta1612 <kouta1612world69@gmail.com>
2025-10-05 08:58:23 +0900

    560

--- neetcode/python/_560/main.py
+++ neetcode/python/_560/main.py
@@ -2,12 +2,16 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefixSum, counter, freq = 0, 0, {0: 1}
+        res, counters = 0, {0: 1}
+        sums = [0] * (len(nums) + 1)
+
         for i in range(len(nums)):
-            prefixSum += nums[i]
-            counter += freq.get(prefixSum - k, 0)
-            freq[prefixSum] = 1 + freq.get(prefixSum, 0)
-        return counter
+            sums[i + 1] = sums[i] + nums[i]
+            key = sums[i + 1] - k
+            res += counters.get(key, 0)
+            counters[sums[i + 1]] = counters.get(sums[i + 1], 0) + 1
+
+        return res
 
 print(Solution().subarraySum([1, 1, 1], 2))
 print(Solution().subarraySum([1, 2, 3], 3))
