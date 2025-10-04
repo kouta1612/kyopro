523eb55a7178b5dac0e6902f8fd133fced2a12cd
kouta1612 <kouta1612world69@gmail.com>
2025-10-04 16:00:30 +0900

    082

--- neetcode/python/_082/main.py
+++ neetcode/python/_082/main.py
@@ -7,16 +7,18 @@ class ListNode:
 
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = pre = ListNode(0, head)
-        cur = head
-        while cur:
-            if cur.next and cur.val == cur.next.val:
-                while cur.next and cur.val == cur.next.val:
-                    cur = cur.next
-                pre.next = cur.next
-            else:
-                pre = cur
+        dummy = ListNode(0, head)
+        pre, cur = dummy, head
+
+        while cur and cur.next:
+            if cur.val != cur.next.val:
+                pre, cur = cur, cur.next
+                continue
+            while cur and cur.next and cur.val == cur.next.val:
+                cur = cur.next
+            pre.next = cur.next
             cur = cur.next
+
         return dummy.next
 
 print(Solution().deleteDuplicates(ListNode(1, ListNode(2, ListNode(3, ListNode(3, ListNode(4, ListNode(4, ListNode(5)))))))))
