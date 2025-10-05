daed6e771573cce8be5cd0e5883a6519a3ceebd7
kouta1612 <kouta1612world69@gmail.com>
2025-10-05 16:52:26 +0900

    300

--- neetcode/python/_300/main.py
+++ neetcode/python/_300/main.py
@@ -4,13 +4,15 @@ from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         res = []
-
         for num in nums:
-            if not res or (res and res[-1] < num):
-                res.append(num)
-            else:
-                li = bisect_left(res, num)
-                res[li] = num
+            if res and res[-1] >= num:
+                l, r = 0, len(res) - 1
+                while l < r:
+                    mid = (l + r) // 2
+                    if res[mid] >= num: r = mid
+                    else: l = mid + 1
+                res[l] = num
+            else: res.append(num)
         return len(res)
 
 print(Solution().lengthOfLIS([10,9,2,5,3,7,101,18]))
