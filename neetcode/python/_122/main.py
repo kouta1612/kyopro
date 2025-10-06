27c4e1bc0ce53ba70990e59d7bd473a5f555ea04
kouta1612 <kouta1612world69@gmail.com>
2025-10-07 08:34:17 +0900

    リファクタリング

--- neetcode/python/_122/main.py
+++ neetcode/python/_122/main.py
@@ -2,10 +2,11 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        now, res = prices[0], 0
-        for price in prices:
-            if now < price:
-                res += price - now
-            now = price
-        return res
+        dp = [0] * (len(prices) + 1)
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                dp[i + 1] = dp[i] + prices[i] - prices[i-1]
+            else:
+                dp[i + 1] = dp[i]
+        return dp[len(prices)]
 print(Solution().maxProfit([7,1,5,3,6,4]))
