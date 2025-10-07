39e74e22631d9840f43dce898d3d50c3edc09736
kouta1612 <kouta1612world69@gmail.com>
2025-10-07 12:08:56 +0900

    139

--- neetcode/python/_139/main.py
+++ neetcode/python/_139/main.py
@@ -2,13 +2,13 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [False] * (len(s)+1)
+        dp = [False] * (len(s) + 1)
         dp[0] = True
         for i in range(len(s) + 1):
-            for w in wordDict:
-                if i + len(w) <= len(s) and s[i:i+len(w)] == w and dp[i]:
-                    dp[i+len(w)] = dp[i]
+            for word in wordDict:
+                if i - len(word) >= 0 and s[i-len(word):i] == word and dp[i-len(word)]:
+                    dp[i] = True
+                    break
         return dp[len(s)]
-
 
 print(Solution().wordBreak("leetcode", ["leet", "code"]))
