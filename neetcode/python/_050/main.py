1b5c5034b475b3084305e00a3003a33e83dbde7c
kouta1612 <kouta1612world69@gmail.com>
2025-10-09 20:03:06 +0900

    050

--- neetcode/python/_050/main.py
+++ neetcode/python/_050/main.py
@@ -1,15 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         def helper(x: float, n: int) -> float:
-            if n == 0:
-                return 1.0
-            if n % 2 == 1:
-                return x * helper(x, n - 1)
-            else:
+            if n == 0: return 1
+            if n % 2 == 0:
                 v = helper(x, n // 2)
                 return v * v
-        v = helper(x, abs(n))
-        return v if n >= 0 else 1 / v
+            return x * helper(x, n - 1)
+
+        sign = 1 if x >= 0 or abs(n) % 2 == 0 else -1
+        res = helper(abs(x), abs(n))
+        return res * sign if n >= 0 else sign * 1 / res
 
 print(Solution().myPow(2.00000, 10))
 print(Solution().myPow(2.10000, 3))
