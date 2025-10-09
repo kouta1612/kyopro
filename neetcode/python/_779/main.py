805c12ff1ce148576fb67bf07c3919279a831313
kouta1612 <kouta1612world69@gmail.com>
2025-10-09 21:27:33 +0900

    779

--- neetcode/python/_779/main.py
+++ neetcode/python/_779/main.py
@@ -1,10 +1,9 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        if n == 1:
-            return 0
-        if k % 2 == 0:
-            return 1 - self.kthGrammar(n - 1,  k // 2)
-        return self.kthGrammar(n - 1, (k + 1) // 2)
+        if n == 1: return 0
+        half = 1 << (n-2)
+        if k <= half: return self.kthGrammar(n-1, k)
+        return 1 - self.kthGrammar(n-1, k - half)
 
 print(Solution().kthGrammar(1, 1))
 print(Solution().kthGrammar(2, 1))
