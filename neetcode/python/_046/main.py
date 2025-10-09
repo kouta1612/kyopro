0336acc71e5dc713b7f45556df3bfa6e376c3346
kouta1612 <kouta1612world69@gmail.com>
2025-10-09 21:52:07 +0900

    046

--- neetcode/python/_046/main.py
+++ neetcode/python/_046/main.py
@@ -2,15 +2,20 @@ from typing import List
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        perms = [[]]
-        for num in nums:
-            newPerms = []
-            for p in perms:
-                for i in range(len(p) + 1):
-                    copyPerm = p[:]
-                    copyPerm.insert(i, num)
-                    newPerms.append(copyPerm)
-            perms = newPerms
-        return perms
+        res = []
+        def helper(perms: List[int], used: List[bool]):
+            if len(perms) == len(nums):
+                res.append(perms[:])
+                return
+            for i in range(len(nums)):
+                if used[i]: continue
+                perms.append(nums[i])
+                used[i] = True
+                helper(perms, used)
+                perms.pop()
+                used[i] = False
+        helper([], [False] * len(nums))
+        return res
+
 
 print(Solution().permute([1,2,3]))
