8ca55969b6f11fc69c0aebc03b04bb45549a5dcc
kouta1612 <kouta1612world69@gmail.com>
2025-10-10 23:22:07 +0900

    031

--- neetcode/python/_031/main.py
+++ neetcode/python/_031/main.py
@@ -2,27 +2,20 @@ from typing import List
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        i = -1
-        for j in range(len(nums) - 2, -1, -1):
-            if nums[j] < nums[j + 1]:
-                i = j
-                break
-        if i == -1:
-            nums.sort()
-            return
-
-        for j in range(len(nums) - 1, -1, -1):
-            if nums[i] < nums[j]:
-                nums[i], nums[j] = nums[j], nums[i]
-                break
-        l, r = i + 1, len(nums) - 1
-        while l < r:
-            nums[l], nums[r] = nums[r], nums[l]
-            l += 1
-            r -= 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i == 0: 
+                nums.sort()
+                return
+            if nums[i-1] < nums[i]:
+                for j in range(len(nums) - 1, -1, -1):
+                    if nums[i-1] < nums[j]:
+                        nums[i-1], nums[j] = nums[j], nums[i-1]
+                        break
+                j, k = i, len(nums) - 1
+                while j < k:
+                    nums[j], nums[k] = nums[k], nums[j]
+                    j, k = j + 1, k - 1
+                return
 
 print(Solution().nextPermutation([1,2,3]))
 print(Solution().nextPermutation([3,2,1]))
