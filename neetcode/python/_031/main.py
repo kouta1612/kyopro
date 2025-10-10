a07ff800b0897170b6118cf5f8e46a214094a95b
kouta1612 <kouta1612world69@gmail.com>
2025-10-10 23:42:23 +0900

    リファクタリング

--- neetcode/python/_031/main.py
+++ neetcode/python/_031/main.py
@@ -2,20 +2,26 @@ from typing import List
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        for i in range(len(nums) - 1, -1, -1):
-            if i == 0: 
-                nums.sort()
-                return
-            if nums[i-1] < nums[i]:
-                for j in range(len(nums) - 1, -1, -1):
-                    if nums[i-1] < nums[j]:
-                        nums[i-1], nums[j] = nums[j], nums[i-1]
-                        break
-                j, k = i, len(nums) - 1
-                while j < k:
-                    nums[j], nums[k] = nums[k], nums[j]
-                    j, k = j + 1, k - 1
-                return
+        i = -1
+        for j in range(len(nums) - 1, 0, -1):
+            if nums[j-1] < nums[j]:
+                i = j
+                break
+        if i == -1:
+            nums.sort()
+            return
+        
+        # nums[i:]の中でnums[i-1]よりも大きい要素のインデックスと値を交換
+        for j in range(len(nums) - 1, -1, -1):
+            if nums[i-1] < nums[j]:
+                nums[i-1], nums[j] = nums[j], nums[i-1]
+                break
+
+        # nums[i:]は降順ソートされている
+        j = len(nums) - 1
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i, j = i + 1, j - 1
 
 print(Solution().nextPermutation([1,2,3]))
 print(Solution().nextPermutation([3,2,1]))
