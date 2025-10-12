fe72943019415dc7820d473f10f0865aab5d2ed7
kouta1612 <kouta1612world69@gmail.com>
2025-10-12 14:06:45 +0900

    リファクタリング

--- neetcode/python/_213/main.py
+++ neetcode/python/_213/main.py
@@ -3,14 +3,10 @@ from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
         def helper(moneys: List[int]) -> int:
-            if not moneys: return 0
-            if len(moneys) == 1: return moneys[0]
-
-            dp = [0] * (len(moneys) + 1)
-            dp[1] = moneys[0]
-            for i in range(1, len(moneys)):
-                dp[i + 1] = max(dp[i], dp[i-1] + moneys[i])
-            return dp[len(moneys)]
+            rob1, rob2 = 0, 0
+            for money in moneys:
+                rob1, rob2 = max(rob2 + money, rob1), rob1
+            return rob1
         return max(nums[0], helper(nums[1:]), helper(nums[:-1]))
 
 print(Solution().rob([1,2,3,1]))
