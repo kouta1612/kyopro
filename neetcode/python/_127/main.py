f854a5052a03f86693e8c6d5b3d5c7b19e775c54
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 08:36:09 +0900

    127

--- neetcode/python/_127/main.py
+++ neetcode/python/_127/main.py
@@ -3,17 +3,16 @@ from collections import deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        if endWord not in wordList: return 0
-
-        wordSet, seen, q, res = set(wordList), set(), deque([beginWord]), 1
-        while len(q) > 0:
+        res, wordSet, seen = 1, set(wordList), set()
+        q = deque([beginWord])
+        while q:
             size = len(q)
             for _ in range(size):
-                v = q.popleft()
-                if v == endWord: return res
-                for i in range(len(v)):
+                now = q.popleft()
+                if now == endWord: return res
+                for i in range(len(now)):
                     for o in range(ord('a'), ord('z') + 1):
-                        s = v[:i] + chr(o) + v[i+1:]
+                        s = now[:i] + chr(o) + now[i + 1:]
                         if s in wordSet and s not in seen:
                             q.append(s)
                             seen.add(s)
