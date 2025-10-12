4f3447e4d1f074fa674218864376fe3e2c784b9a
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 08:13:34 +0900

    039

--- neetcode/python/_039/main.py
+++ neetcode/python/_039/main.py
@@ -2,15 +2,21 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[] for _ in range(target + 1)]
-        dp[0] = [[]]
-        for c in candidates:
-            for t in range(c, target + 1):
-                for comb in dp[t-c]:
-                    newComb = comb[:]
-                    newComb.append(c)
-                    dp[t].append(newComb)
-        return dp[target]
+        res = []
+        def helper(i, sum: int, comb: List[int]):
+            if sum == target:
+                res.append(comb[:])
+                return
+            if sum > target: return
+            if i >= len(candidates): return
+
+            helper(i + 1, sum, comb)
+
+            comb.append(candidates[i])
+            helper(i, sum + candidates[i], comb)
+            comb.pop()
+        helper(0, 0, [])
+        return res
 
 print(Solution().combinationSum([2,3,6,7], 7))
 print(Solution().combinationSum([2,3,5], 8))
