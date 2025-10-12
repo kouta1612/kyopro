f784df3a06ee572d55229877e6849f18f66ba15b
kouta1612 <kouta1612world69@gmail.com>
2025-10-12 22:01:38 +0900

    リファクタリング

--- neetcode/python/_033/main.py
+++ neetcode/python/_033/main.py
@@ -6,13 +6,14 @@ class Solution:
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] == target: return mid
-            if nums[mid] >= nums[l]:
-                if nums[l] <= target <= nums[mid]: r = mid
-                else: l = mid + 1
-            else: 
-                if nums[mid] <= target <= nums[r]: l = mid
+            if nums[mid] <= nums[r]:
+                if nums[mid] < target <= nums[r]: l = mid + 1
                 else: r = mid - 1
+            else:
+                if nums[l] <= target < nums[mid]: r = mid - 1
+                else: l = mid + 1
         return -1
+
 print(Solution().search([4,5,6,7,0,1,2], 0))
 print(Solution().search([4,5,6,7,0,1,2], 3))
 print(Solution().search([1], 0))
