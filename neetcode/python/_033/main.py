27f2f412658d2fc205ec44a93e54c028ebb3a9b2
kouta1612 <kouta1612world69@gmail.com>
2025-10-12 21:37:01 +0900

    033

--- neetcode/python/_033/main.py
+++ neetcode/python/_033/main.py
@@ -5,18 +5,13 @@ class Solution:
         l, r = 0, len(nums) - 1
         while l <= r:
             mid = (l + r) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[l] <= nums[mid]:
-                if nums[l] <= target < nums[mid]:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            else:
-                if nums[mid] < target <= nums[r]:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+            if nums[mid] == target: return mid
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target <= nums[mid]: r = mid
+                else: l = mid + 1
+            else: 
+                if nums[mid] <= target <= nums[r]: l = mid
+                else: r = mid - 1
         return -1
 print(Solution().search([4,5,6,7,0,1,2], 0))
 print(Solution().search([4,5,6,7,0,1,2], 3))
