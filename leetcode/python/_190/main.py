d0a0ebbcd878e8bf4004a732ae5492641e6604ef
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 17:09:33 +0900

    190

--- leetcode/python/_190/main.py
+++ leetcode/python/_190/main.py
@@ -1,9 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-        for i in range(32):
-            if n & 1<<i:
-                res += 1<<(31-i)
+        for i in range(16):
+            if 1<<i & n > 0: res += 1<<(31-i)
+            if 1<<(31-i) & n > 0: res += 1<<i
         return res
 
 print(Solution().reverseBits(43261596))
