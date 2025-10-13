8e25f4ac593b42c965b374f82f058f485a65e97d
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 11:38:58 +0900

    206

--- leetcode/python/_206/main.py
+++ leetcode/python/_206/main.py
@@ -8,12 +8,10 @@ class ListNode:
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         pre, cur = None, head
-
         while cur:
             tmp = cur.next
             cur.next = pre
             pre, cur = cur, tmp
-
         return pre
 
 print(Solution().reverseList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))))
