6745c3c0e2b7b8e225ec83cfd37d4366d4262406
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 09:42:58 +0900

    242

---
+++ neetcode/python/_242/main.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+print(Solution().isAnagram("anagram", "nagaram"))
+print(Solution().isAnagram("rat", "car"))
