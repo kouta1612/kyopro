ffee69ce624c947251deac67f8ecdc62544fd1f9
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 10:49:14 +0900

    リファクタリング

--- leetcode/python/_125/main.py
+++ leetcode/python/_125/main.py
@@ -1,15 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alnum = ""
-        for c in s:
-            if c.isalnum(): alnum += c
-        alnum = alnum.lower()
-
-        i, j = 0, len(alnum) - 1
-        while i < j:
-            if alnum[i] != alnum[j]: return False
-            i += 1
-            j -= 1
-        return True
+        s = ''.join(c.lower() for c in s if c.isalnum())
+        return s == s[::-1]
 
 print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
