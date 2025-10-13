6123cc898c05be3555289c12a8b86a8de34e2822
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 11:14:22 +0900

    リファクタリング

--- leetcode/python/_020/main.py
+++ leetcode/python/_020/main.py
@@ -4,7 +4,7 @@ class Solution:
         mp = {')': '(', '}': '{', ']': '['}
         for c in s:
             if c in mp:
-                if stack and stack[-1] == mp[c]: stack = stack[:-1]
+                if stack and stack[-1] == mp[c]: stack.pop()
                 else: return False
             else:
                 stack.append(c)
