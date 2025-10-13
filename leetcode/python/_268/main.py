f0da08d0a4599eb863d3d98105f42e71c33e5b65
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 17:42:15 +0900

    リファクタリング

--- leetcode/python/_268/main.py
+++ leetcode/python/_268/main.py
@@ -2,11 +2,9 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        res = 0
-        for i in range(len(nums) + 1):
-            res ^= i
-        for num in nums:
-            res ^= num
+        res = len(nums)
+        for i, num in enumerate(nums):
+            res ^= i ^ num
         return res
 
 print(Solution().missingNumber([3,0,1]))
