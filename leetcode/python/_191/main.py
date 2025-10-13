c09930b067768a1975195e504b1b1625a896af32
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 16:14:00 +0900

    リファクタリング

--- leetcode/python/_191/main.py
+++ leetcode/python/_191/main.py
@@ -1,8 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         res = 0
-        for i in range(32):
-            if n & 1<<i > 0: res += 1
+        while n > 0:
+            n &= n - 1
+            res += 1
         return res
 
 print(Solution().hammingWeight(11))
