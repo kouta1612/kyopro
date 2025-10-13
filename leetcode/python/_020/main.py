bcb735ed3f8af85047a9805d9a6af4c9cec4308f
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 11:10:53 +0900

    020

--- leetcode/python/_020/main.py
+++ leetcode/python/_020/main.py
@@ -1,13 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        mp = {'}': '{', ']': '[', ')': '(', '(': None, '[': None, '{': None}
-        for char in s:
-            if stack and mp[char] == stack[-1]:
-                stack.pop()
+        mp = {')': '(', '}': '{', ']': '['}
+        for c in s:
+            if c in mp:
+                if stack and stack[-1] == mp[c]: stack = stack[:-1]
+                else: return False
             else:
-                stack.append(char)
-        return len(stack) == 0
+                stack.append(c)
+        return not stack
 
 print(Solution().isValid("()"))
 print(Solution().isValid("()[]{}"))
