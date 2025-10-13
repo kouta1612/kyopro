e2d7782e2d1512a7c272b27ff1cb816b95c353cc
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 11:01:40 +0900

    121

--- leetcode/python/_121/main.py
+++ leetcode/python/_121/main.py
@@ -2,10 +2,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy, profit = prices[0], 0
-        for i in range(1, len(prices)):
-            profit = max(profit, prices[i] - buy)
-            buy = min(buy, prices[i])
-        return profit
+        res, buy = 0, prices[0]
+        for price in prices:
+            res = max(res, price - buy)
+            buy = min(buy, price)
+        return res
 
 print(Solution().maxProfit([7,1,5,3,6,4]))
