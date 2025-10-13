ac997633214cbf324fc338717484b4b7a0599825
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 12:10:58 +0900

    リファクタリング

--- leetcode/python/_021/main.py
+++ leetcode/python/_021/main.py
@@ -12,19 +12,13 @@ class ListNode:
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        cur = dummy
-        
-        while list1 and list2:
-            if list1.val <= list2.val:
-                cur.next = list1
-                list1 = list1.next
-            else:
-                cur.next = list2
-                list2 = list2.next
-            cur = cur.next
-        cur.next = list1 or list2
+        if not list1 or not list2: return list1 or list2
 
-        return dummy.next
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+
+        list2.next = self.mergeTwoLists(list1, list2.next)
+        return list2        
 
 print(Solution().mergeTwoLists(ListNode(1, ListNode(2, ListNode(4))), ListNode(1, ListNode(3, ListNode(4)))))
