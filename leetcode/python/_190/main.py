08f9f2474886bdbc21104ff219dbfae6d01c20bc
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 17:19:06 +0900

    リファクタリング

--- leetcode/python/_190/main.py
+++ leetcode/python/_190/main.py
@@ -1,9 +1,9 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-        for i in range(16):
-            if 1<<i & n > 0: res += 1<<(31-i)
-            if 1<<(31-i) & n > 0: res += 1<<i
+        for i in range(32):
+            res = (res<<1) | (n & 1)
+            n >>= 1
         return res
 
 print(Solution().reverseBits(43261596))
