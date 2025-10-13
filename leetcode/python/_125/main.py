98d3c4805b6629e7bc16bbfdb9c1d431bc238573
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 10:28:24 +0900

    125

--- leetcode/python/_125/main.py
+++ leetcode/python/_125/main.py
@@ -1,18 +1,16 @@
-import re
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        i,j = 0, len(s)-1
+        alnum = ""
+        for c in s:
+            if c.isalnum(): alnum += c
+        alnum = alnum.lower()
+        alnum.replace(" ", "")
+
+        i, j = 0, len(alnum) - 1
         while i < j:
-            while i < j and not s[i].isalnum():
-                i += 1
-            while i < j and not s[j].isalnum():
-                j -= 1
-            if s[i].lower() != s[j].lower():
-                return False
-            i, j = i + 1, j - 1
+            if alnum[i] != alnum[j]: return False
+            i += 1
+            j -= 1
         return True
-            
-            
 
 print(Solution().isPalindrome("A man, a plan, a canal: Panama"))
