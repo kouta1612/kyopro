864e8fad773299533087d4fa64803c28f8c61c4c
kouta1612 <kouta1612world69@gmail.com>
2025-10-13 19:45:27 +0900

    049

--- leetcode/python/_049/main.py
+++ leetcode/python/_049/main.py
@@ -2,10 +2,14 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = dict()
+        wordDict = dict()
         for s in strs:
-            v = ''.join(sorted(s))
-            mp[v] = mp.get(v, []) + [s]
-        return list(mp.values())
+            anagram = ''.join(sorted(s))
+            if anagram not in wordDict: wordDict[anagram] = []
+            wordDict[anagram].append(s)
+        res = []
+        for v in wordDict.values():
+            res.append(v)
+        return res
 
 print(Solution().groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
