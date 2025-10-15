ba7b14ac085396d70b7660ec84ec20388780e998
kouta1612 <kouta1612world69@gmail.com>
2025-10-15 20:41:49 +0900

    リファクタリング

--- leetcode/python/_015/main.py
+++ leetcode/python/_015/main.py
@@ -6,14 +6,15 @@ class Solution:
         nums.sort()
         print(nums)
         for i in range(len(nums)-2):
-            if i - 1 >= 0 and nums[i-1] == nums[i]: continue
+            if i > 0 and nums[i] == nums[i-1]: continue
             j, k = i + 1, len(nums) - 1
             while j < k:
                 total = nums[i] + nums[j] + nums[k]
                 if total == 0:
                     res.append([nums[i], nums[j], nums[k]])
                     j += 1
-                    while j < k and nums[j - 1] == nums[j]: j += 1
+                    while j < k and nums[j] == nums[j-1]: j += 1
+                    while j < k and k < len(nums) - 1 and nums[k] == nums[k+1]: k -= 1
                 elif total > 0:
                     k -= 1
                 else:
