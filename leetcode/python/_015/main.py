dfbf471eae989e88bba378fde79280a132948497
kouta1612 <kouta1612world69@gmail.com>
2025-10-15 13:36:45 +0900

    015

---
+++ leetcode/python/_015/main.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        nums.sort()
+        print(nums)
+        for i in range(len(nums)-2):
+            if i - 1 >= 0 and nums[i-1] == nums[i]: continue
+            j, k = i + 1, len(nums) - 1
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+                if total == 0:
+                    res.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                elif total > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return list(res)
+
+print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
+print(Solution().threeSum([-4, -1, -1, 0, 1, 2]))
