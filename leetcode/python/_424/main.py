f449bf97ba872bfc35b9f241d2ae87c0abcf7bf9
kouta1612 <kouta1612world69@gmail.com>
2025-10-16 22:48:03 +0900

    リファクタリング

--- leetcode/python/_424/main.py
+++ leetcode/python/_424/main.py
@@ -2,21 +2,15 @@ from collections import defaultdict
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        dic = defaultdict(int)
-        j, res = 0, 0
+        counter = defaultdict(int)
+        j, res, maxCount = 0, 0, 0
         for i in range(len(s)):
-            while j < len(s):
-                dic[s[j]] += 1
-                maxNum = 0
-                for num in dic.values():
-                    maxNum = max(maxNum, num)
-                remain = (j - i + 1) - maxNum
-                if remain <= k: j += 1
-                else:
-                    dic[s[j]] -= 1
-                    break
+            while j < len(s) and (j - i + 1) - max(maxCount, counter[s[j]] + 1) <= k:
+                counter[s[j]] += 1
+                maxCount = max(maxCount, counter[s[j]])
+                j += 1
             res = max(res, j - i)
-            dic[s[i]] -= 1
+            counter[s[i]] -= 1
         return res
 
 print(Solution().characterReplacement("ABAB", 2))
