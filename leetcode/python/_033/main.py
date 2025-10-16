2e6cf9e045a1c9433c0c750f1d1ba19f795452ee
kouta1612 <kouta1612world69@gmail.com>
2025-10-17 08:30:01 +0900

    033

--- leetcode/python/_033/main.py
+++ leetcode/python/_033/main.py
@@ -6,12 +6,12 @@ class Solution:
         while l <= r:
             mid = (l + r) // 2
             if nums[mid] == target: return mid
-            if nums[mid] <= nums[r]:
-                if nums[mid] < target <= nums[r]: l = mid + 1
-                else: r = mid - 1
-            else:
-                if nums[l] <= target < nums[mid]: r = mid - 1
+            if nums[mid] >= nums[l]:
+                if nums[l] <= target <= nums[mid]: r = mid - 1
                 else: l = mid + 1
+            else:
+                if nums[mid] <= target <= nums[r]: l = mid + 1
+                else: r = mid - 1
         return -1
 
 print(Solution().search([4,5,6,7,0,1,2], 0))
