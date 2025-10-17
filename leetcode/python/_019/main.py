b6c2fb9dc7225bc3eaf2b99d7d65eb7b76bad15f
kouta1612 <kouta1612world69@gmail.com>
2025-10-18 08:56:34 +0900

    019

--- leetcode/python/_019/main.py
+++ leetcode/python/_019/main.py
@@ -5,26 +5,24 @@ class ListNode:
         self.val = val
         self.next = next
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        cur = head
-        num = 0
-        while cur:
-            cur = cur.next
-            num += 1
-        pre, cur = ListNode(), head
+        dummy = ListNode(0, head)
+        left, right = dummy, head
 
-        move = num-n
-        for i in range(move):
-            pre = cur
-            cur = cur.next
+        for _ in range(n):
+            right = right.next
+        
+        while right:
+            left, right = left.next, right.next
+        
+        left.next = left.next.next
 
-        if cur == head:
-            head = head.next
-        else:
-            pre.next = cur.next
-            cur = None
-
-        return head
+        return dummy.next
 
 print(Solution().removeNthFromEnd(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2))
