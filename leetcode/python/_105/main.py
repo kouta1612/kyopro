00b58df61f17129ca3612650f3c9fb287be58093
kouta1612 <kouta1612world69@gmail.com>
2025-10-19 15:22:53 +0900

    105

--- leetcode/python/_105/main.py
+++ leetcode/python/_105/main.py
@@ -9,11 +9,11 @@ class TreeNode:
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder: return None
-        
+
         res = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        res.left = self.buildTree(preorder[1:1+mid], inorder[:mid])
-        res.right = self.buildTree(preorder[1+mid:], inorder[mid + 1:])
+        idx = inorder.index(preorder[0])
+        res.left = self.buildTree(preorder[1:1 + idx], inorder[:idx])
+        res.right = self.buildTree(preorder[1 + idx:], inorder[idx + 1:])
         return res
 
 print(Solution().buildTree([3,9,20,15,7], [9,3,15,20,7]))
