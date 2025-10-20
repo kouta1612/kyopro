023194b567b9f7a24c820ff9cdd9db7b5842fd64
kouta1612 <kouta1612world69@gmail.com>
2025-10-20 21:03:49 +0900

    039

--- leetcode/python/_039/main.py
+++ leetcode/python/_039/main.py
@@ -2,15 +2,19 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        dp = [[] for _ in range(target + 1)]
-        dp[0] = [[]]
-        for c in candidates:
-            for i in range(c, target + 1):
-                for comb in dp[i - c]:
-                    newComb = comb[:]
-                    newComb.append(c)
-                    dp[i].append(newComb)
-        return dp[target]
+        self.res = []
+        def helper(i, total: int, combs: List[int]):
+            if i >= len(candidates): return
+            if total > target: return
+            if total == target:
+                self.res.append(combs[:])
+                return
+            for j in range(i, len(candidates)):
+                combs.append(candidates[j])
+                helper(j, total + candidates[j], combs)
+                combs.pop()
+        helper(0, 0, [])
+        return self.res
 
 print(Solution().combinationSum([2,3,6,7], 7))
 print(Solution().combinationSum([2,3,5], 8))
