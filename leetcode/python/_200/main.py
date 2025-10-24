512aea804fa31fd9ccb802e8faf15a7746a7b383
kouta1612 <kouta1612world69@gmail.com>
2025-10-24 11:30:56 +0900

    200

--- leetcode/python/_200/main.py
+++ leetcode/python/_200/main.py
@@ -1,26 +1,23 @@
 from typing import List
 
 class Solution:
-    di, dj = [-1,0,1,0], [0,1,0,-1]
-
     def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        def dfs(r, c: int):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS: return
+            if grid[r][c] == "0": return
+            grid[r][c] = "0"
+            dfs(r+1,c)
+            dfs(r,c-1)
+            dfs(r-1,c)
+            dfs(r,c+1)
         res = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
+        for i in range(ROWS):
+            for j in range(COLS):
                 if grid[i][j] == "1":
+                    dfs(i, j)
                     res += 1
-                    self.dfs(i, j, grid)
         return res
-
-    def dfs(self, i, j: int, grid: List[List[str]]):
-        grid[i][j] = "#"
-        for k in range(4):
-            ni, nj = i + self.di[k], j + self.dj[k]
-            if ni < 0 or nj < 0 or ni >= len(grid) or nj >= len(grid[ni]):
-                continue
-            if not grid[ni][nj] == "1":
-                continue
-            self.dfs(ni, nj, grid)
 
 print(Solution().numIslands([["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]))
 print(Solution().numIslands([["1","1","0","0","0"],["1","1","0","0","0"],["0","0","1","0","0"],["0","0","0","1","1"]]))
