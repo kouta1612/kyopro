7223cc7b519a3fb206c366fe9152f4ba5ef4d26b
kouta1612 <kouta1612world69@gmail.com>
2025-10-24 11:33:04 +0900

    リファクタリング

--- leetcode/python/_200/main.py
+++ leetcode/python/_200/main.py
@@ -3,14 +3,14 @@ from typing import List
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
+        dx, dy = [1, 0, 0, -1], [0, 1, -1, 0]
         def dfs(r, c: int):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS: return
             if grid[r][c] == "0": return
             grid[r][c] = "0"
-            dfs(r+1,c)
-            dfs(r,c-1)
-            dfs(r-1,c)
-            dfs(r,c+1)
+            for i in range(4):
+                nr, nc = r + dx[i], c + dy[i]
+                dfs(nr,nc)
         res = 0
         for i in range(ROWS):
             for j in range(COLS):
