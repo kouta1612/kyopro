ec9c4e5e430746a6bc457cdd3c12b531d0d7c524
kouta1612 <kouta1612world69@gmail.com>
2025-10-26 13:12:22 +0900

    リファクタリング

--- leetcode/python/_091/main.py
+++ leetcode/python/_091/main.py
@@ -1,14 +1,11 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        digitSet = set()
-        for c in range(1, 27):
-            digitSet.add(str(c))
         n = len(s)
         dp = [0] * (n + 1)
         dp[0] = 1
         for i in range(1, n + 1):
-            if s[i - 1] in digitSet: dp[i] += dp[i-1]
-            if i - 2 >= 0 and s[i-2:i] in digitSet: dp[i] += dp[i - 2]
+            if s[i - 1] != '0': dp[i] += dp[i-1]
+            if i - 2 >= 0 and '10' <= s[i-2:i] <= '26': dp[i] += dp[i - 2]
         return dp[n]
 
 print(Solution().numDecodings("12"))
