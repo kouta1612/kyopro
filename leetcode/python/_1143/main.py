2e90142ccc1f49dcf325426ef7a4cc201978b43f
kouta1612 <kouta1612world69@gmail.com>
2025-10-27 22:28:27 +0900

    リファクタリング

--- leetcode/python/_1143/main.py
+++ leetcode/python/_1143/main.py
@@ -5,10 +5,9 @@ class Solution:
         for i in range(1, ROWS+1):
             for j in range(1, COLS+1):
                 if text1[i-1] == text2[j-1]:
-                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1)
+                    dp[i][j] = dp[i-1][j-1] + 1
                 else:
-                    dp[i][j] = max(dp[i][j], dp[i-1][j])
-                    dp[i][j] = max(dp[i][j], dp[i][j-1])
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         return dp[ROWS][COLS]
 
 print(Solution().longestCommonSubsequence("abcde", "ace"))
