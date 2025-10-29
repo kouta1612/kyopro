caffe72edef34ae3806bd2e74ee53342e4577db6
kouta1612 <kouta1612world69@gmail.com>
2025-10-29 19:08:48 +0900

    リファクタリング

--- leetcode/python/_056/main.py
+++ leetcode/python/_056/main.py
@@ -10,8 +10,6 @@ class Solution:
             elif newInterval[1] < intervals[i][0]:
                 res.append(newInterval[:])
                 newInterval = intervals[i][:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         res.append(newInterval)
