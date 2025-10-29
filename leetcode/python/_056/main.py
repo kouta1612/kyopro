23e918f682f1fb1a328c13fa6ddae78b47e0f371
kouta1612 <kouta1612world69@gmail.com>
2025-10-29 19:16:34 +0900

    リファクタリング

--- leetcode/python/_056/main.py
+++ leetcode/python/_056/main.py
@@ -4,15 +4,11 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
         res = []
-        newInterval = []
         for i in range(len(intervals)):
-            if not newInterval: newInterval = intervals[i][:]
-            elif newInterval[1] < intervals[i][0]:
-                res.append(newInterval[:])
-                newInterval = intervals[i][:]
+            if not res or res[-1][1] < intervals[i][0]: 
+                res.append([intervals[i][0], intervals[i][1]])
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-        res.append(newInterval)
+                res[-1] = [min(res[-1][0], intervals[i][0]), max(res[-1][1], intervals[i][1])]
         return res
 
 print(Solution().merge([[1,3],[2,6],[8,10],[15,18]]))
