0094415cea1a08c8636d14c334d967cfdc13eda5
kouta1612 <kouta1612world69@gmail.com>
2025-10-30 22:17:51 +0900

    048

--- leetcode/python/_048/main.py
+++ leetcode/python/_048/main.py
@@ -2,17 +2,13 @@ from typing import List
 
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        l, r = 0, len(matrix) - 1
-        while l < r:
-            t, b = l, r
-            for i in range(r - l):
-                tmp = matrix[t][l+i]
-                matrix[t][l+i] = matrix[b-i][l]
-                matrix[b-i][l] = matrix[b][r-i]
-                matrix[b][r-i] = matrix[t+i][r]
-                matrix[t+i][r] = tmp
-            l += 1
-            r -= 1
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
+
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Solution().rotate(matrix)
