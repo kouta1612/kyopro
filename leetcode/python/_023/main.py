84fc3de991b42583ef1ab7c29390b1a31ebeaccb
kouta1612 <kouta1612world69@gmail.com>
2025-11-01 17:45:28 +0900

    リファクタリング

--- leetcode/python/_023/main.py
+++ leetcode/python/_023/main.py
@@ -20,10 +20,10 @@ class Solution:
         cur = dummy = ListNode(0)
         while l1 and l2:
             if l1.val < l2.val:
-                cur.next = ListNode(l1.val)
+                cur.next = l1
                 l1 = l1.next
             else:
-                cur.next = ListNode(l2.val)
+                cur.next = l2
                 l2 = l2.next
             cur = cur.next
         if l1: cur.next = l1
