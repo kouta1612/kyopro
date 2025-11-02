c6a9338d9e4ea5e0ca7f62b4371337f8ad0cb9bc
kouta1612 <kouta1612world69@gmail.com>
2025-11-03 01:41:02 +0900

    リファクタリング

--- leetcode/python/_238/main.py
+++ leetcode/python/_238/main.py
@@ -2,15 +2,18 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left, right = [1] * (len(nums) + 1), [1] * (len(nums) + 1)
+        res = [1] * len(nums)
+
+        left = 1
         for i in range(len(nums)):
-            left[i + 1] = left[i] * nums[i]
-        for i in range(len(nums) - 1, -1, -1):
-            right[i] = right[i + 1] * nums[i]
-        
-        res = []
-        for i in range(len(nums)):
-            res.append(left[i] * right[i + 1])
+            res[i] *= left
+            left *= nums[i]
+
+        right = 1
+        for i in reversed(range(len(nums))):
+            res[i] *= right
+            right *= nums[i]
+
         return res
 
 print(Solution().productExceptSelf([1, 2, 3, 4]))
