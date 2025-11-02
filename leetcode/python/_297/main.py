9eb258b7996858374f4be7c564d99a0487083651
kouta1612 <kouta1612world69@gmail.com>
2025-11-02 11:06:07 +0900

    297

---
+++ leetcode/python/_297/main.py
@@ -0,0 +1,37 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        res = []
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        vals = data.split(',')
+        self.i = 0
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            res = TreeNode(int(vals[self.i]))
+            self.i += 1
+            res.left = dfs()
+            res.right = dfs()
+            return res
+        return dfs()
+
+print(Codec().serialize(TreeNode(1, TreeNode(2), TreeNode(3))))
+print(Codec().deserialize(Codec().serialize(TreeNode(1, TreeNode(2), TreeNode(3)))))
+print(Codec().serialize(TreeNode(1, TreeNode(2), TreeNode(3))))
