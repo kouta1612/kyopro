3e3363f21baa0349a91235dfda0ad6451579ed8e
kouta1612 <kouta1612world69@gmail.com>
2025-11-03 19:23:31 +0900

    011

--- leetcode/python/_011/main.py
+++ leetcode/python/_011/main.py
@@ -2,10 +2,10 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        res = 0
         l, r = 0, len(height) - 1
+        res = 0
         while l < r:
-            res = max(res, (r-l) * min(height[l], height[r]))
+            res = max(res, (r - l) * min(height[l], height[r]))
             if height[l] <= height[r]: l += 1
             else: r -= 1
         return res
