71db1b8a9f510c827d35ba2bd35274bced9cfde0
kouta1612 <kouta1612world69@gmail.com>
2025-11-04 19:57:06 +0900

    143

--- leetcode/python/_143/main.py
+++ leetcode/python/_143/main.py
@@ -5,25 +5,31 @@ class ListNode:
         self.val = val
         self.next = next
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        slow, fast = head, head.next
-        while fast and fast.next:
-            slow, fast = slow.next, fast.next.next
-        
-        second = slow.next
-        slow.next = None
+        first, second = head, head.next
+        while second and second.next:
+            first, second = first.next, second.next.next
+        tmp = first.next
+        first.next = None
+        first, second = head, tmp
+
         pre = None
         while second:
             tmp = second.next
             second.next = pre
             pre, second = second, tmp
+        second = pre
 
-        first, second = head, pre
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next, second.next = second, tmp1
-            first, second = tmp1, tmp2
-
+        while first and second:
+            tmp, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp
+            first, second = tmp, tmp2
 
 print(Solution().reorderList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))))
