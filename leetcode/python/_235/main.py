19ab07d8cf1b21399569e9c6623a9eea59a356b8
kouta1612 <kouta1612world69@gmail.com>
2025-11-05 22:32:33 +0900

    リファクタリング

--- leetcode/python/_235/main.py
+++ leetcode/python/_235/main.py
@@ -7,8 +7,9 @@ class TreeNode:
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if p.val > q.val: p, q = q, p
-        if p.val <= root.val <= q.val: return root
-        if q.val < root.val: return self.lowestCommonAncestor(root.left, p, q)
-        return self.lowestCommonAncestor(root.right, p, q)
+        while root:
+            if p.val <= root.val <= q.val: return root
+            elif q.val < root.val: root = root.left
+            else: root = root.right
 
 print(Solution().lowestCommonAncestor(TreeNode(6), TreeNode(2), TreeNode(8)))
