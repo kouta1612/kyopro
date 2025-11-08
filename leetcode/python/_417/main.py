3ee2547c68ef4857ae97f3bc64f2109788171612
kouta1612 <kouta1612world69@gmail.com>
2025-11-08 21:09:53 +0900

    417

--- leetcode/python/_417/main.py
+++ leetcode/python/_417/main.py
@@ -2,23 +2,29 @@ from typing import List
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific, atlantic = set(), set()
-        vecs = [(0,1),(0,-1),(1,0),(-1,0)]
         ROWS, COLS = len(heights), len(heights[0])
-        def dfs(r, c: int, seen):
-            seen.add((r, c))
-            for dr, dc in vecs:
-                nr, nc = r + dr, c + dc
-                if not (0 <= nr < ROWS and 0 <= nc < COLS): continue
-                if (nr, nc) in seen: continue
-                if heights[nr][nc] < heights[r][c]: continue
-                dfs(nr, nc, seen)
+        pat, atl = set(), set()
+
+        def dfs(r, c: int, visited: set, prev: int):
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or
+                (r, c) in visited or heights[r][c] < prev):
+                return
+            visited.add((r, c))
+            for nr, nc in [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]:
+                dfs(nr, nc, visited, heights[r][c])
+
         for r in range(ROWS):
-            dfs(r, 0, pacific)
-            dfs(r, COLS-1, atlantic)
+            dfs(r, 0, pat, heights[r][0])
+            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
         for c in range(COLS):
-            dfs(0, c, pacific)
-            dfs(ROWS-1, c, atlantic)
-        return list(pacific & atlantic)
+            dfs(0, c, pat, heights[0][c])
+            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pat and (r, c) in atl:
+                    res.append((r, c))
+        return res
 
 print(Solution().pacificAtlantic([[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]))
