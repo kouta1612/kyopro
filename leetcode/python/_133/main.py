50139fa0335a3511590469298b1c5eba36fc0939
kouta1612 <kouta1612world69@gmail.com>
2025-11-08 17:44:46 +0900

    133

--- leetcode/python/_133/main.py
+++ leetcode/python/_133/main.py
@@ -1,18 +1,18 @@
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-
 from typing import Optional
 class Solution:
-    def cloneGraph(self, node: Optional[Node]) -> Optional[Node]:
-        mp = {}
-        def dfs(node: Optional[Node]):
-            if node in mp: return mp[node]
-            if not node: return None
-            copy = Node(node.val)
-            mp[node] = copy
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node: return None
+
+        nodeMap = {}
+
+        def dfs(node: Optional['Node']) -> Optional['Node']:
+            res = Node(node.val)
+            nodeMap[node] = res
             for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
-            return copy
+                if nei in nodeMap:
+                    res.neighbors.append(nodeMap[nei])
+                else:
+                    v = dfs(nei)
+                    res.neighbors.append(v)
+            return res
         return dfs(node)
