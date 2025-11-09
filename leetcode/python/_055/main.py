002bf660fb282311c6a0757ade1066f91666131a
kouta1612 <kouta1612world69@gmail.com>
2025-11-09 21:33:32 +0900

    リファクタリング

--- leetcode/python/_055/main.py
+++ leetcode/python/_055/main.py
@@ -3,9 +3,9 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        minOK = len(nums) - 1
+        goal = len(nums) - 1
         for i in range(n - 2, -1, -1):
-            if i + nums[i] >= minOK: minOK = i
-        return minOK == 0
+            if i + nums[i] >= goal: goal = i
+        return goal == 0
 
 print(Solution().canJump([2,3,1,1,4]))
