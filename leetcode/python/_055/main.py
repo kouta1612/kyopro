64449589c6a7b26ec79ea4c47365c3f8e4d50f2b
kouta1612 <kouta1612world69@gmail.com>
2025-11-09 21:36:23 +0900

    リファクタリング

--- leetcode/python/_055/main.py
+++ leetcode/python/_055/main.py
@@ -3,9 +3,11 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         n = len(nums)
-        goal = len(nums) - 1
-        for i in range(n - 2, -1, -1):
-            if i + nums[i] >= goal: goal = i
-        return goal == 0
+        maxReach = 0
+        for i, num in enumerate(nums):
+            if i > maxReach: return False
+            maxReach = max(maxReach, i + num)
+            if maxReach >= n - 1: return True
+        return True
 
 print(Solution().canJump([2,3,1,1,4]))
