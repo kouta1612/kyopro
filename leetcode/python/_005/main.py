c464fdf899344fa0bfe3531571cf4487548aea8d
kouta1612 <kouta1612world69@gmail.com>
2025-11-09 11:58:33 +0900

    005

--- leetcode/python/_005/main.py
+++ leetcode/python/_005/main.py
@@ -1,20 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        dp = [[False] * (len(s) + 1) for _ in range(len(s) + 1)]
+        for size in range(2):
+            for i in range(len(s)):
+                j = i + size
+                dp[i][j] = True
+        for size in range(2, len(s) + 1):
+            for i in range(0, len(s) + 1 - size):
+                j = i + size
+                if dp[i + 1][j - 1] and s[i] == s[j - 1]:
+                    dp[i][j] = True
         res = ""
-
         for i in range(len(s)):
-            l, r = i, i
-            while 0 <= l and r < len(s) and s[l] == s[r]:
-                if len(res) < r - l + 1:
-                    res = s[l:r+1]
-                l -= 1
-                r += 1
-            l, r = i, i + 1
-            while 0 <= l and r < len(s) and s[l] == s[r]:
-                if len(res) < r - l + 1:
-                    res = s[l:r+1]
-                l -= 1
-                r += 1
+            for j in range(i + 1, len(s) + 1):
+                if dp[i][j] and len(res) < len(s[i:j]):
+                    res = s[i:j]
         return res
 
 print(Solution().longestPalindrome("babad"))
