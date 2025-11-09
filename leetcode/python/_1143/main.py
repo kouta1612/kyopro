3ff3a55dba06d58fd712dbb5f9d531022810bb47
kouta1612 <kouta1612world69@gmail.com>
2025-11-09 21:00:41 +0900

    1143

--- leetcode/python/_1143/main.py
+++ leetcode/python/_1143/main.py
@@ -1,13 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        ROWS, COLS = len(text1), len(text2)
-        dp = [[0] * (COLS + 1) for _ in range(ROWS + 1)]
-        for i in range(1, ROWS+1):
-            for j in range(1, COLS+1):
-                if text1[i-1] == text2[j-1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
+        dp = [[0] * (len(text2) + 1) for _ in range((len(text1) + 1))]
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[ROWS][COLS]
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j], dp[i][j - 1])
+        return dp[len(text1)][len(text2)]
 
 print(Solution().longestCommonSubsequence("abcde", "ace"))
