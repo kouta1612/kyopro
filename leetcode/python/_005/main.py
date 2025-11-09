9821d315b3b58cf4c95d47744836a9a6845a131b
kouta1612 <kouta1612world69@gmail.com>
2025-11-09 11:59:43 +0900

    リファクタリング

--- leetcode/python/_005/main.py
+++ leetcode/python/_005/main.py
@@ -1,18 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        dp = [[False] * (len(s) + 1) for _ in range(len(s) + 1)]
+        n = len(s)
+        dp = [[False] * (n + 1) for _ in range(n + 1)]
         for size in range(2):
-            for i in range(len(s)):
+            for i in range(n):
                 j = i + size
                 dp[i][j] = True
-        for size in range(2, len(s) + 1):
-            for i in range(0, len(s) + 1 - size):
+        for size in range(2, n + 1):
+            for i in range(0, n + 1 - size):
                 j = i + size
                 if dp[i + 1][j - 1] and s[i] == s[j - 1]:
                     dp[i][j] = True
         res = ""
-        for i in range(len(s)):
-            for j in range(i + 1, len(s) + 1):
+        for i in range(n):
+            for j in range(i + 1, n + 1):
                 if dp[i][j] and len(res) < len(s[i:j]):
                     res = s[i:j]
         return res
