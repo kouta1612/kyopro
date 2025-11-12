848f961d52671001c89353f826be71311df66ca4
kouta1612 <kouta1612world69@gmail.com>
2025-11-12 22:22:15 +0900

    リファクタリング

--- leetcode/python/_076/main.py
+++ leetcode/python/_076/main.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if s == t: return s
@@ -9,18 +7,21 @@ class Solution:
         
         l, r = 0, 0
         counter = 0
-        res = s + s
+        res = ""
+        minlen = float('inf')
         while l < len(s):
             while r < len(s) and not (r > 0 and counter == len(tmap) and s[r-1] in smap and s[r-1] in tmap and smap[s[r-1]] == tmap[s[r-1]]):
                 smap[s[r]] = smap.get(s[r], 0) + 1
                 if smap[s[r]] == tmap.get(s[r], 0): counter += 1
                 r += 1
-            if counter == len(tmap) and len(res) >= r - l: res = s[l:r]
+            if counter == len(tmap) and minlen > r - l: 
+                res = s[l:r]
+                minlen = len(res)
             if smap[s[l]] == tmap.get(s[l], 0): counter -= 1
             smap[s[l]] -= 1
             if smap[s[l]] == 0: del smap[s[l]]
             l += 1
-        return res if res != s + s else ""
+        return res
 
 print(Solution().minWindow("ADOBECODEBANC", "ABC"))
 print(Solution().minWindow("a", "a"))
