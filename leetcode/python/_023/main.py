a7f8f6233dbdfff028b344b285a65e038938da7e
kouta1612 <kouta1612world69@gmail.com>
2025-11-13 22:03:15 +0900

    リファクタリング

--- leetcode/python/_023/main.py
+++ leetcode/python/_023/main.py
@@ -15,7 +15,7 @@ class Solution:
         cur = dummy = ListNode(0)
         while heap:
             v, i, node = heapq.heappop(heap)
-            cur.next = ListNode(node.val)
+            cur.next = node
             cur = cur.next
             if node.next: heapq.heappush(heap, (node.next.val, i, node.next))
         return dummy.next
