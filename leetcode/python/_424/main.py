db2b51bf467cbb0e42176f7ee1447bf8734086a2
kouta1612 <kouta1612world69@gmail.com>
2025-11-16 15:26:10 +0900

    424

--- leetcode/python/_424/main.py
+++ leetcode/python/_424/main.py
@@ -3,18 +3,24 @@ from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l, r = 0, 0
-        res = 0
+        n = len(s)
         counter = defaultdict(int)
-        maxCount = 0
-        while r < len(s):
+        res = 0
+        while r < n:
             counter[s[r]] += 1
-            maxCount = max(maxCount, counter[s[r]])
-            while r - l + 1 - maxCount > k:
+            while l < n:
+                maxCounter = 0
+                for o in range(ord('A'), ord('Z') + 1):
+                    maxCounter = max(maxCounter, counter[chr(o)])
+                remove = r - l + 1 - maxCounter
+                if remove <= k: break
                 counter[s[l]] -= 1
                 l += 1
             res = max(res, r - l + 1)
             r += 1
+
         return res
+
 
 print(Solution().characterReplacement("ABAB", 2))
 print(Solution().characterReplacement("AABABBA", 1))
