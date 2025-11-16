a18627c8ccf47af2422480f4ff5a6c861289cebe
kouta1612 <kouta1612world69@gmail.com>
2025-11-17 08:46:45 +0900

    079

--- leetcode/python/_079/main.py
+++ leetcode/python/_079/main.py
@@ -3,24 +3,24 @@ from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
-        pathSet = set()
+        seen = set()
 
-        def dfs(r, c, i):
-            if i ==  len(word): return True
-            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or 
-                board[r][c] != word[i] or (r, c) in pathSet):
-                return False
-            pathSet.add((r, c))
-            for nr, nc in [[r + 1, c], [r - 1, c], [r, c - 1], [r, c + 1]]:
-                if dfs(nr, nc, i + 1): return True
-            pathSet.remove((r, c))
+        def dfs(i, j, k: int) -> bool:
+            if k == len(word): return True
+            if i < 0 or j < 0 or i >= ROWS or j >= COLS: return False
+            if (i, j) in seen: return False
+            if word[k] != board[i][j]: return False
+
+            seen.add((i, j))
+            for ni, nj in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
+                if dfs(ni, nj, k+1): return True
+            seen.remove((i, j))
             return False
-
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(r, c, 0): return True
         return False
-            
 
 print(Solution().exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED"))
 print(Solution().exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE"))
