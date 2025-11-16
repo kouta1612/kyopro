96b4b3287263dee2353f592db6b32e606d589ffb
kouta1612 <kouta1612world69@gmail.com>
2025-11-16 19:34:48 +0900

    019

--- leetcode/python/_019/main.py
+++ leetcode/python/_019/main.py
@@ -7,13 +7,13 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy = ListNode(0, head)
-        left, right = dummy, head
-        for _ in range(n):
-            right = right.next
-        while right:
-            left, right = left.next, right.next
-        left.next = left.next.next
+        dummy = first = second = ListNode(0, head)
+        for _ in range(n + 1):
+            second = second.next
+        while second:
+            first = first.next
+            second = second.next
+        first.next = first.next.next
         return dummy.next
 
 print(Solution().removeNthFromEnd(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2))
