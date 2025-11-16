628054fee4a18556569ec4b01cb503e60e021d97
kouta1612 <kouta1612world69@gmail.com>
2025-11-16 13:44:48 +0900

    015

--- leetcode/python/_015/main.py
+++ leetcode/python/_015/main.py
@@ -2,21 +2,22 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         nums.sort()
-        res = []
-        for i in range(len(nums) - 2):
-            if i > 0 and nums[i] == nums[i - 1]: continue
 
-            j, k = i + 1, len(nums) - 1
+        res = []
+        for i in range(n - 2):
+            if i > 0 and nums[i] == nums[i - 1]: continue
+            j, k = i + 1, n - 1
             while j < k:
-                curSum = nums[i] + nums[j] + nums[k]
-                if curSum == 0:
-                    res.append([nums[i], nums[j], nums[k]])
+                s = nums[i] + nums[j] + nums[k]
+                if s == 0:
+                    res.append((nums[i], nums[j], nums[k]))
                     j += 1
                     k -= 1
-                    while j < k and nums[j] == nums[j - 1]: j += 1
-                    while j < k and nums[k] == nums[k + 1]: k -= 1
-                elif curSum > 0:
+                    while j < k and nums[j] == nums[j-1]: j += 1
+                    while k > j and nums[k] == nums[k+1]: k -= 1
+                elif s > 0:
                     k -= 1
                 else:
                     j += 1
