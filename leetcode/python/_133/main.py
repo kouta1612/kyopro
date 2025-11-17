647b148c6190b3bca8b6366f9bfbb2983c705a0a
kouta1612 <kouta1612world69@gmail.com>
2025-11-17 23:41:09 +0900

    133

--- leetcode/python/_133/main.py
+++ leetcode/python/_133/main.py
@@ -1,23 +1,24 @@
+from typing import Optional
+
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node: return None
+        mapOldToNew = {}
 
-        nodeMap = {}
-
-        def dfs(cur: Optional['Node']) -> Optional['Node']:
-            if cur in nodeMap: return nodeMap[cur]
-            clone = Node(cur.val)
-            nodeMap[cur] = clone
-            for nei in cur.neighbors:
-                clone.neighbors.append(dfs(nei))
-            return clone
+        def dfs(node: Optional[Node]) -> Optional[Node]:
+            if not node: return None
+            if node in mapOldToNew: return mapOldToNew[node]
+            res = Node(node.val)
+            mapOldToNew[node] = res
+            for nei in node.neighbors:
+                res.neighbors.append(dfs(nei))
+            return res
 
         return dfs(node)
+
 
 print(Solution().cloneGraph(None))
