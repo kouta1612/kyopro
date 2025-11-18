3abba2697596602959fb9d2bacea99a7fbd7ba98
kouta1612 <kouta1612world69@gmail.com>
2025-11-19 00:15:54 +0900

    417

--- leetcode/python/_417/main.py
+++ leetcode/python/_417/main.py
@@ -3,27 +3,27 @@ from typing import List
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pat, atl = set[(int, int)](), set[(int, int)]()
+        pac, atl = set(), set()
 
-        def dfs(r, c: int, visited: set[(int, int)], prev: int):
-            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or
-                (r, c) in visited or heights[r][c] < prev):
-                return
-            visited.add((r, c))
-            for nr, nc in [[r + 1, c], [r - 1, c], [r, c + 1], [r, c - 1]]:
-                dfs(nr, nc, visited, heights[r][c])
+        def dfs(r, c: int, visit: set):
+            visit.add((r, c))
+            for nr, nc in [[r+1, c], [r-1, c], [r, c+1], [r, c-1]]:
+                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
+                if (nr, nc) in visit: continue
+                if heights[nr][nc] < heights[r][c]: continue
+                dfs(nr, nc, visit)
 
         for r in range(ROWS):
-            dfs(r, 0, pat, heights[r][0])
-            dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+            if (r, 0) not in pac: dfs(r, 0, pac)
+            if (r, COLS - 1) not in atl: dfs(r, COLS - 1, atl)
         for c in range(COLS):
-            dfs(0, c, pat, heights[0][c])
-            dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+            if (0, c) not in pac: dfs(0, c, pac)
+            if (ROWS - 1, c) not in atl: dfs(ROWS - 1, c, atl)
 
         res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) in pat and (r, c) in atl:
+                if (r, c) in pac and (r, c) in atl:
                     res.append((r, c))
         return res
 
