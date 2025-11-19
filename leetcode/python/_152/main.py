33dccf854e78c9c1c066ff2139cfdf30460fa01a
kouta1612 <kouta1612world69@gmail.com>
2025-11-20 08:43:32 +0900

    152

--- leetcode/python/_152/main.py
+++ leetcode/python/_152/main.py
@@ -2,16 +2,11 @@ from typing import List
 
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        n, res = len(nums), max(nums)
+        res = max(nums)
         curMax, curMin = 1, 1
-        for num in nums:
-            if num == 0: 
-                curMax, curMin = 1, 1
-            else:
-                tmp = curMax * num
-                curMax = max(tmp, curMin * num, num)
-                curMin = min(tmp, curMin * num, num)
-                res = max(res, curMax)
+        for n in nums:
+            curMax, curMin = max(curMax * n, curMin * n, n), min(curMax * n, curMin * n, n)
+            res = max(res, curMax)
         return res
 
 print(Solution().maxProduct([2,3,-2,4]))
