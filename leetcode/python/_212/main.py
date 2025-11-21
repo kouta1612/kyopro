f874956bbc261076bc4cd3523333c6f5998e19a6
kouta1612 <kouta1612world69@gmail.com>
2025-11-21 13:04:50 +0900

    212

--- leetcode/python/_212/main.py
+++ leetcode/python/_212/main.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.word = False
+        self.isWord = False
 
     def add(self, word: str):
         cur = self
@@ -11,35 +11,31 @@ class TrieNode:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
-        cur.word = True
+        cur.isWord = True
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         ROWS, COLS = len(board), len(board[0])
         trie = TrieNode()
         for word in words: trie.add(word)
-        
-        res = []
 
-        def dfs(i, j: int, node: Optional[TrieNode], word: str):
+        res, seen = set(), set()
+        def dfs(i, j: int, cur: str, node: Optional[TrieNode]):
             if i < 0 or j < 0 or i >= ROWS or j >= COLS: return
+            if (i, j) in seen: return
             c = board[i][j]
-            if c == "#": return
             if c not in node.children: return
 
-            word += c
+            cur += c
             node = node.children[c]
-            if node.word: 
-                res.append(word)
-                node.word = False
-            board[i][j] = "#"
-            for ni, nj in [[i + 1, j], [i - 1, j], [i, j + 1], [i, j - 1]]:
-                dfs(ni, nj, node, word)
-            board[i][j] = c
-
-        for i in range(ROWS):
-            for j in range(COLS):
-                dfs(i, j, trie, "")
-        return res
+            if node.isWord: res.add(cur)
+            seen.add((i, j))
+            for ni, nj in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
+                dfs(ni, nj, cur, node)
+            seen.remove((i, j))
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, "", trie)
+        return list(res)
 
 print(Solution().findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], ["oath","pea","eat","rain"]))
