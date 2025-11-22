87b0e6eaae9dfd6c7309261fff6e5d3e62cec7c2
kouta1612 <kouta1612world69@gmail.com>
2025-11-22 16:47:45 +0900

    322

--- leetcode/python/_322/main.py
+++ leetcode/python/_322/main.py
@@ -2,14 +2,13 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [1<<32] * (amount + 1)
+        dp = [amount + 1] * (amount + 1)
         dp[0] = 0
-        coins.sort()
-        for i in range(1, amount + 1):
-            for j in range(len(coins)):
-                if i - coins[j] < 0: break
-                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
-        return dp[amount] if dp[amount] != 1<<32 else -1
+        for i in range(amount + 1):
+            for coin in coins:
+                if i-coin < 0: continue
+                dp[i] = min(dp[i], dp[i-coin] + 1)
+        return dp[amount] if dp[amount] != amount + 1 else -1
 
 print(Solution().coinChange([1,2,5], 11))
 print(Solution().coinChange([2], 3))
