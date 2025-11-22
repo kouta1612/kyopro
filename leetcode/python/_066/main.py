65bb10cd2f5f1b177a4b965c4266374eed1d937a
kouta1612 <kouta1612world69@gmail.com>
2025-11-22 22:13:53 +0900

    リファクタリング

--- leetcode/python/_066/main.py
+++ leetcode/python/_066/main.py
@@ -4,12 +4,10 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         n = len(digits)
         for i in range(n - 1, -1, -1):
-            if digits[i] == 9:
-                digits[i] = 0
-                if i == 0: return [1] + digits
-            else: 
+            if digits[i] < 9:
                 digits[i] += 1
-                break
-        return digits
+                return digits
+            else: digits[i] = 0
+        return [1] + digits
 
 print(Solution().plusOne([1,2,3]))
