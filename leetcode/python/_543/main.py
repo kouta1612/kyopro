82027d8df948e17b538f3cc73a60382b91b2d6de
kouta1612 <kouta1612world69@gmail.com>
2025-11-22 17:31:47 +0900

    543

--- leetcode/python/_543/main.py
+++ leetcode/python/_543/main.py
@@ -9,16 +9,14 @@ class TreeNode:
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.res = 0
-        
-        def dfs(root):
-            if root is None:
-                return 0
+
+        def dfs(root: Optional[TreeNode]) -> int:
+            if not root: return 0
             left, right = dfs(root.left), dfs(root.right)
             self.res = max(self.res, left + right)
             return max(left, right) + 1
 
         dfs(root)
-
         return self.res
 
 print(Solution().diameterOfBinaryTree(TreeNode(1, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3))))
