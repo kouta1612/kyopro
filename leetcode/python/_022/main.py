8a8fab097e0ed8bd355c98853a0d81a48f1427da
kouta1612 <kouta1612world69@gmail.com>
2025-11-23 16:09:19 +0900

    022

--- leetcode/python/_022/main.py
+++ leetcode/python/_022/main.py
@@ -4,17 +4,23 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
 
-        def helper(open, close: int, s: str):
-            if len(s) == n * 2:
-                res.append(s)
+        def dfs(i: int, cur: str):
+            if i == n * 2:
+                if self.isParenthes(cur): res.append(cur)
                 return
-            if open < n:
-                helper(open + 1, close, s + "(")
-            if open > close:
-                helper(open, close + 1, s + ")")
-        
-        helper(0, 0, "")
+            dfs(i + 1, cur + "(")
+            dfs(i + 1, cur + ")")
+
+        dfs(0, "")
         return res
+
+    def isParenthes(self, word: str) -> bool:
+        stack = []
+        for c in word:
+            if not stack and c == ")": return False
+            if stack and stack[-1] == "(" and c == ")": stack.pop()
+            else: stack.append(c)
+        return len(stack) == 0
 
 print(Solution().generateParenthesis(3))
 print(Solution().generateParenthesis(1))
