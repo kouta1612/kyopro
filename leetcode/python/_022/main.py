ff7853ec907437fca533cfc2584125d64c14ed9f
kouta1612 <kouta1612world69@gmail.com>
2025-11-23 16:11:18 +0900

    リファクタリング

--- leetcode/python/_022/main.py
+++ leetcode/python/_022/main.py
@@ -4,23 +4,16 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
 
-        def dfs(i: int, cur: str):
-            if i == n * 2:
-                if self.isParenthes(cur): res.append(cur)
+        def dfs(opened, closed: int, cur: str):
+            if opened == n and closed == n:
+                res.append(cur)
                 return
-            dfs(i + 1, cur + "(")
-            dfs(i + 1, cur + ")")
+            if opened < n: dfs(opened + 1, closed, cur + "(")
+            if closed < opened: dfs(opened, closed + 1, cur + ")")
 
-        dfs(0, "")
+        dfs(0, 0, "")
         return res
 
-    def isParenthes(self, word: str) -> bool:
-        stack = []
-        for c in word:
-            if not stack and c == ")": return False
-            if stack and stack[-1] == "(" and c == ")": stack.pop()
-            else: stack.append(c)
-        return len(stack) == 0
 
 print(Solution().generateParenthesis(3))
 print(Solution().generateParenthesis(1))
