f0178c375649a38b0d75f49d6a6ab48a8d79fa8e
kouta1612 <kouta1612world69@gmail.com>
2025-11-25 05:49:01 +0900

    002

--- leetcode/python/_002/main.py
+++ leetcode/python/_002/main.py
@@ -7,21 +7,14 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = cur = ListNode()
-
-        one = 0
-        while l1 or l2 or one:
-            d1 = l1.val if l1 else 0
-            d2 = l2.val if l2 else 0
-            val = d1 + d2 + one
-            one = val // 10
-            val %= 10
-            cur.next = ListNode(val)
-            
+        carry = 0
+        dummy = cur = ListNode(0)
+        while l1 or l2 or carry:
+            val = (l1.val if l1 else 0) + (l2.val if l2 else 0) + carry
+            cur.next, carry = ListNode(val % 10), val // 10
             cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return dummy.next
-
 
 print(Solution().addTwoNumbers(ListNode(2, ListNode(4, ListNode(3))), ListNode(5, ListNode(6, ListNode(4)))))
