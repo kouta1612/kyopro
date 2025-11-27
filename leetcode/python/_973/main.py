f7805c72f99ccc7784fc28c8ca92bef9f969a23e
kouta1612 <kouta1612world69@gmail.com>
2025-11-27 21:08:07 +0900

    リファクタリング

--- leetcode/python/_973/main.py
+++ leetcode/python/_973/main.py
@@ -1,16 +1,13 @@
 from typing import List
-from heapq import heappush, heappop
+from heapq import heappush, heappushpop
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heaps = []
         for x, y in points:
             dist = x * x + y * y
-            heappush(heaps, (dist, x, y))
-        res = []
-        for _ in range(k):
-            _, x, y = heappop(heaps)
-            res.append((x, y))
-        return res
+            if len(heaps) < k: heappush(heaps, (-dist, x, y))
+            else: heappushpop(heaps, (-dist, x, y))
+        return [[x, y] for _, x, y in heaps]
 
 print(Solution().kClosest([[1, 3], [-2, 2]], 1))
