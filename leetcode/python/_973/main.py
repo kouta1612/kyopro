eee80a7dec0d71ff313db1cc72bcd7e1715cf1bc
kouta1612 <kouta1612world69@gmail.com>
2025-11-27 21:07:36 +0900

    リファクタリング

--- leetcode/python/_973/main.py
+++ leetcode/python/_973/main.py
@@ -1,12 +1,11 @@
 from typing import List
-from math import sqrt
 from heapq import heappush, heappop
 
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heaps = []
         for x, y in points:
-            dist = sqrt(x * x + y * y)
+            dist = x * x + y * y
             heappush(heaps, (dist, x, y))
         res = []
         for _ in range(k):
