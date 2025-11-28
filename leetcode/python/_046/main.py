bb83e33c2ed615e48079b3b58f4ef42e56a8ee40
kouta1612 <kouta1612world69@gmail.com>
2025-11-28 15:28:14 +0900

    046

--- leetcode/python/_046/main.py
+++ leetcode/python/_046/main.py
@@ -2,14 +2,15 @@ from typing import List
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if not nums: return [[]]
-        res = []
-        perms = self.permute(nums[1:])
-        for perm in perms:
-            for i in range(len(perm) + 1):
-                permcopy = perm.copy()
-                permcopy.insert(i, nums[0])
-                res.append(permcopy)
-        return res
+        perms = [[]]
+        for num in nums:
+            newperms = []
+            for p in perms:
+                for i in range(len(p) + 1):
+                    pcopy = p.copy()
+                    pcopy.insert(i, num)
+                    newperms.append(pcopy)
+            perms = newperms
+        return perms
 
 print(Solution().permute([1,2,3]))
