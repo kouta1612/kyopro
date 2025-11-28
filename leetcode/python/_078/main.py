cfc8a38b2955dd970a67b56e9adf02c95eb7711e
kouta1612 <kouta1612world69@gmail.com>
2025-11-28 10:44:57 +0900

    078

--- leetcode/python/_078/main.py
+++ leetcode/python/_078/main.py
@@ -2,16 +2,19 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        def helper(i: int, subs: List[int]):
-            if i == len(nums):
-                res.append(subs)
-                return
-            
-            helper(i + 1, subs)
-            helper(i + 1, subs + [nums[i]])
-        
-        helper(0, [])
+        res = [[]]
+
+        def dfs(i: int, vals: List[int]):
+            if i == len(nums): return
+
+            dfs(i + 1, vals)
+
+            vals.append(nums[i])
+            res.append(vals[:])
+            dfs(i + 1, vals)
+            vals.pop()
+
+        dfs(0, [])
         return res
 
 print(Solution().subsets([1,2,3]))
