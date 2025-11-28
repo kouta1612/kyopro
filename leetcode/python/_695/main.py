6848625053e28189c8c96fb9f5044d7e6d04cd56
kouta1612 <kouta1612world69@gmail.com>
2025-11-28 17:29:34 +0900

    695

--- leetcode/python/_695/main.py
+++ leetcode/python/_695/main.py
@@ -2,20 +2,22 @@ from typing import List
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        res = 0
+        ROWS, COLS = len(grid), len(grid[0])
 
         def dfs(i, j: int) -> int:
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]):
-                return 0
-            if grid[i][j] == 0:
-                return 0
+            if i < 0 or j < 0 or i >= ROWS or j >= COLS: return 0
+            if grid[i][j] == 0: return 0
             grid[i][j] = 0
-            return 1 + dfs(i + 1, j) + dfs(i, j + 1) + dfs(i - 1, j) + dfs(i, j - 1)
+            res = 1
+            for ni, nj in [[i+1, j], [i-1, j], [i, j+1], [i, j-1]]:
+                res += dfs(ni, nj)
+            return res
 
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    res = max(res, dfs(i, j))
+        res = 0
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 0: continue
+                res = max(res, dfs(i, j))
         return res
 
 print(Solution().maxAreaOfIsland([[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]))
