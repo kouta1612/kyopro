3a9d4b0d129b3c54d87043a4e546ffd6f586230d
kouta1612 <kouta1612world69@gmail.com>
2025-11-30 09:57:54 +0900

    リファクタリング

--- leetcode/python/_787/main.py
+++ leetcode/python/_787/main.py
@@ -1,16 +1,22 @@
 from typing import List
+from collections import defaultdict
+from heapq import heappop, heappush
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        costs = [float('inf')] * n
-        costs[src] = 0
-        for _ in range(k + 1):
-            tmps = costs.copy()
-            for start, end, cost in flights:
-                if costs[start] == float('inf'): continue
-                if costs[start] + cost >= tmps[end]: continue
-                tmps[end] = costs[start] + cost
-            costs = tmps
-        return costs[dst] if costs[dst] != float('inf') else -1
+        graph = defaultdict(list)
+        for s, t, w in flights: graph[s].append((t, w))
+        best = defaultdict(lambda: defaultdict(lambda: float('inf')))
+        best[src][k + 1] = 0
+        heap = [(0, src, k + 1)]
+        while heap:
+            cost, s, remain = heappop(heap)
+            if s == dst: return cost
+            if remain == 0: continue
+            for to, w in graph[s]:
+                if cost + w < best[to][remain - 1]:
+                    best[to][remain - 1] = cost + w
+                    heappush(heap, (best[to][remain - 1], to, remain - 1))
+        return -1
 
 print(Solution().findCheapestPrice(4, [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], 0, 3, 1))
