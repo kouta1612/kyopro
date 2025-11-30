4454ee412f232d42ec4ebdc8bb9eee5a2ede463c
kouta1612 <kouta1612world69@gmail.com>
2025-11-30 19:01:07 +0900

    リファクタリング

--- leetcode/python/_494/main.py
+++ leetcode/python/_494/main.py
@@ -2,17 +2,13 @@ from typing import List
 from collections import defaultdict
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = defaultdict(int)
+        dp = {0: 1}
         for num in nums:
-            if len(dp) == 0:
-                dp[num] += 1
-                dp[-num] += 1
-                continue
             nextDP = defaultdict(int)
             for key, val in dp.items():
                 nextDP[key + num] += dp[key]
                 nextDP[key - num] += dp[key]
             dp = nextDP
-        return dp[target]
+        return dp.get(target, 0)
 
 print(Solution().findTargetSumWays([1,1,1,1,1], 3))
