3a3e59b451044d02800f00e33ead9bd8ce7a4d0b
kouta1612 <kouta1612world69@gmail.com>
2025-12-01 23:02:37 +0900

    リファクタリング

--- leetcode/python/_097/main.py
+++ leetcode/python/_097/main.py
@@ -9,8 +9,10 @@ class Solution:
             if (i, j) in dp: return dp[(i, j)]
 
             if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                dp[(i, j)] = True
                 return True
             if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                dp[(i, j)] = True
                 return True
             dp[(i, j)] = False
             return False
