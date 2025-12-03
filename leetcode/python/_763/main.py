290aa188b79a08cba4f1af4ec585178baa6e174a
kouta1612 <kouta1612world69@gmail.com>
2025-12-04 08:40:58 +0900

    リファクタリング

--- leetcode/python/_763/main.py
+++ leetcode/python/_763/main.py
@@ -2,18 +2,14 @@ from types import List
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        counter = {}
-        for c in s: counter[c] = 1 + counter.get(c, 0)
-        l, r = 0, 0
-        wordset = set()
+        lastIndex = {}
+        for i, c in enumerate(s): lastIndex[c] = i
+        size, end = 0, 0
         res = []
-        while r < len(s):
-            wordset.add(s[r])
-            counter[s[r]] -= 1
-            if counter[s[r]] == 0:
-                wordset.remove(s[r])
-                if len(wordset) == 0:
-                    res.append(r + 1 - l)
-                    l = r + 1
-            r += 1
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[c])
+            if i == end:
+                res.append(size)
+                size = 0
         return res
