c5d3ee3bcc117ad944b37c735ea0fd083b667867
kouta1612 <kouta1612world69@gmail.com>
2025-12-04 08:03:32 +0900

    リファクタリング

--- leetcode/python/_1899/main.py
+++ leetcode/python/_1899/main.py
@@ -2,9 +2,10 @@ from types import List
 
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        res = [0, 0, 0]
-        x, y, z = target[0], target[1], target[2]
-        for tx, ty, tz in triplets:
-            if tx > x or ty > y or tz > z: continue
-            res = [max(tx, res[0]), max(ty, res[1]), max(tz, res[2])]
-        return res == target
+        good = set()
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+            for i, v in enumerate(t):
+                if v == target[i]: good.add(i)
+        return len(good) == 3
