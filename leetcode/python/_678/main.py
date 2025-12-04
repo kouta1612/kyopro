1c54e3b20198401033778b8a9563f7b6e73d8e4e
kouta1612 <kouta1612world69@gmail.com>
2025-12-04 22:28:31 +0900

    リファクタリング

--- leetcode/python/_678/main.py
+++ leetcode/python/_678/main.py
@@ -1,22 +1,13 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        dp = {}
-
-        def dfs(i, leftCount: int) -> bool:
-            if leftCount < 0: return False
-            if i == len(s): return leftCount == 0
-            if (i, leftCount) in dp: return dp[(i, leftCount)]
-
-            if s[i] == "(": 
-                dp[(i, leftCount)] = dfs(i + 1, leftCount + 1)
-                return dp[(i, leftCount)]
-            elif s[i] == ")": 
-                dp[(i, leftCount)] = dfs(i + 1, leftCount - 1)
-                return dp[(i, leftCount)]
-            dp[(i, leftCount)] = (
-                dfs(i + 1, leftCount + 1) or
-                dfs(i + 1, leftCount - 1) or
-                dfs(i + 1, leftCount)
-            )
-            return dp[(i, leftCount)]
-        return dfs(0, 0)
+        leftMin, leftMax = 0, 0
+        for c in s:
+            if c == "(":
+                leftMin, leftMax = leftMin + 1, leftMax + 1
+            elif c == ")":
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1
+            if leftMax < 0: return False
+            if leftMin < 0: leftMin = 0
+        return leftMin == 0
