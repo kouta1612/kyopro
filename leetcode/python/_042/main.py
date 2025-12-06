9b4578ca43a2441aa0c37b2ce86225fb2de1b03c
kouta1612 <kouta1612world69@gmail.com>
2025-12-06 22:54:04 +0900

    リファクタリング

--- leetcode/python/_042/main.py
+++ leetcode/python/_042/main.py
@@ -2,15 +2,18 @@ from types import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        lMaxHeights, rMaxHeights = [0] * n, [0] * n
-        for i in range(1, n):
-            lMaxHeights[i] = max(lMaxHeights[i-1], height[i-1])
-        for i in range(n - 2, -1, -1):
-            rMaxHeights[i] = max(rMaxHeights[i+1], height[i+1])
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
         res = 0
-        for i in range(1, n - 1):
-            res += max(min(lMaxHeights[i], rMaxHeights[i]) - height[i], 0)
+        while l < r:
+            if leftMax <= rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
         return res
 
 print(Solution().trap([0,1,0,2,1,0,1,3,2,1,2,1]))
