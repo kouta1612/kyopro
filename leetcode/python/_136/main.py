114672d2a271c4e4419dae5ad22604ac421d1130
kouta1612 <kouta1612world69@gmail.com>
2025-12-06 15:47:27 +0900

    136

--- leetcode/python/_136/main.py
+++ leetcode/python/_136/main.py
@@ -3,8 +3,7 @@ from typing import List
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         res = 0
-        for num in nums:
-            res ^= num
+        for num in nums: res ^= num
         return res
 
 print(Solution().singleNumber([4,1,2,1,2]))
