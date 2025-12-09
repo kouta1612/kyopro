3296807b5bb23219973e1053cc625a4f3aeda18f
kouta1612 <kouta1612world69@gmail.com>
2025-12-09 21:20:45 +0900

    025

---
+++ leetcode/python/_025/main.py
@@ -0,0 +1,22 @@
+from types import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        cur = head
+        for _ in range(k):
+            if not cur: return head
+            cur = cur.next
+        pre, cur = None, head
+        for _ in range(k):
+            tmp = cur.next
+            cur.next = pre
+            pre, cur = cur, tmp
+        head.next = self.reverseKGroup(cur, k)
+        return pre
+
+print(Solution().reverseKGroup(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2))
