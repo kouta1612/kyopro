ee37e56e2d0b12220782a39faae46aae88fa49d6
kouta1612 <kouta1612world69@gmail.com>
2025-12-20 10:54:04 +0900

    036

--- leetcode/python/_036/main.py
+++ leetcode/python/_036/main.py
@@ -3,18 +3,18 @@ from collections import defaultdict
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        ROWS, COLS = len(board), len(board[0])
-        rowmap, colmap = defaultdict(set), defaultdict(set)
-        squares = defaultdict(set)
+        ROWS, COLS = len(board), len(board)
+        rset, cset = [set() for _ in range(ROWS)], [set() for _ in range(COLS)]
+        bset = [[set() for _ in range(3)] for _ in range(3)]
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == ".": continue
-                if board[r][c] in rowmap[r]: return False
-                if board[r][c] in colmap[c]: return False
-                if board[r][c] in squares[(r//3, c//3)]: return False
-                rowmap[r].add(board[r][c])
-                colmap[c].add(board[r][c])
-                squares[(r//3, c//3)].add(board[r][c])
+                if board[r][c] in rset[r]: return False
+                if board[r][c] in cset[c]: return False
+                if board[r][c] in bset[r//3][c//3]: return False
+                rset[r].add(board[r][c])
+                cset[c].add(board[r][c])
+                bset[r//3][c//3].add(board[r][c])
         return True
 
 print(Solution().isValidSudoku([
