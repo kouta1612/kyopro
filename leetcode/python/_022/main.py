a0e072f53e2047a02e26e40d25b1dcd332fdc37d
kouta1612 <kouta1612world69@gmail.com>
2025-12-21 09:55:44 +0900

    リファクタリング

--- leetcode/python/_022/main.py
+++ leetcode/python/_022/main.py
@@ -3,16 +3,14 @@ from typing import List
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         res = []
-        def dfs(cur: int, s: str, opened, closed: int):
-            if opened < closed: return
-            if cur == n * 2:
-                if opened == closed: res.append(s)
+        def dfs(s: str, opened, closed: int):
+            if opened == n and closed == n:
+                res.append(s)
                 return
-            dfs(cur + 1, s + "(", opened + 1, closed)
-            dfs(cur + 1, s + ")", opened, closed + 1)
-        dfs(0, "", 0, 0)
+            if opened < n: dfs(s + "(", opened + 1, closed)
+            if closed < opened: dfs(s + ")", opened, closed + 1)
+        dfs("", 0, 0)
         return res
-
 
 print(Solution().generateParenthesis(3))
 print(Solution().generateParenthesis(1))
