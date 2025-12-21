9474f5ea209db37b25d65d31322e9a2d4a25ac2a
kouta1612 <kouta1612world69@gmail.com>
2025-12-21 09:29:59 +0900

    155

--- leetcode/python/_155/main.py
+++ leetcode/python/_155/main.py
@@ -4,23 +4,22 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.minstack = []
+        self.minStack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        minval = self.minstack[-1] if self.minstack else val
-        minval = min(minval, val)
-        self.minstack.append(minval)
+        val = min(self.minStack[-1], val) if self.minStack else val
+        self.minStack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
-        self.minstack.pop()
+        self.minStack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.minstack[-1]
+        return self.minStack[-1]
 
 print(MinStack().push(-2))
 print(MinStack().push(0))
