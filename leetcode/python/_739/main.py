4c98813bc998a04d80094df84aa138c69d8d61a1
kouta1612 <kouta1612world69@gmail.com>
2025-12-22 21:04:35 +0900

    739

--- leetcode/python/_739/main.py
+++ leetcode/python/_739/main.py
@@ -1,14 +1,16 @@
 from typing import List
+from collections import deque
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res = [0] * len(temperatures)
-        stack = []
-        for i, temp in enumerate(temperatures):
-            while stack and stack[-1][1] < temp:
-                j, t = stack.pop()
+        n = len(temperatures)
+        res = [0] * n
+        deq = deque() # [i,v]
+        for i, t in enumerate(temperatures[:n]):
+            while deq and deq[-1][1] < t:
+                j, _ = deq.pop()
                 res[j] = i - j
-            stack.append((i, temp))
+            deq.append((i, t))
         return res
 
 print(Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
