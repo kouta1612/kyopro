29abe1b935878227df4866ecbbba54e44b21e855
kouta1612 <kouta1612world69@gmail.com>
2025-12-25 09:38:06 +0900

    074

--- leetcode/python/_074/main.py
+++ leetcode/python/_074/main.py
@@ -6,16 +6,19 @@ class Solution:
         top, bottom = 0, ROWS - 1
         while top < bottom:
             mid = (top + bottom) // 2
-            if target == matrix[mid][0]: return True
-            elif target < matrix[mid][0]: bottom = mid - 1
-            elif target <= matrix[mid][COLS - 1]: top = mid
-            else: top = mid + 1
-        left, right = 0, COLS - 1
-        while left <= right:
-            mid = (left + right) // 2
-            if target == matrix[top][mid]: return True
-            elif target < matrix[top][mid]: right = mid - 1
-            else: left = mid + 1
+            if matrix[mid][0] > target:
+                bottom = mid - 1
+            elif matrix[mid][COLS - 1] >= target:
+                bottom = mid
+            else:
+                top = mid + 1
+        if top > bottom: return False
+        l, r = 0, COLS - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[top][mid] == target: return True
+            elif matrix[top][mid] < target: l = mid + 1
+            else: r = mid - 1
         return False
 
 print(Solution().searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3))
