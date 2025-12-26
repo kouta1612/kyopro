c99f63eff8a168eabbdb770852706d0307c6a9e0
kouta1612 <kouta1612world69@gmail.com>
2025-12-26 09:31:31 +0900

    981

--- leetcode/python/_981/main.py
+++ leetcode/python/_981/main.py
@@ -1,20 +1,19 @@
 class TimeMap:
-
     def __init__(self):
-        self.store = {}
+        self.timemap = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = []
-        self.store[key].append((value, timestamp))
+        if key not in self.timemap: self.timemap[key] = []
+        self.timemap[key].append((value, timestamp))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store: return ""
-        ok, ng = -1, len(self.store[key])
+        if key not in self.timemap: return ""
+        ok, ng = -1, len(self.timemap[key])
         while abs(ok - ng) > 1:
             wj = (ok + ng) // 2
-            if self.store[key][wj][1] <= timestamp: ok = wj
+            if self.timemap[key][wj][1] <= timestamp: ok = wj
             else: ng = wj
-        return self.store[key][ok][0] if ok >= 0 else ""
+        if ok == -1: return ""
+        return self.timemap[key][ok][0]
 
 print(TimeMap().get("foo", 1))
