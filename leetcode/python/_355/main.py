258ca1f6bf88f5edf64c34b2fef5e8683036f50a
kouta1612 <kouta1612world69@gmail.com>
2025-12-28 11:35:04 +0900

    355

--- leetcode/python/_355/main.py
+++ leetcode/python/_355/main.py
@@ -7,31 +7,36 @@ class Twitter:
     def __init__(self):
         self.timer = 0
         self.tweets = defaultdict(list)
-        self.followings = defaultdict(set)
+        self.follows = defaultdict(set)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.timer -= 1
+        self.timer += 1
         self.tweets[userId].append((self.timer, tweetId))
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        heap = []
-        users = {userId} | self.followings[userId]
-        for u in users:
-            for t in self.tweets[u][-10:]:
-                heappush(heap, t)
-        res = []
-        while heap and len(res) < 10:
-            _, tweetId = heappop(heap)
-            res.append(tweetId)
-        return res
+        heaps = []
+        feeds = []
+
+        self.follows[userId].add(userId)
+        for followeeId in self.follows[userId]:
+            if not self.tweets[followeeId]: continue
+            timer, tweetId = self.tweets[followeeId][-1]
+            heappush(heaps, (-timer, tweetId, followeeId, len(self.tweets[followeeId]) - 1))
+        while heaps and len(feeds) < 10:
+            _, tweetId, uid, idx = heappop(heaps)
+            feeds.append(tweetId)
+            if idx - 1 >= 0:
+                timer, tweetId = self.tweets[uid][idx - 1]
+                heappush(heaps, (-timer, tweetId, uid, idx - 1))
+        return feeds
 
     def follow(self, followerId: int, followeeId: int) -> None:
         if followerId == followeeId: return
-        self.followings[followerId].add(followeeId)
+        self.follows[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId not in self.followings[followerId]: return
-        self.followings[followerId].remove(followeeId)
+        if followeeId not in self.follows[followerId]: return
+        self.follows[followerId].remove(followeeId)
 
 print(Twitter().postTweet(1, 5))
 print(Twitter().getNewsFeed(1))
