58104cd8736a0cc4ff57b50a4594b421a02c1917
kouta1612 <kouta1612world69@gmail.com>
2025-12-29 10:31:37 +0900

    リファクタリング

--- leetcode/python/_130/main.py
+++ leetcode/python/_130/main.py
@@ -5,9 +5,9 @@ class Solution:
         ROWS, COLS = len(board), len(board[0])
         def dfs(i, j: int):
             if i < 0 or j < 0 or i >= ROWS or j >= COLS: return
-            if board[i][j] == "X" or board[i][j] == "T": return
-            if board[i][j] == "O": board[i][j] = "T"
+            if board[i][j] != "O": return
 
+            board[i][j] = "T"
             for ni, nj in [[i+1, j], [i, j+1], [i-1, j], [i, j-1]]:
                 dfs(ni, nj)
         
@@ -21,8 +21,6 @@ class Solution:
         for i in range(ROWS):
             for j in range(COLS):
                 if board[i][j] == "O": board[i][j] = "X"
-        for i in range(ROWS):
-            for j in range(COLS):
-                if board[i][j] == "T": board[i][j] = "O"
+                elif board[i][j] == "T": board[i][j] = "O"
 
 print(Solution().solve([["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]))
