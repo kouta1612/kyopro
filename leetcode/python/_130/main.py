27d84127afcb799f596ed89052ccc24a6587f21c
kouta1612 <kouta1612world69@gmail.com>
2025-12-29 10:27:58 +0900

    130

--- leetcode/python/_130/main.py
+++ leetcode/python/_130/main.py
@@ -3,24 +3,26 @@ from typing import List
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
+        def dfs(i, j: int):
+            if i < 0 or j < 0 or i >= ROWS or j >= COLS: return
+            if board[i][j] == "X" or board[i][j] == "T": return
+            if board[i][j] == "O": board[i][j] = "T"
 
-        def dfs(r, c: int):
-            if r < 0 or c < 0 or r == ROWS or c == COLS: return
-            if board[r][c] != "O": return
-            board[r][c] = "T"
-            for nr, nc in [[r+1, c], [r-1, c], [r, c+1], [r, c-1]]:
-                dfs(nr, nc)
+            for ni, nj in [[i+1, j], [i, j+1], [i-1, j], [i, j-1]]:
+                dfs(ni, nj)
         
-        for r in range(ROWS):
-            if board[r][0] == "O": dfs(r, 0)
-            if board[r][COLS - 1] == "O": dfs(r, COLS - 1)
-        for c in range(COLS):
-            if board[0][c] == "O": dfs(0, c)
-            if board[ROWS - 1][c] == "O": dfs(ROWS - 1, c)
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "T": board[r][c] = "O"
-                elif board[r][c] == "O": board[r][c] = "X"
+        for i in range(ROWS):
+            if board[i][0] == "O": dfs(i, 0)
+            if board[i][COLS-1] == "O": dfs(i, COLS-1)
+        for j in range(COLS):
+            if board[0][j] == "O": dfs(0, j)
+            if board[ROWS-1][j] == "O": dfs(ROWS-1, j)
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == "O": board[i][j] = "X"
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == "T": board[i][j] = "O"
 
 print(Solution().solve([["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]))
