958a96bc9cb560531d29e576a16ebe531106f08c
kouta1612 <kouta1612world69@gmail.com>
2025-12-29 11:20:12 +0900

    684

--- leetcode/python/_684/main.py
+++ leetcode/python/_684/main.py
@@ -1,25 +1,30 @@
 from typing import List
 
+class UnionFind:
+    def __init__(self, n: int):
+        self.par = [-1] * n
+        self.rank = [1] * n
+
+    def root(self, x: int) -> int:
+        if self.par[x] == -1: return x
+        self.par[x] = self.root(self.par[x])
+        return self.par[x]
+
+    def unite(self, x, y: int) -> bool:
+        rx, ry = self.root(x), self.root(y)
+        if rx == ry: return False
+
+        if self.rank[rx] < self.rank[ry]: rx, ry = ry, rx
+        self.par[ry] = rx
+        self.rank[rx] += self.rank[ry]
+        return True
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        par = [-1] * (n + 1)
-        rank = [0] * (n + 1)
-
-        def root(node: int) -> int:
-            if par[node] == -1: return node
-            par[node] = root(par[node])
-            return par[node]
-
-        def union(x, y: int) -> bool:
-            rx, ry = root(x), root(y)
-            if rx == ry: return False
-            if rank[rx] < rank[ry]: rx, ry = ry, rx
-            par[rx] = ry
-            if rank[rx] == rank[ry]: rx += 1
-            return True
-
+        union = UnionFind(len(edges))
         for s, t in edges:
-            if not union(s, t): return [s, t]
+            s -= 1
+            t -= 1
+            if not union.unite(s, t): return [s + 1, t + 1]
 
 print(Solution().findRedundantConnection([[1,2], [1,3], [2,3]]))
