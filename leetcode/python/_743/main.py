14345368802834b95584f2c0b412df2c8a0e5531
kouta1612 <kouta1612world69@gmail.com>
2025-12-29 14:20:11 +0900

    743

--- leetcode/python/_743/main.py
+++ leetcode/python/_743/main.py
@@ -4,19 +4,17 @@ from heapq import heappop, heappush
 
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = defaultdict(list)
-        for s, t, w in times: graph[s].append((t, w))
-        visited = set()
-        res = 0
-        heap = [(0, k)]
+        graph = {i: [] for i in range(n)}
+        for u, v, t in times: graph[u-1].append((v-1, t))
+
+        dist = {}
+        heap = [(0, k - 1)]
         while heap:
-            w, node = heappop(heap)
-            if node in visited: continue
-            visited.add(node)
-            res = max(res, w)
-            for t, w2 in graph[node]:
-                if t in visited: continue
-                heappush(heap, (w + w2, t))
-        return res if len(visited) == n else -1
+            t, u = heappop(heap)
+            if u in dist: continue
+            dist[u] = t
+            if len(dist) == n: return max(dist.values())
+            for v, w in graph[u]: heappush(heap, (t + w, v))
+        return -1
 
 print(Solution().networkDelayTime([[2,1,1],[2,3,1],[3,4,1]], 4, 2))
