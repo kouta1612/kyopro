595f2d1dd6ad5c51e4b691f3feb1552f2b2b3378
kouta1612 <kouta1612world69@gmail.com>
2025-12-30 10:41:00 +0900

    787

--- leetcode/python/_787/main.py
+++ leetcode/python/_787/main.py
@@ -4,19 +4,23 @@ from heapq import heappop, heappush
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(list)
-        for s, t, w in flights: graph[s].append((t, w))
-        best = defaultdict(lambda: defaultdict(lambda: float('inf')))
-        best[src][k + 1] = 0
-        heap = [(0, src, k + 1)]
+        graph = {i: [] for i in range(n)}
+        for u, v, c in flights: graph[u].append((v, c))
+        heap = [(0, src, 0)]
+        visited = set()
+        res = float('inf')
         while heap:
-            cost, s, remain = heappop(heap)
-            if s == dst: return cost
-            if remain == 0: continue
-            for to, w in graph[s]:
-                if cost + w < best[to][remain - 1]:
-                    best[to][remain - 1] = cost + w
-                    heappush(heap, (best[to][remain - 1], to, remain - 1))
-        return -1
+            cost, start, move = heappop(heap)
+            if move > k + 1: continue
+            if (start, move) in visited: continue
+            if start == dst:
+                res = min(res, cost)
+                continue
+
+            visited.add((start, move))
+            for end, w in graph[start]:
+                if (end, move + 1) in visited: continue
+                heappush(heap, (cost + w, end, move + 1))
+        return res if res != float('inf') else -1
 
 print(Solution().findCheapestPrice(4, [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], 0, 3, 1))
