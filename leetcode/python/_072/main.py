566e3c7e35418fa393e99b75346bd2a226acac41
kouta1612 <kouta1612world69@gmail.com>
2025-12-31 11:41:27 +0900

    072

--- leetcode/python/_072/main.py
+++ leetcode/python/_072/main.py
@@ -1,15 +1,14 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         ROWS, COLS = len(word1), len(word2)
-        dp = [[float("inf")] * (COLS + 1) for _ in range(ROWS + 1)]
-        for r in range(ROWS + 1): dp[r][COLS] = ROWS - r
-        for c in range(COLS + 1): dp[ROWS][c] = COLS - c
-        for r in range(ROWS - 1, -1, -1):
-            for c in range(COLS - 1, -1, -1):
-                if word1[r] == word2[c]:
-                    dp[r][c] = dp[r+1][c+1]
-                else:
-                    dp[r][c] = 1 + min(dp[r+1][c], dp[r][c+1], dp[r+1][c+1])
-        return dp[0][0]
+        dp = [[float('inf')] * (COLS + 1) for _ in range(ROWS + 1)]
+        for r in range(ROWS + 1):
+            for c in range(COLS + 1):
+                if r == 0 and c == 0: dp[r][c] = 0
+                elif r == 0: dp[r][c] = c
+                elif c == 0: dp[r][c] = r
+                elif word1[r-1] == word2[c-1]: dp[r][c] = dp[r-1][c-1]
+                else: dp[r][c] = 1 + min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1])
+        return dp[ROWS][COLS]
 
 print(Solution().minDistance("horse", "ros"))
