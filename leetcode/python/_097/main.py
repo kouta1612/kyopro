2893f874a934ada4067cfa131749d0b56ed3fae5
kouta1612 <kouta1612world69@gmail.com>
2025-12-31 10:39:22 +0900

    097

--- leetcode/python/_097/main.py
+++ leetcode/python/_097/main.py
@@ -2,21 +2,13 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3): return False
 
-        dp = {}
-
-        def dfs(i, j: int) -> bool:
-            if i == len(s1) and j == len(s2): return True
-            if (i, j) in dp: return dp[(i, j)]
-
-            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
-                dp[(i, j)] = True
-                return True
-            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
-                dp[(i, j)] = True
-                return True
-            dp[(i, j)] = False
-            return False
-
-        return dfs(0, 0)
+        n1, n2 = len(s1), len(s2)
+        dp = [[False] * (n2 + 1) for _ in range(n1 + 1)]
+        dp[0][0] = True
+        for i in range(n1 + 1):
+            for j in range(n2 + 1):
+                if i-1 >= 0 and s1[i-1] == s3[i+j-1]: dp[i][j] |= dp[i-1][j]
+                if j-1 >= 0 and s2[j-1] == s3[i+j-1]: dp[i][j] |= dp[i][j-1]
+        return dp[n1][n2]
 
 print(Solution().isInterleave("aabcc", "dbbca", "aadbbcbcac"))
