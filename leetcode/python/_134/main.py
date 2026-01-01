f0a8026203da379fe297c682e0957d113b4a8704
kouta1612 <kouta1612world69@gmail.com>
2026-01-01 15:09:18 +0900

    134

--- leetcode/python/_134/main.py
+++ leetcode/python/_134/main.py
@@ -3,13 +3,12 @@ from typing import List
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         if sum(gas) < sum(cost): return -1
-        total = 0
-        res = 0
+        total, start = 0, 0
         for i in range(len(gas)):
             total += gas[i] - cost[i]
             if total < 0:
                 total = 0
-                res = i + 1
-        return res
+                start = i + 1
+        return start
 
 print(Solution().canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]))
