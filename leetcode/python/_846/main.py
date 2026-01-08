3c4d9f0daf3607068dbca06c52e42e2ff5752fd6
kouta1612 <kouta1612world69@gmail.com>
2026-01-08 09:13:11 +0900

    846

--- leetcode/python/_846/main.py
+++ leetcode/python/_846/main.py
@@ -1,22 +1,15 @@
-from heapq import heappop, heapify
-from collections import defaultdict
 from typing import List
+from collections import Counter
 
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize != 0: return False
-        
-        counter = defaultdict(int)
-        for num in hand: counter[num] += 1
-        heap = list(counter.keys())
-        heapify(heap)
 
-        while heap:
-            first = heap[0]
-            for i in range(first, first + groupSize):
-                if i not in counter: return False
-                counter[i] -= 1
-                if counter[i] == 0: 
-                    del counter[i]
-                    if heappop(heap) != i: return False
+        counter = Counter(hand)
+        for x in sorted(counter):
+            freq = counter[x]
+            if freq <= 0: continue
+            for i in range(groupSize):
+                if counter[x + i] < freq: return False
+                counter[x + i] -= freq
         return True
