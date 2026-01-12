1b1364c3b6bd50471b71e3aa9a0adcda89e2eda4
kouta1612 <kouta1612world69@gmail.com>
2026-01-12 16:40:34 +0900

    004

---
+++ leetcode/python/_004/main.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        if len(nums1) > len(nums2): nums1, nums2 = nums2, nums1
+
+        m, n = len(nums1), len(nums2)
+        total = m + n
+        half = total // 2
+        l, r = 0, m
+        while True:
+            i = (l + r) // 2
+            j = half - i
+            nums1_left = nums1[i-1] if i > 0 else float('-inf')
+            nums1_right = nums1[i] if i < m else float('inf')
+            nums2_left = nums2[j-1] if j > 0 else float('-inf')
+            nums2_right = nums2[j] if j < n else float('inf')
+            if nums1_left <= nums2_right and nums2_left <= nums1_right:
+                if total % 2 == 1: return min(nums1_right, nums2_right)
+                else: return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
+            elif nums1_left > nums2_right:
+                r = i - 1
+            else:
+                l = i + 1
+
+print(Solution().findMedianSortedArrays([1, 3], [2]))
