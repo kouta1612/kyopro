613986ff8da1a9dae9b66458d57ae328f0790799
kouta1612 <kouta1612world69@gmail.com>
2026-01-15 09:37:22 +0900

    778

--- leetcode/python/_778/main.py
+++ leetcode/python/_778/main.py
@@ -3,17 +3,22 @@ from heapq import heappop, heappush
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        ROWS =  COLS = len(grid)
-        heap = [[grid[0][0], 0, 0]]
-        visited = set[(int, int)]()
+        n = len(grid)
+        heap = [(grid[0][0], 0, 0)]
+        visited = set()
+
         while heap:
-            v, r, c = heappop(heap)
-            if r == ROWS - 1 and c == COLS - 1: return v
-            if (r, c) in visited: continue
-            visited.add((r, c))
-            for nr, nc in [[r+1, c], [r-1, c], [r, c+1], [r, c-1]]:
-                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
-                if (nr, nc) in visited: continue
-                heappush(heap, [max(v, grid[nr][nc]), nr, nc])
+            cost, i, j = heappop(heap)
+            if i == n - 1 and j == n - 1: return cost
+            if (i, j) in visited: continue
+            visited.add((i, j))
+
+            dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            for di, dj in dirs:
+                ni, nj = di + i, dj + j
+                if ni < 0 or nj < 0 or ni >= n or nj >= n: continue
+                if (ni, nj) in visited: continue
+                next_cost = max(cost, grid[ni][nj])
+                heappush(heap, (next_cost, ni, nj))
 
 print(Solution().swimInWater([[0,2],[1,3]]))
