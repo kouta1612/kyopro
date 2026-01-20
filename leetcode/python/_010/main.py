73c86de18891ed2cf2cb61edd00b9f636012c63b
kouta1612 <kouta1612world69@gmail.com>
2026-01-20 09:34:09 +0900

    010

--- leetcode/python/_010/main.py
+++ leetcode/python/_010/main.py
@@ -1,22 +1,20 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        cache = {}
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
 
-        def dfs(i, j: int) -> bool:
-            if (i, j) in cache: return cache[(i, j)]
-            if i >= len(s) and j >= len(p): return True
-            if j >= len(p): return False
-
-            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
-            if j + 1 < len(p) and p[j + 1] == "*":
-                cache[(i, j)] = (match and dfs(i + 1, j)) or dfs(i, j + 2)
-                return cache[(i, j)]
-            if match:
-                cache[(i, j)] = dfs(i + 1, j + 1)
-                return cache[(i, j)]
-            cache[(i, j)] = False
-            return cache[(i, j)]
-
-        return dfs(0, 0)
+        for j in range(2, n + 1):
+            if p[j-1] == "*": dp[0][j] = dp[0][j-2]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i-1] == p[j-1] or p[j-1] == ".":
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == "*":
+                    dp[i][j] = dp[i][j-2]
+                    if s[i-1] == p[j-2] or p[j-2] == ".":
+                        dp[i][j] |= dp[i-1][j]
+        return dp[m][n]
 
 print(Solution().isMatch("aa", "a"))
