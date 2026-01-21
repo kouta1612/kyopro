55f48da460fd8c81716c2761fb2ff0ef72831875
kouta1612 <kouta1612world69@gmail.com>
2026-01-21 09:20:44 +0900

    1851

--- leetcode/python/_1851/main.py
+++ leetcode/python/_1851/main.py
@@ -4,16 +4,19 @@ from heapq import heappush, heappop
 class Solution:
     def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
         intervals.sort()
-        res, i = {}, 0
+        qs = sorted([q, i] for i, q in enumerate(queries))
+        i = 0
         heap = []
-        for q in sorted(queries):
-            while i < len(intervals) and intervals[i][0] <= q:
+        res = [-1] * len(qs)
+        for v, qi in qs:
+            while i < len(intervals) and intervals[i][0] <= v:
                 l, r = intervals[i]
-                heappush(heap, (r - l + 1, r))
+                heappush(heap, (r-l+1, r))
                 i += 1
-            while heap and heap[0][1] < q:
+            while heap and heap[0][1] < v:
                 heappop(heap)
-            res[q] = heap[0][0] if heap else -1
-        return [res[q] for q in queries]
+            if heap:
+                res[qi] = heap[0][0]
+        return res
 
 print(Solution().minInterval([[1, 4], [2, 8], [3, 6]], [2, 3, 4]))
