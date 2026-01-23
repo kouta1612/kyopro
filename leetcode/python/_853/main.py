a245016155c1b6196ab5fd4d73be7f24b78574be
kouta1612 <kouta1612world69@gmail.com>
2026-01-24 08:21:15 +0900

    853

--- leetcode/python/_853/main.py
+++ leetcode/python/_853/main.py
@@ -2,13 +2,14 @@ from typing import List
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        n = len(speed)
-        items = [(position[i], (target - position[i]) / speed[i]) for i in range(n)]
-        items.sort()
-
-        stack = []
-        for pos, sec in reversed(items):
-            if not stack or stack[-1] < sec: stack.append(sec)
-        return len(stack)
+        cars = sorted(zip(position, speed), reverse=True)
+        cur = 0.0
+        res = 0
+        for pos, spd in cars:
+            time = (target - pos) / spd
+            if time <= cur: continue
+            res += 1
+            cur = time
+        return res
 
 print(Solution().carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]))
