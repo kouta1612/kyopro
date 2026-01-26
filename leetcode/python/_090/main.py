94f25cdcb107f220988150f527169fa2039cc37f
kouta1612 <kouta1612world69@gmail.com>
2026-01-26 09:26:59 +0900

    リファクタリング

--- leetcode/python/_090/main.py
+++ leetcode/python/_090/main.py
@@ -2,12 +2,13 @@ from typing import List
 
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         nums.sort()
         res = []
 
         def dfs(start: int, path: List[int]):
             res.append(path[:])
-            for i in range(start, len(nums)):
+            for i in range(start, n):
                 if i > start and nums[i] == nums[i-1]: continue
                 path.append(nums[i])
                 dfs(i+1, path)
