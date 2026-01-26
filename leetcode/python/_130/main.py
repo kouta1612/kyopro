c4b43dfc638dd4c7923ac8dbb869a2409b014f60
kouta1612 <kouta1612world69@gmail.com>
2026-01-27 08:28:59 +0900

    130

--- leetcode/python/_130/main.py
+++ leetcode/python/_130/main.py
@@ -3,24 +3,26 @@ from typing import List
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         ROWS, COLS = len(board), len(board[0])
-        def dfs(i, j: int):
-            if i < 0 or j < 0 or i >= ROWS or j >= COLS: return
-            if board[i][j] != "O": return
+        for r in range(ROWS):
+            if board[r][0] == "O": board[r][0] = "T"
+            if board[r][COLS-1] == "O": board[r][COLS-1] = "T"
+        for c in range(COLS):
+            if board[0][c] == "O": board[0][c] = "T"
+            if board[ROWS-1][c] == "O": board[ROWS-1][c] = "T"
+        
+        def dfs(r, c: int):
+            for nr, nc in [[r+1, c], [r, c+1], [r-1, c], [r, c-1]]:
+                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
+                if board[nr][nc] != "O": continue
+                board[nr][nc] = "T"
+                dfs(nr, nc)
 
-            board[i][j] = "T"
-            for ni, nj in [[i+1, j], [i, j+1], [i-1, j], [i, j-1]]:
-                dfs(ni, nj)
-        
-        for i in range(ROWS):
-            if board[i][0] == "O": dfs(i, 0)
-            if board[i][COLS-1] == "O": dfs(i, COLS-1)
-        for j in range(COLS):
-            if board[0][j] == "O": dfs(0, j)
-            if board[ROWS-1][j] == "O": dfs(ROWS-1, j)
-        
-        for i in range(ROWS):
-            for j in range(COLS):
-                if board[i][j] == "O": board[i][j] = "X"
-                elif board[i][j] == "T": board[i][j] = "O"
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "T": dfs(r, c)
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O": board[r][c] = "X"
+                elif board[r][c] == "T": board[r][c] = "O"
 
 print(Solution().solve([["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]))
