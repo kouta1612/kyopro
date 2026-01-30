fc08716cc65d7443be2386fb572605e8414b7cdd
kouta1612 <kouta1612world69@gmail.com>
2026-01-31 08:27:17 +0900

    518

--- leetcode/python/_518/main.py
+++ leetcode/python/_518/main.py
@@ -4,10 +4,9 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = [0] * (amount + 1)
         dp[0] = 1
-
         for coin in coins:
-            for v in range(coin, amount + 1):
-                dp[v] += dp[v-coin]
+            for v in range(0, amount + 1 - coin):
+                dp[v + coin] += dp[v]
         return dp[amount]
 
 print(Solution().change(5, [1,2,5]))
