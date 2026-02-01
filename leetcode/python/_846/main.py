65762de7988ceb054cd21cb467a0bee74b30dfce
kouta1612 <kouta1612world69@gmail.com>
2026-02-01 09:14:43 +0900

    846

--- leetcode/python/_846/main.py
+++ leetcode/python/_846/main.py
@@ -4,12 +4,23 @@ from collections import Counter
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
         if len(hand) % groupSize != 0: return False
-
-        counter = Counter(hand)
-        for x in sorted(counter):
-            freq = counter[x]
-            if freq <= 0: continue
+        count = Counter(hand)
+        for x in sorted(count):
+            if count[x] <= 0: continue
+            freq = count[x]
             for i in range(groupSize):
-                if counter[x + i] < freq: return False
-                counter[x + i] -= freq
+                if count[x+i] < freq: return False
+                count[x+i] -= freq
         return True
+
+print(Solution().isNStraightHand([1,2,3,6,2,3,4,7,8], 3))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 4))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 3))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 2))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 1))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 0))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 7))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 8))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 9))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 10))
+print(Solution().isNStraightHand([1,2,3,4,5,6], 11))
