b87c70edf0e1a1ceab2d4fe175c062ba175aaad7
kouta1612 <kouta1612world69@gmail.com>
2026-02-04 08:00:37 +0900

    007

--- leetcode/python/_007/main.py
+++ leetcode/python/_007/main.py
@@ -2,16 +2,18 @@ from math import fmod
 
 class Solution:
     def reverse(self, x: int) -> int:
-        MAX_VALUE = 2**31 - 1
+        MIN, MAX = -2**31, 2**31-1
 
         sign = 1 if x >= 0 else -1
         x = abs(x)
-        res = 0
-        while x != 0:
-            digit = x % 10
+
+        cur = 0
+        while x > 0:
+            if sign > 0 and ((cur > MAX // 10) or ((cur == MAX // 10) and (x > 7))): return 0
+            if sign < 0 and ((cur > MAX // 10) or ((cur == MAX // 10) and (x > 8))): return 0
+
+            cur = cur * 10 + x % 10
             x //= 10
-            if res > MAX_VALUE // 10 or (res == MAX_VALUE and digit > 7): return 0
-            res = res * 10 + digit
-        return res * sign
+        return cur * sign
 
 print(Solution().reverse(123))
