eec5dc8c32fc87e28326329299057693b0fee51b
kouta1612 <kouta1612world69@gmail.com>
2026-02-09 08:25:20 +0900

    329

--- leetcode/python/_329/main.py
+++ leetcode/python/_329/main.py
@@ -1,23 +1,27 @@
 from types import List
+from collections import defaultdict
 
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        n, m = len(matrix), len(matrix[0])
-        dp = [[0] * m for _ in range(n)]
+        ROWS, COLS = len(matrix), len(matrix[0])
+        cache = defaultdict(int)
 
-        def dfs(i, j: int) -> int:
-            if dp[i][j] > 0: return dp[i][j]
-            res = 1
-            for ni, nj in [(i+1, j), (i, j+1), (i-1, j), (i, j-1)]:
-                if 0 <= ni < n and 0 <= nj < m and matrix[ni][nj] > matrix[i][j]:
-                   res = max(res, 1 + dfs(ni, nj)) 
-            dp[i][j] = res
-            return dp[i][j]
+        def dfs(r, c: int) -> int:
+            if (r, c) in cache: return cache[(r, c)]
+
+            now = 1
+            for nr, nc in [[r+1, c], [r, c+1], [r-1, c], [r, c-1]]:
+                if nr < 0 or nc < 0 or nr >= ROWS or nc >= COLS: continue
+                if matrix[r][c] >= matrix[nr][nc]: continue
+                now = max(now, 1 + dfs(nr, nc))
+
+            cache[(r, c)] = now
+            return now
         
         res = 0
-        for i in range(n):
-            for j in range(m):
-                res = max(res, dfs(i, j))
+        for r in range(ROWS):
+            for c in range(COLS):
+                res = max(res, dfs(r, c))
         return res
 
 print(Solution().longestIncreasingPath([[9,9,4],[6,6,8],[2,1,1]]))
