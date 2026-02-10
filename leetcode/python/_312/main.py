915916fa9f0362fa76df57eb6380f1ae8df8cf4c
kouta1612 <kouta1612world69@gmail.com>
2026-02-10 13:13:49 +0900

    312

--- leetcode/python/_312/main.py
+++ leetcode/python/_312/main.py
@@ -3,20 +3,16 @@ from types import List
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
-        dp = {}
-
-        def dfs(l, r: int) -> int:
-            if l > r: return 0
-            if (l, r) in dp: return dp[(l, r)]
-
-            dp[(l, r)] = 0
-            for i in range(l, r + 1):
-                s = nums[l - 1] * nums[i] * nums[r + 1]
-                s += dfs(l, i - 1) + dfs(i + 1, r)
-                dp[(l, r)] = max(dp[(l, r)], s)
-            
-            return dp[(l, r)]
-
-        return dfs(1, len(nums) - 2)
+        n = len(nums)
+        dp = [[0] * n for _ in range(n)]
+        for length in range(2, n):
+            for left in range(n - length):
+                right = left + length
+                for k in range(left + 1, right):
+                    dp[left][right] = max(
+                        dp[left][right],
+                        dp[left][k] + dp[k][right] + nums[left] * nums[k] * nums[right]
+                    )
+        return dp[0][n-1]
 
 print(Solution().maxCoins([3,1,5,8]))
