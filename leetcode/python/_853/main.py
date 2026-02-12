af0b29feea5a530725a797dfb952e30a49d4ec35
kouta1612 <kouta1612world69@gmail.com>
2026-02-12 09:04:31 +0900

    853

--- leetcode/python/_853/main.py
+++ leetcode/python/_853/main.py
@@ -2,14 +2,13 @@ from typing import List
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars = sorted(zip(position, speed), reverse=True)
-        cur = 0.0
-        res = 0
-        for pos, spd in cars:
-            time = (target - pos) / spd
-            if time <= cur: continue
-            res += 1
-            cur = time
-        return res
+        cars = sorted([[pos, sp] for pos, sp in zip(position, speed)], reverse=True)
+        pre, fleet = 0, 0
+        for pos, sp in cars:
+            t = (target - pos) / sp
+            if pre < t: 
+                fleet += 1
+                pre = t
+        return fleet
 
 print(Solution().carFleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]))
