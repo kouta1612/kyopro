a642f6d02871768174f2ef65568e1c3f3a68c108
kouta1612 <kouta1612world69@gmail.com>
2026-02-14 09:05:53 +0900

    146

--- leetcode/python/_146/main.py
+++ leetcode/python/_146/main.py
@@ -1,48 +1,47 @@
-class Node:
-    def __init__(self, key, val: int):
-        self.key = key
-        self.val = val
+class Node: 
+    def __init__(self, key, value: int):
         self.prev = None
         self.next = None
+        self.key = key
+        self.value = value
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.store = {}
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next = self.right
-        self.right.prev = self.left
+        self.left, self.right = Node(-1, -1), Node(-1, -1)
+        self.left.next, self.right.prev = self.right, self.left
 
-    def insert(self, node: Node):
-        prev = self.right.prev
-        prev.next = node
-        node.prev = prev
-        node.next = self.right
-        self.right.prev = node
+    def remove(self, key: int):
+        node = self.store[key]
+        pre, nxt = node.prev, node.next
+        pre.next, nxt.prev = nxt, pre
 
-    def remove(self, node: Node):
-        prev = node.prev
-        nxt = node.next
-        prev.next, nxt.prev = nxt, prev
+    def add(self, key: int):
+        node = self.store[key]
+        pre, nxt = self.right.prev, self.right
+        pre.next, nxt.prev = node, node
+        node.prev, node.next = pre, nxt
 
     def get(self, key: int) -> int:
         if key not in self.store: return -1
-
-        node = self.store[key]
-        self.remove(node)
-        self.insert(node)
-
-        return self.store[key].val
+        self.remove(key)
+        self.add(key)
+        return self.store[key].value
 
     def put(self, key: int, value: int) -> None:
-        if key in self.store: self.remove(self.store[key])
-        node = Node(key, value)
-        self.store[key] = node
-        self.insert(node)
+        if key in self.store:
+            self.store[key].value = value
+            self.remove(key)
+            self.add(key)
+            return
+
+        self.store[key] = Node(key, value)
+        self.add(key)
         if len(self.store) > self.cap:
             lru = self.left.next
-            self.remove(lru)
+            self.remove(lru.key)
             del self.store[lru.key]
 
 print(LRUCache(2).put(1, 1))
