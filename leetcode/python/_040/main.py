0ba4451f4800aa360e00c9dda3a5e647f87a94fb
kouta1612 <kouta1612world69@gmail.com>
2026-02-15 15:17:57 +0900

    040

--- leetcode/python/_040/main.py
+++ leetcode/python/_040/main.py
@@ -4,20 +4,20 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         n = len(candidates)
         candidates.sort()
-        res = []
+        res, path = [], []
 
-        def dfs(i, rem: int, cur: List[int]):
+        def dfs(i, rem: int):
             if rem == 0:
-                res.append(cur[:])
+                res.append(path[:])
                 return
             if rem < 0: return
 
             for j in range(i, n):
                 if j > i and candidates[j] == candidates[j-1]: continue
-                cur.append(candidates[j])
-                dfs(j+1, rem-candidates[j], cur)
-                cur.pop()
-        dfs(0, target, [])
+                path.append(candidates[j])
+                dfs(j+1, rem-candidates[j])
+                path.pop()
+        dfs(0, target)
         return res
 
 print(Solution().combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
