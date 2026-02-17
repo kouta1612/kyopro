aab6f89805519fbf861cffe8a529a955ad56a4e0
kouta1612 <kouta1612world69@gmail.com>
2026-02-18 06:33:27 +0900

    416

--- leetcode/python/_416/main.py
+++ leetcode/python/_416/main.py
@@ -4,14 +4,18 @@ from collections import defaultdict
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-        target = total // 2
         if total % 2 == 1: return False
 
-        dp = [False] * (target+1)
+        nums.sort()
+        n = len(nums)
+        target = total // 2
+        dp = [False] * (target + 1)
         dp[0] = True
-        for num in nums:
-            for i in range(target, num-1, -1):
-                dp[i] |= dp[i-num]
+        for i in range(n):
+            nextDP = dp[:]
+            for v in range(nums[i], target + 1):
+                if dp[v-nums[i]]: nextDP[v] = True
+            dp = nextDP
         return dp[target]
 
 print(Solution().canPartition([1,5,11,5]))
