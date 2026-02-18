75910fc722751401c89d6659aa521b9ee115ed19
kouta1612 <kouta1612world69@gmail.com>
2026-02-19 06:51:23 +0900

    045

--- leetcode/python/_045/main.py
+++ leetcode/python/_045/main.py
@@ -4,14 +4,15 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
         if n == 1: return 0
-
-        farthest, end, res = 0, 0, 0
-        for i in range(n):
+        res = 0
+        end = 0
+        farthest = 0
+        for i in range(n-1):
             farthest = max(farthest, i + nums[i])
             if i == end:
                 end = farthest
                 res += 1
-                if end >= n - 1: break
-        return res
+                if end >= n-1: return res
+        return -1
 
 print(Solution().jump([2,3,1,1,4]))
