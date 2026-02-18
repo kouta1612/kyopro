05fdfc53c883018ae3942f6aaba7e0c6414fdc07
kouta1612 <kouta1612world69@gmail.com>
2026-02-18 09:17:27 +0900

    097

--- leetcode/python/_097/main.py
+++ leetcode/python/_097/main.py
@@ -1,16 +1,19 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        m, n = len(s1), len(s2)
-        if m + n != len(s3): return False
+        n1, n2, n3 = len(s1), len(s2), len(s3)
+        if n1 + n2 != n3: return False
 
-        dp = [[False] * (n+1) for _ in range(m+1)]
+        dp = [[False] * (n2+1) for _ in range(n1+1)]
         dp[0][0] = True
-        for i in range(m+1):
-            for j in range(n+1):
-                if i > 0 and s1[i-1] == s3[i+j-1]:
-                    dp[i][j] |= dp[i-1][j]
-                if j > 0 and s2[j-1] == s3[i+j-1]:
-                    dp[i][j] |= dp[i][j-1]
-        return dp[m][n]
+        for i in range(1, n1+1):
+            if dp[i-1][0] and s1[i-1] == s3[i-1]: dp[i][0] = True
+        for j in range(1, n2+1):
+            if dp[0][j-1] and s2[j-1] == s3[j-1]: dp[0][j] = True
+
+        for i in range(1, n1+1):
+            for j in range(1, n2+1):
+                if dp[i-1][j] and s1[i-1] == s3[i+j-1]: dp[i][j] = True
+                if dp[i][j-1] and s2[j-1] == s3[i+j-1]: dp[i][j] = True
+        return dp[n1][n2]
 
 print(Solution().isInterleave("aabcc", "dbbca", "aadbbcbcac"))
