e9dbce147e92339c6549beedfd8d84d3605efa1e
kouta1612 <kouta1612world69@gmail.com>
2026-02-21 08:38:11 +0900

    239

--- leetcode/python/_239/main.py
+++ leetcode/python/_239/main.py
@@ -3,13 +3,14 @@ from collections import deque
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        dq = deque()
+        deq = deque()
+        n = len(nums)
         res = []
-        for i, num in enumerate(nums):
-            if dq and dq[0] == i-k: dq.popleft()
-            while dq and nums[dq[-1]] < num: dq.pop()
-            dq.append(i)
-            if i >= k-1: res.append(nums[dq[0]])
+        for i in range(n):
+            if deq and deq[0] == i-k: deq.popleft()
+            while deq and nums[deq[-1]] < nums[i]: deq.pop()
+            deq.append(i)
+            if i >= k - 1: res.append(nums[deq[0]])
         return res
 
 print(Solution().maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
