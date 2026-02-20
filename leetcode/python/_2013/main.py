7fc643262672cedd7982804ede3bd7f856d2a9c8
kouta1612 <kouta1612world69@gmail.com>
2026-02-20 09:10:56 +0900

    2013

--- leetcode/python/_2013/main.py
+++ leetcode/python/_2013/main.py
@@ -4,22 +4,22 @@ from typing import List
 class DetectSquares:
 
     def __init__(self):
-        self.xs = defaultdict(set)
-        self.xys = defaultdict(int)
+        self.ys = defaultdict(set)
+        self.cnt = defaultdict(int)
 
     def add(self, point: List[int]) -> None:
-        x,y = point
-        self.xs[x].add(y)
-        self.xys[(x,y)] += 1
+        x, y = point
+        self.cnt[(x, y)] += 1
+        self.ys[x].add(y)
 
     def count(self, point: List[int]) -> int:
+        px, py = point
         res = 0
-        x, y = point
-        for ny in self.xs[x]:
-            if y == ny: continue
-            side = y - ny
-            res += self.xys[(x,ny)] * self.xys[(x-side, ny)] * self.xys[(x-side, y)]
-            res += self.xys[(x,ny)] * self.xys[(x+side, ny)] * self.xys[(x+side, y)]
+        for y in self.ys[px]:
+            if py == y: continue
+            d = abs(py - y)
+            res += self.cnt[(px-d, py)] * self.cnt[(px-d, y)] * self.cnt[(px, y)]
+            res += self.cnt[(px+d, py)] * self.cnt[(px+d, y)] * self.cnt[(px, y)]
         return res
 
 print(DetectSquares().count([3,4]))
