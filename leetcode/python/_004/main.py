40a2d2b6e54bc45355f35e46e6d77f0144aed426
kouta1612 <kouta1612world69@gmail.com>
2026-02-23 07:46:34 +0900

    004

--- leetcode/python/_004/main.py
+++ leetcode/python/_004/main.py
@@ -3,26 +3,21 @@ from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         if len(nums1) > len(nums2): nums1, nums2 = nums2, nums1
-
         m, n = len(nums1), len(nums2)
         total = m + n
         half = total // 2
         l, r = 0, m
-        while l <= r:
+        while True:
             i = (l + r) // 2
             j = half - i
-            nums1_left = nums1[i-1] if i > 0 else float('-inf')
-            nums1_right = nums1[i] if i < m else float('inf')
-            nums2_left = nums2[j-1] if j > 0 else float('-inf')
-            nums2_right = nums2[j] if j < n else float('inf')
-            if nums1_left <= nums2_right and nums2_left <= nums1_right:
-                if (m + n) % 2 == 1: 
-                    return min(nums1_right, nums2_right)
-                else: 
-                    return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
-            elif nums1_left > nums2_right:
-                r = i - 1
-            else:
-                l = i + 1
+            left1 = nums1[i-1] if i > 0 else float('-inf')
+            right1 = nums1[i] if i < m else float('inf')
+            left2 = nums2[j-1] if j > 0 else float('-inf')
+            right2 = nums2[j] if j < n else float('inf')
+            if left1 <= right2 and left2 <= right1:
+                if (m + n) % 2: return min(right1, right2)
+                else: return (max(left1, left2) + min(right1, right2)) / 2
+            elif left1 > right2: r = i-1
+            else: l = i+1
 
 print(Solution().findMedianSortedArrays([1, 3], [2]))
