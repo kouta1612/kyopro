1c23ba9ca04028effb4128cd0511e2e5dc2cd3ea
kouta1612 <kouta1612world69@gmail.com>
2026-02-26 08:19:11 +0900

    287

--- leetcode/python/_287/main.py
+++ leetcode/python/_287/main.py
@@ -2,11 +2,13 @@ from typing import List
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
         slow, fast = nums[0], nums[0]
         while True:
             slow, fast = nums[slow], nums[nums[fast]]
             if slow == fast: break
         slow = nums[0]
+
         while slow != fast:
             slow, fast = nums[slow], nums[fast]
         return slow
