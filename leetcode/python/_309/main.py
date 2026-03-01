dc3d2a8bf5d13374377efb0a3b80baff07c709d1
kouta1612 <kouta1612world69@gmail.com>
2026-03-02 06:42:27 +0900

    309

--- leetcode/python/_309/main.py
+++ leetcode/python/_309/main.py
@@ -3,14 +3,12 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        hold, sold, cool = float('-inf'), float('-inf'), 0
+        buy, sold, cool = float('-inf'), 0, 0
         for i in range(n):
-            prev_hold = hold
-            prev_sold = sold
-            prev_cool = cool
-            hold = max(prev_hold, prev_cool - prices[i])
-            sold = prev_hold + prices[i]
-            cool = max(prev_sold, prev_cool)
-        return max(hold, sold, cool)
+            pre_buy, pre_sold, pre_cool = buy, sold, cool
+            buy = max(buy, pre_cool - prices[i])
+            sold = max(sold, pre_buy + prices[i])
+            cool = max(cool, pre_sold)
+        return max(buy, sold, cool)
 
 print(Solution().maxProfit([1,2,3,0,2]))
