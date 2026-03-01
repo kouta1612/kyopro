d6ac4644e6560ac71fdc0419de2bf7ff41d19b17
kouta1612 <kouta1612world69@gmail.com>
2026-03-01 10:58:50 +0900

    787

--- leetcode/python/_787/main.py
+++ leetcode/python/_787/main.py
@@ -1,18 +1,21 @@
 from typing import List
 from collections import defaultdict
-from heapq import heappop, heappush
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        INF = float('inf')
-        dist = [INF] * n
-        dist[src] = 0
+        graph = defaultdict(list)
+        for start, end, cost in flights:
+            graph[start].append((end, cost))
+        
+        res = [float('inf')] * n
+        res[src] = 0
         for _ in range(k+1):
-            tmp = dist[:]
-            for u, v, c in flights:
-                if dist[u] == INF: continue
-                tmp[v] = min(tmp[v], dist[u]+c)
-            dist = tmp
-        return dist[dst] if dist[dst] != INF else -1
+            tmp = res[:]
+            for start in range(n):
+                if res[start] == float('inf'): continue
+                for end, cost in graph[start]:
+                    tmp[end] = min(tmp[end], cost+res[start])
+            res = tmp
+        return res[dst] if res[dst] != float('inf') else -1
 
 print(Solution().findCheapestPrice(4, [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], 0, 3, 1))
