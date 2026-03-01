1f2f3612d98bbe3ecf0142632cabf96ee779f2ed
kouta1612 <kouta1612world69@gmail.com>
2026-03-02 06:52:52 +0900

    678

--- leetcode/python/_678/main.py
+++ leetcode/python/_678/main.py
@@ -1,21 +1,14 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        n = len(s)
-        min_open, max_open = 0, 0
+        leftmin, leftmax = 0, 0
         for c in s:
-            if c == "(": 
-                min_open += 1
-                max_open += 1
-            elif c == ")":
-                min_open -= 1
-                max_open -= 1
-            else:
-                min_open -= 1
-                max_open += 1
+            if c == "(": leftmin, leftmax = leftmin + 1, leftmax + 1
+            elif c == ")": leftmin, leftmax = leftmin - 1, leftmax - 1
+            else: leftmin, leftmax = leftmin - 1, leftmax + 1
             
-            if max_open < 0: return False
-            if min_open < 0: min_open = 0
-        return min_open == 0
+            if leftmax < 0: return False
+            if leftmin < 0: leftmin = 0
+        return leftmin == 0
 
 print(Solution().checkValidString("()"))
 print(Solution().checkValidString("(*)"))
