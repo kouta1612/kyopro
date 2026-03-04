56c48c1dd7756ef1c657696436a0c8d65ff41eee
kouta1612 <kouta1612world69@gmail.com>
2026-03-05 08:17:55 +0900

    084

--- leetcode/python/_084/main.py
+++ leetcode/python/_084/main.py
@@ -4,16 +4,15 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         res = 0
         stack = []
+        heights.append(0)
         for i, h in enumerate(heights):
-            start = i
-            while stack and stack[-1][1] > h:
-                idx, height = stack.pop()
-                res = max(res, height * (i - idx))
-                start = idx
-            stack.append((start, h))
-        n = len(heights)
-        for i, h in stack:
-            res = max(res, h * (n - i))
+            while stack and heights[stack[-1]] > h: 
+                height = heights[stack.pop()]
+                left = -1
+                if stack: left = stack[-1]
+                width = i - left - 1
+                res = max(res, height * width)
+            stack.append(i)
         return res
 
 print(Solution().largestRectangleArea([2,1,5,6,2,3]))
