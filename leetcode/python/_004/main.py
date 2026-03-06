3ae9fcc3f2c0030cf988c674c223dd4d54cd1ffd
kouta1612 <kouta1612world69@gmail.com>
2026-03-06 09:39:32 +0900

    004

--- leetcode/python/_004/main.py
+++ leetcode/python/_004/main.py
@@ -15,9 +15,11 @@ class Solution:
             left2 = nums2[j-1] if j > 0 else float('-inf')
             right2 = nums2[j] if j < n else float('inf')
             if left1 <= right2 and left2 <= right1:
-                if (m + n) % 2: return min(right1, right2)
-                else: return (max(left1, left2) + min(right1, right2)) / 2
-            elif left1 > right2: r = i-1
-            else: l = i+1
+                if total % 2 == 0: return (max(left1, left2) + min(right1, right2)) / 2
+                else: return min(right1, right2)
+            elif left1 > right2:
+                r = i-1
+            else:
+                l = i + 1
 
 print(Solution().findMedianSortedArrays([1, 3], [2]))
