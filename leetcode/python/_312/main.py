9cb89f641f3d349479c5c5cd8b0a0dba7054258c
kouta1612 <kouta1612world69@gmail.com>
2026-03-08 08:23:56 +0900

    312

--- leetcode/python/_312/main.py
+++ leetcode/python/_312/main.py
@@ -4,12 +4,12 @@ class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
         n = len(nums)
-        dp = [[0] * (n+1) for _ in range(n+1)]
+        dp = [[0] * n for _ in range(n)]
         for length in range(2, n):
-            for left in range(0, n - length):
-                right = left + length
-                for i in range(left + 1, right):
-                    dp[left][right] = max(dp[left][right], dp[left][i] + dp[i][right] + nums[left] * nums[i] * nums[right])
+            for l in range(n - length):
+                r = l + length
+                for i in range(l + 1, r):
+                    dp[l][r] = max(dp[l][r], dp[l][i] + dp[i][r] + nums[l] * nums[i] * nums[r])
         return dp[0][n-1]
 
 print(Solution().maxCoins([3,1,5,8]))
