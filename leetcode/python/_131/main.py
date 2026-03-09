f5e39270845f3f9eeeb06ae9fc3ea6d93a7a2734
kouta1612 <kouta1612world69@gmail.com>
2026-03-10 06:59:50 +0900

    131

--- leetcode/python/_131/main.py
+++ leetcode/python/_131/main.py
@@ -9,13 +9,13 @@ class Solution:
             if start == n:
                 res.append(path[:])
                 return
-            for end in range(start, n):
-                if s[start:end+1] != s[start:end+1][::-1]: continue
-                path.append(s[start:end+1])
-                dfs(end+1, path)
-                path.pop()
+            for i in range(start, n):
+                if s[start:i+1] == s[start:i+1][::-1]:
+                    path.append(s[start:i+1])
+                    dfs(i+1, path)
+                    path.pop()
+        
         dfs(0, [])
         return res
-
 
 print(Solution().partition("aab"))
