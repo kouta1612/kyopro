d6f747ba4b079617ef1533d24d02cfb090122eec
kouta1612 <kouta1612world69@gmail.com>
2026-03-10 06:48:07 +0900

    567

--- leetcode/python/_567/main.py
+++ leetcode/python/_567/main.py
@@ -3,15 +3,15 @@ class Solution:
         m, n = len(s1), len(s2)
         if m > n: return False
 
-        count1, count2 = [0] * 26, [0] * 26
+        counter1, counter2 = [0] * 26, [0] * 26
         for i in range(m):
-            count1[ord(s1[i]) - ord('a')] += 1
-            count2[ord(s2[i]) - ord('a')] += 1
-        if count1 == count2: return True
+            counter1[ord(s1[i]) - ord('a')] += 1
+            counter2[ord(s2[i]) - ord('a')] += 1
+        if counter1 == counter2: return True
         for i in range(m, n):
-            count2[ord(s2[i]) - ord('a')] += 1
-            count2[ord(s2[i-m]) - ord('a')] -= 1
-            if count1 == count2: return True
+            counter2[ord(s2[i]) - ord('a')] += 1
+            counter2[ord(s2[i-m]) - ord('a')] -= 1
+            if counter1 == counter2: return True
         return False
 
 print(Solution().checkInclusion("ab", "eidbaooo"))
