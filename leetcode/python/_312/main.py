c72b0d8c7bc3d1020be1b10e6f8e788441d54c27
kouta1612 <kouta1612world69@gmail.com>
2026-03-16 06:40:37 +0900

    312

--- leetcode/python/_312/main.py
+++ leetcode/python/_312/main.py
@@ -6,9 +6,9 @@ class Solution:
         n = len(nums)
         dp = [[0] * n for _ in range(n)]
         for length in range(2, n):
-            for l in range(n - length):
+            for l in range(0, n - length):
                 r = l + length
-                for i in range(l + 1, r):
+                for i in range(l+1, r):
                     dp[l][r] = max(dp[l][r], dp[l][i] + dp[i][r] + nums[l] * nums[i] * nums[r])
         return dp[0][n-1]
 
