fd74dc15fb1c1ee1b3abb16c7186e7fea0cb93f6
kouta1612 <kouta1612world69@gmail.com>
2026-03-15 10:49:06 +0900

    115

--- leetcode/python/_115/main.py
+++ leetcode/python/_115/main.py
@@ -1,12 +1,12 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        ns, nt = len(s), len(t)
-        dp = [[0] * (nt + 1) for _ in range(ns + 1)]
-        for i in range(ns + 1): dp[i][0] = 1
-        for i in range(1, ns + 1):
-            for j in range(1, nt + 1):
+        m, n = len(s), len(t)
+        dp = [[0] * (n+1) for _ in range(m+1)]
+        for i in range(m+1): dp[i][0] = 1
+        for i in range(1, m+1):
+            for j in range(1, n+1):
                 dp[i][j] = dp[i-1][j]
                 if s[i-1] == t[j-1]: dp[i][j] += dp[i-1][j-1]
-        return dp[ns][nt]
+        return dp[m][n]
 
 print(Solution().numDistinct("rabbbit", "rabbit"))
