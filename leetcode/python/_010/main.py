f0247dd8e5da8555d3cb181b27c328e3385077de
kouta1612 <kouta1612world69@gmail.com>
2026-03-17 07:22:23 +0900

    010

--- leetcode/python/_010/main.py
+++ leetcode/python/_010/main.py
@@ -5,8 +5,8 @@ class Solution:
         dp[0][0] = True
         for i in range(2, n+1):
             if p[i-1] == "*": dp[0][i] = dp[0][i-2]
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
+        for i in range(1, m+1):
+            for j in range(1, n+1):
                 if s[i-1] == p[j-1] or p[j-1] == ".": dp[i][j] = dp[i-1][j-1]
                 elif p[j-1] == "*":
                     dp[i][j] = dp[i][j-2]
