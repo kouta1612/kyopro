9c9d6a72dd63f036d4eed9c1398fc56898291659
kouta1612 <kouta1612world69@gmail.com>
2026-03-19 07:05:10 +0900

    115

--- leetcode/python/_115/main.py
+++ leetcode/python/_115/main.py
@@ -5,8 +5,8 @@ class Solution:
         for i in range(m+1): dp[i][0] = 1
         for i in range(1, m+1):
             for j in range(1, n+1):
-                dp[i][j] = dp[i-1][j]
-                if s[i-1] == t[j-1]: dp[i][j] += dp[i-1][j-1]
+                if s[i-1] == t[j-1]: dp[i][j] = dp[i-1][j-1]
+                dp[i][j] += dp[i-1][j]
         return dp[m][n]
 
 print(Solution().numDistinct("rabbbit", "rabbit"))
