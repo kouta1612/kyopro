d7626591c00dddb38515ca9ed72fc0aecc6e5cda
kouta1612 <kouta1612world69@gmail.com>
2026-03-20 14:25:09 +0900

    a

---
+++ edp/A/go/main.py
@@ -0,0 +1,9 @@
+n = int(input())
+h = list(map(int, input().split()))
+
+dp = [float('inf')] * n
+dp[0] = 0
+dp[1] = abs(h[1] - h[0])
+for i in range(2, n):
+    dp[i] = min(dp[i-2] + abs(h[i-2] - h[i]), dp[i-1] + abs(h[i-1] - h[i]))
+print(dp[n-1])
