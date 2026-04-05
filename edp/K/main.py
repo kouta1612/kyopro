43138d37ed39dc718f4fcc3182c284ed87ecbc91
kouta1612 <kouta1612world69@gmail.com>
2026-04-06 06:32:56 +0900

    解き直し用に削除

--- edp/K/main.py
+++
@@ -1,9 +0,0 @@
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-dp = [False] * (k+1)
-for i in range(1, k+1):
-    for j in a:
-        if i-j >= 0 and not dp[i-j]:
-            dp[i] = True
-            break
-print("First" if dp[k] else "Second")
