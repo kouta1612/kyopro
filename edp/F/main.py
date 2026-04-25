af9383592fc3f59640026f183385e3e654792c49
kouta1612 <kouta1612world69@gmail.com>
2026-04-25 09:06:24 +0900

    f

--- edp/F/main.py
+++ edp/F/main.py
@@ -4,8 +4,9 @@ ns, nt = len(s), len(t)
 dp = [[0] * (nt+1) for _ in range(ns+1)]
 for i in range(1, ns+1):
     for j in range(1, nt+1):
-        if s[i-1] == t[j-1]: dp[i][j] = max(dp[i-1][j-1] + 1, dp[i-1][j], dp[i][j-1])
+        if s[i-1] == t[j-1]: dp[i][j] = dp[i-1][j-1] + 1
         else: dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
 res = []
 i, j = ns, nt
 while i > 0 and j > 0:
@@ -13,6 +14,7 @@ while i > 0 and j > 0:
         res.append(s[i-1])
         i -= 1
         j -= 1
-    elif dp[i][j-1] >= dp[i-1][j]: j -= 1
-    else: i -= 1
+    elif dp[i-1][j] >= dp[i][j-1]: i -= 1
+    else: j -= 1
+
 print("".join(res[::-1]))
