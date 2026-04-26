62bec7d5e5b7e3b2b54a64992b599ddcde5857e9
kouta1612 <kouta1612world69@gmail.com>
2026-04-26 11:06:29 +0900

    m

--- edp/M/main.py
+++ edp/M/main.py
@@ -8,8 +8,8 @@ for i in range(N+1): dp[i][0] = 1
 
 for i in range(1, N+1):
     prefix = [0] * (K+2)
-    for x in range(1, K+2): prefix[x] = (prefix[x-1] + dp[i-1][x-1]) % MOD
-    for j in range(1, K+1): 
-        left = max(0, j-a[i-1])
-        dp[i][j] = (prefix[j+1] - prefix[left]) % MOD
+    for j in range(1, K+2): prefix[j] = prefix[j-1] + dp[i-1][j-1]
+    for j in range(K+1):
+        dp[i][j] = prefix[j+1] - prefix[max(0, j-a[i-1])]
+        dp[i][j] %= MOD
 print(dp[N][K])
