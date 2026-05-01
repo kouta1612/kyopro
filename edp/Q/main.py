9fb0bea3e53af7981792a371d8639934d5a20c2e
kouta1612 <kouta1612world69@gmail.com>
2026-05-01 19:35:46 +0900

    リファクタリング

--- edp/Q/main.py
+++ edp/Q/main.py
@@ -4,11 +4,14 @@ class segtree:
         while self.n < n: self.n *= 2
         self.nodes = [0] * (self.n * 2)
 
-    def query(self, l: int, r: int, i: int, ql: int, qr: int) -> int:
+    def query(self, ql: int, qr: int) -> int:
+        return self._query(0, self.n, 0, ql, qr)
+    
+    def _query(self, l: int, r: int, i: int, ql: int, qr: int) -> int:
         if r <= ql or qr <= l: return 0
         if ql <= l and r <= qr: return self.nodes[i]
         mid = (l+r) // 2
-        vl, vr = self.query(l, mid, 2*i+1, ql, qr), self.query(mid, r, 2*i+2, ql, qr)
+        vl, vr = self._query(l, mid, 2*i+1, ql, qr), self._query(mid, r, 2*i+2, ql, qr)
         return max(vl, vr)
 
     def update(self, i: int, x: int):
@@ -24,7 +27,7 @@ h, a = list(map(int, input().split())), list(map(int, input().split()))
 MAX_H = max(h)
 seg = segtree(MAX_H+1)
 for i in range(N):
-    best = seg.query(0, seg.n, 0, 0, h[i])
+    best = seg.query(0, h[i])
     seg.update(h[i], best + a[i])
 
-print(seg.query(0, seg.n, 0, 0, seg.n))
+print(seg.query(0, seg.n))
